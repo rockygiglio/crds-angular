955d32b42de890515c7b461ad1b153e06873d7e8
silbermm <silbermm@gmail.com>
2016-01-07 22:02:49 -0500

    Hotfix for Unity Config
    Determine if explicitly registering controllers fixes production

--- Gateway/crds-angular/App_Start/UnityConfig.cs
+++ Gateway/crds-angular/App_Start/UnityConfig.cs
@@ -3,6 +3,7 @@ using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Configuration;
 using System.Configuration;
 using System.Web.Http;
+using crds_angular.Controllers.API;
 using Unity.WebApi;
 
 
@@ -15,7 +16,6 @@ namespace crds_angular
             var section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
             IUnityContainer container = new UnityContainer();
             section.Configure(container);
-
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
