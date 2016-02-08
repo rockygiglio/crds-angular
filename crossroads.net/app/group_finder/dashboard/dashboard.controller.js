132d5c7191f3925b7f4f03ae34eb3e86c92e4bb5
Taylor C. MacDonald <tcmacdonald@gmail.com>
2016-02-08 16:57:28 -0500

    Implement constants containing sole reference to series specific content.

--- crossroads.net/app/group_finder/dashboard/dashboard.controller.js
+++ crossroads.net/app/group_finder/dashboard/dashboard.controller.js
@@ -3,17 +3,21 @@
 
   module.exports = DashboardCtrl;
 
-  DashboardCtrl.$inject = ['$scope', '$log', '$state', 'Profile', 'Person', 'AuthService'];
+  DashboardCtrl.$inject = ['$scope', '$log', '$state', 'Profile', 'Person', 'AuthService', 'SERIES'];
 
-  function DashboardCtrl($scope, $log, $state, Profile, Person, AuthService) {
+  function DashboardCtrl($scope, $log, $state, Profile, Person, AuthService, SERIES) {
     var vm = this;
 
     if (AuthService.isAuthenticated() === false) {
       $log.debug('not logged in');
-      $state.go('brave.welcome');
+      $state.go(SERIES.permalink + '.welcome');
     }
 
     vm.profileData = { person: Person };
+
+    vm.startOver = function() {
+      $state.go(SERIES.permalink + '.summary');
+    };
   }
 
 })();
