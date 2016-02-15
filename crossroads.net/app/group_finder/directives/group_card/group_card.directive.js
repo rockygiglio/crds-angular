76414cb7871706561688f04f3d8e8811e0bbfe8b
Gabriel Smith <gabriel.smith01@gmail.com>
2016-02-15 07:31:10 -0500

    updated group directive for join results; added modal

--- crossroads.net/app/group_finder/directives/group_card/group_card.directive.js
+++ crossroads.net/app/group_finder/directives/group_card/group_card.directive.js
@@ -4,6 +4,8 @@
   module.exports = GroupCardDirective;
 
   require('./group_card.html');
+  require('./group_join.html');
+  require('./join_modal.html');
 
   GroupCardDirective.$inject = ['$log'];
 
@@ -12,10 +14,19 @@
       restrict: 'AE',
       scope: {
         group: '=',
-        host: '='
+        host: '=',
+        template: '@'
       },
       controller: require('./group_card.controller'),
-      templateUrl: 'group_card/group_card.html'
+      // templateUrl: 'group_card/group_card.html'
+      templateUrl: function (el, attrs) {
+        var template = 'group_card/group_card.html';
+        if (attrs.template === 'join') {
+          template = 'group_card/group_join.html';
+        }
+
+        return template;
+      }
     };
   }
 })();
