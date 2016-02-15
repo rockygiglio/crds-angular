39a1304bcefc3698cd56266728cdf0cacd1f5294
Taylor C. MacDonald <tcmacdonald@gmail.com>
2016-02-15 11:59:25 -0500

    Refactor login/auth redirect.

--- crossroads.net/app/group_finder/group_finder.controller.js
+++ crossroads.net/app/group_finder/group_finder.controller.js
@@ -3,15 +3,26 @@
 
   module.exports = GroupFinderCtrl;
 
-  GroupFinderCtrl.$inject = ['$rootScope', '$anchorScroll'];
+  GroupFinderCtrl.$inject = ['$state', '$rootScope', '$anchorScroll'];
 
-  function GroupFinderCtrl($rootScope, $anchorScroll) {
+  function GroupFinderCtrl($state, $rootScope, $anchorScroll) {
 
     // Reset scroll position to top of window whenever state changes.
     // @see https://github.com/angular-ui/ui-router/wiki#state-change-events
     $rootScope.$on('$stateChangeSuccess', function () {
       $anchorScroll();
     });
+
+    var vm = this;
+        // TODO
+        vm.isHost = true;
+
+    if(vm.isHost) {
+      $state.go('group_finder.dashboard');
+    } else {
+      $state.go('group_finder.summary');
+    }
+
   }
 
 })();
