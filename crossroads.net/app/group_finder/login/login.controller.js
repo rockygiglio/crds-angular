3f3f87d5a5576863f8776a8c17207f9e6461330c
Chris Tallent <chris@tallented.net>
2016-02-14 22:54:15 -0500

    Revert debug button used to test the distance matrix service call

--- crossroads.net/app/group_finder/login/login.controller.js
+++ crossroads.net/app/group_finder/login/login.controller.js
@@ -3,18 +3,9 @@
 
   module.exports = LoginCtrl;
 
-  LoginCtrl.$inject = ['$log', '$state', '$cookies', 'Responses', 'AuthService', 'GroupInfo', 'GoogleDistanceMatrixService'];
+  LoginCtrl.$inject = ['$log', '$state', '$cookies', 'Responses', 'AuthService', 'GroupInfo'];
 
-  function LoginCtrl($log, $state, $cookies, Responses, AuthService, GroupInfo, GoogleDistanceMatrixService) {
-    var vm = this;
-    vm.matrix = function() {
-      GoogleDistanceMatrixService.distanceFromAddress('10031 Manor Ln, Verona KY', [
-        '828 Heights Blvd, Florence, KY 41042',
-        '3500 Montgomery Rd, Cincinnati, OH 45207',
-        'asdfasdfa,a  a a zxxxx'
-      ]);
-    };
-
+  function LoginCtrl($log, $state, $cookies, Responses, AuthService, GroupInfo) {
     if (AuthService.isAuthenticated() === true) {
       if (GroupInfo.getHosting().length > 0 || GroupInfo.getParticipating().length > 0) {
         $log.debug('login.controller.js - group member: redirecting');
