d869187b3ee37cb553210871af9ab967f2f9920c
Chris Tallent <chris@tallented.net>
2016-02-17 00:52:38 -0500

    [#113773115] Convert the group invitation form to a directive; Leave the actual email process as a To Do

---
+++ crossroads.net/app/group_finder/directives/group_invitation/group_invitation.controller.js
@@ -0,0 +1,30 @@
+(function(){
+    'use strict';
+
+    module.exports = GroupInvitationCtrl;
+
+    GroupInvitationCtrl.$inject = ['$scope', '$log'];
+
+    function GroupInvitationCtrl($scope, $log) {
+        var vm = this;
+        vm.inviteMember = inviteMember;
+
+        //
+        // Controller implementation
+        //
+
+        function inviteMember() {
+            // TODO add validation. Review how to send email without `toContactId`
+            $log.debug('Sending group invitation Email to: ' + vm.invitee);
+            var toSend = {
+                'fromContactId': $scope.group.host.contactId,
+                'fromUserId': 0,
+                'toContactId': 0,
+                'templateId': 0,
+                'mergeData': {}
+            };
+
+        };
+    }
+
+})();
