a065227fed6603f7499004a0321604f682605b3b
IngageSandiR <sandi.ritter@ingagepartners.com>
2016-02-18 12:30:19 -0500

    US3208 fix scripts

--- CI/SQL/20160217141000_US3208-AddCurrentGroupsPageView.sql
+++ CI/SQL/20160217141000_US3208-AddCurrentGroupsPageView.sql
@@ -1,3 +1,6 @@
+USE MinitryPlatform
+GO
+
 SET IDENTITY_INSERT [dbo].[dp_Page_Views] ON
 
 INSERT INTO [dp_Page_Views]([Page_View_ID],[View_Title],[Page_ID],[Description],[Field_List],[View_Clause],[Order_By],[User_ID],[User_Group_ID])
--- CI/SQL/20160218091800_US3208-EndDateExistingJourneyGroups.sql
+++ CI/SQL/20160218091800_US3208-EndDateExistingJourneyGroups.sql
@@ -1,3 +1,6 @@
+USE MinistryPlatform
+GO
+
 UPDATE Groups
 SET End_Date = '01-25-2016'
 WHERE End_Date IS NULL and Group_Type_ID = 19
