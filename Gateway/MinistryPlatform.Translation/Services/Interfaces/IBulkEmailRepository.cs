540ae95fc71bda1cbbb456e98fb5dfa92c3cc194
jpc11623 <john.cleaver@ingagepartners.com>
2016-02-19 07:26:29 -0500

    Kids Club Optin
    US3238 - Updated to account for pending subscriptions and added some unit tests

--- Gateway/MinistryPlatform.Translation/Services/Interfaces/IBulkEmailRepository.cs
+++ Gateway/MinistryPlatform.Translation/Services/Interfaces/IBulkEmailRepository.cs
@@ -10,6 +10,6 @@ namespace MinistryPlatform.Translation.Services.Interfaces
         List<int> GetPageViewIds(string token, int publicationId);
         List<BulkEmailSubscriber> GetSubscribers(string token, int publicationId, List<int> pageViewIds);
         void UpdateSubscriber(string token, BulkEmailSubscriber subscriber);
-        void SetSubscriberStatus(string token, BulkEmailSubscriberOpt subscriberOpt);
+        bool SetSubscriberStatus(string token, BulkEmailSubscriberOpt subscriberOpt);
     }
 }
