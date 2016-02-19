f0a7f79152c7c8f8463bfd9804f9ca0c40ac5a5b
IngageSandiR <sandi.ritter@ingagepartners.com>
2016-02-19 10:58:48 -0500

    US3214 more tests

--- Gateway/MinistryPlatform.Translation.Test/Services/AddressServiceTest.cs
+++ Gateway/MinistryPlatform.Translation.Test/Services/AddressServiceTest.cs
@@ -58,7 +58,7 @@ namespace MinistryPlatform.Translation.Test.Services
                 {"State/Region", "OH"},
                 {"Postal_Code", "45454"},
                 {"Foreign_Country", "USA"},
-                {"County", "Hamilton"},
+                {"County", "Hamilton"}
             };
 
             _ministryPlatformService.Setup(m => m.CreateRecord(271, It.IsAny<Dictionary<string, object>>(), apiToken, false)).Returns(addressId);
@@ -68,6 +68,50 @@ namespace MinistryPlatform.Translation.Test.Services
 
             Assert.IsNotNull(addrId);
             Assert.AreEqual(addressId, addrId);
+        }
+
+        [Test]
+        public void FindMatchingAddresses()
+        {
+           var addrRecords = new List<Dictionary<string, object>>()
+            {
+                new Dictionary<string, object>()
+                {
+                    {"dp_RecordID", 12345},
+                    {"Address_Line_1", "321 Road Ln"},
+                    {"Address_Line_2", "Suite 100"},
+                    {"City", "Madison"},
+                    {"State/Region", "OH"},
+                    {"Postal_Code", "45454"},
+                    {"Foreign_Country", "USA"},
+                    {"County", "Hamilton"}
+                }
+            };
+
+            var addr = new Address()
+            {
+                Address_ID = 12345,
+                Address_Line_1 = "321 Road Ln",
+                Address_Line_2 = "Suite 100",
+                City = "Madison",
+                State = "OH",
+                Postal_Code = "45454",
+                Foreign_Country = "USA",
+                County = "Hamilton"
+            };
+
+            _ministryPlatformService.Setup(m => m.GetRecordsDict(271, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(addrRecords);
+
+            var records = _fixture.FindMatchingAddresses(addr);
+
+            _ministryPlatformService.VerifyAll();
+            Assert.IsNotNull(records);
+            Assert.AreEqual(addr.Address_ID , records[0].Address_ID);
+            Assert.AreEqual(addr.Address_Line_1, records[0].Address_Line_1);
+            Assert.AreEqual(addr.Address_Line_2, records[0].Address_Line_2);
+            Assert.AreEqual(addr.City, records[0].City);
+            Assert.AreEqual(addr.State, records[0].State);
+            Assert.AreEqual(addr.Postal_Code, records[0].Postal_Code);
         }
 
     }
