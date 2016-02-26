a7f42107d1d82268c41690cee156364b1ace6fd4
Gabriel Smith <gabriel.smith01@gmail.com>
2016-02-26 16:43:17 -0500

    updated group type on dashboard

--- crossroads.net/app/group_finder/services/group_info.service.js
+++ crossroads.net/app/group_finder/services/group_info.service.js
@@ -35,8 +35,13 @@
           var cid = $cookies.get('userId');
           if (cid) {
             _.each(data, function(group) {
+
+              // default to something
               if (group.contactId === parseInt(cid)) {
                 group.isHost = true;
+                if (_.has(group.singleAttributes[73].attribute, 'description')) {
+                  group.type = group.singleAttributes[73].attribute.description;
+                }
                 groups.hosting.push(group);
 
                 // Query the other participants of the group
