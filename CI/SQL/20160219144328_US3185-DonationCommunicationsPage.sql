75512142e1b532ed012631d13e83321d4a307b87
jpc11623 <john.cleaver@ingagepartners.com>
2016-02-28 20:11:27 -0500

    Trip Participants Message
    US3185 - Added Page ID and Page Section for Donation Communications page.

--- CI/SQL/20160219144328_US3185-DonationCommunicationsPage.sql
+++ CI/SQL/20160219144328_US3185-DonationCommunicationsPage.sql
@@ -4,7 +4,8 @@ GO
 IF NOT EXISTS(SELECT * FROM [dbo].[dp_Pages] WHERE [Display_Name] = 'Donation Communications')
 BEGIN
 INSERT INTO [dbo].[dp_Pages]
-           ([Display_Name]
+           ([Page_ID]
+		   ,[Display_Name]
            ,[Singular_Name]
            ,[Description]
            ,[View_Order]
@@ -15,7 +16,8 @@ INSERT INTO [dbo].[dp_Pages]
            ,[Selected_Record_Expression]
            ,[Display_Copy])
      VALUES
-           ('Donation Communications'
+           (540
+		   ,'Donation Communications'
            ,'Donation Communication'
            ,'Communication Messages for Donations'
            ,100
@@ -25,4 +27,16 @@ INSERT INTO [dbo].[dp_Pages]
            ,'Donation_Communications_ID, Donation_ID, Communication_ID'
            ,'cr_Donation_Communications.Donation_Communications_ID'
            ,0)
+END
+
+IF NOT EXISTS(SELECT * FROM [dbo].[dp_Page_Section_Pages] WHERE Page_ID=540 AND Page_Section_ID=9)
+BEGIN
+	INSERT INTO [dbo].[dp_Page_Section_Pages]
+		([Page_ID],
+		[Page_Section_ID],
+		[User_ID]) 
+	VALUES
+		(540,
+		9,
+		NULL)
 END
