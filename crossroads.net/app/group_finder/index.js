e57ed11d0a18650c7d7dccb12fc1feef84380bda
Gabriel Smith <gabriel.smith01@gmail.com>
2016-03-02 16:07:52 -0500

    code cleanup

--- crossroads.net/app/group_finder/index.js
+++ crossroads.net/app/group_finder/index.js
@@ -10,20 +10,21 @@
 
   angular.module(MODULES.GROUP_FINDER, [MODULES.CORE, MODULES.COMMON, 'rzModule'])
     .config(require('./group_finder.routes'))
+    .constant('GROUP_ROLE_ID_PARTICIPANT',       require('./group_finder.constants').GROUP_ROLE_ID_PARTICIPANT)
     .constant('DAYS',                  require('./group_finder.constants').DAYS)
     .constant('SERIES',                require('./group_finder.constants').SERIES)
     .constant('GROUP_TYPES',           require('./group_finder.constants').GROUP_TYPES)
     .constant('GROUP_API_CONSTANTS',   require('./group_finder.constants').GROUP_API_CONSTANTS)
     .constant('GOOGLE_API_KEY',        require('./group_finder.constants').GOOGLE_API_KEY)
-    .constant('QA_PAGES',               require('./group_finder.constants').QA_PAGES)
-    .constant('GROUP_ROLE_ID_PARTICIPANT',       require('./group_finder.constants').GROUP_ROLE_ID_PARTICIPANT)
+    .constant('QA_PAGES',              require('./group_finder.constants').QA_PAGES)
     .constant('GROUP_ROLE_ID_HOST',    require('./group_finder.constants').GROUP_ROLE_ID_HOST)
     .constant('EMAIL_TEMPLATES',       require('./group_finder.constants').EMAIL_TEMPLATES)
+    .constant('ANYWHERE_GROUP_ID',     require('./group_finder.constants').ANYWHERE_GROUP_ID)
     .filter('humanize',                require('./filters/humanize.filter.js'))
     .controller('GroupFinderCtrl',     require('./group_finder.controller'))
     .controller('SummaryCtrl',         require('./summary/summary.controller'))
     .controller('GroupInvitationCtrl', require('./invitation/invitation.controller'))
-    ;
+  ;
 
   require('./services');
   require('./dashboard');
