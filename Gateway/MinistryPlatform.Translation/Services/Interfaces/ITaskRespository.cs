7b7d68fbe2ea1ffa5f9c7313cb1846139813f7d3
jpc11623 <john.cleaver@ingagepartners.com>
2016-03-03 13:46:53 -0500

    Recurring Event Reservations
    US3381 - Initial code checkin

---
+++ Gateway/MinistryPlatform.Translation/Services/Interfaces/ITaskRespository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MinistryPlatform.Models;
+
+
+namespace MinistryPlatform.Translation.Services.Interfaces
+{
+    interface ITaskRespository
+    {
+        List<MPTask> GetRecurringEventTasks();
+
+        void UpdateTask(MPTask task);
+    }
+}
---
+++ Gateway/MinistryPlatform.Translation/Services/TaskRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinistryPlatform.Models;
+using MinistryPlatform.Translation.Services.Interfaces;
+
+namespace MinistryPlatform.Translation.Services
+{
+    public class TaskRepository : ITaskRespository
+    {
+        public List<MPTask> GetRecurringEventTasks()
+        {
+            return null;
+        }
+
+        public void UpdateTask(MPTask task)
+        {
+            
+        }
+    }
+}
