8c600c908d32204e5171c947506bab662904cf8d
Gabriel Smith <gabriel.smith01@gmail.com>
2016-03-03 13:05:40 -0500

    update review page if no times selected

--- crossroads.net/app/group_finder/join/join_review.controller.js
+++ crossroads.net/app/group_finder/join/join_review.controller.js
@@ -9,6 +9,7 @@
     'Responses',
     'ZipcodeService',
     'GroupInvitationService',
+    'LookupDefinitions',
     'ANYWHERE_GROUP_ID',
     'GROUP_ROLE_ID_PARTICIPANT'
   ];
@@ -19,51 +20,97 @@
     Responses,
     ZipcodeService,
     GroupInvitationService,
+    LookupDefinitions,
     ANYWHERE_GROUP_ID,
     GROUP_ROLE_ID_PARTICIPANT
   ) {
     var vm = this;
+    vm.initialize = initialize;
+    vm.goToHost = goToHost;
+    vm.goToResults = goToResults;
+    vm.lookup = LookupDefinitions;
+    vm.lookupContains = lookupContains;
 
-    vm.responses = Responses;
-    vm.showUpsell = parseInt(vm.responses.data.prior_participation) > 2;
-    vm.showResults = vm.showUpsell === false;
-    vm.contactCrds = false;
-    Responses.data.completedQa = true;
+    function initialize() {
 
-    if (vm.responses.data.location && vm.responses.data.location.zip) {
-      vm.zipcode = parseInt(vm.responses.data.location.zip);
-      if (ZipcodeService.isLocalZipcode(vm.zipcode) === false) {
-        vm.showUpsell = false;
-        vm.showResults = false;
-        vm.contactCrds = true;
+      vm.responses = getResponses();
+      vm.showUpsell = parseInt(vm.responses.prior_participation) > 2;
+      vm.showResults = vm.showUpsell === false;
+      vm.contactCrds = false;
+      Responses.data.completedQa = true;
+      var meetTime = {
+        week: true,
+        weekend: true
+      };
 
-        var promise = GroupInvitationService.acceptInvitation(ANYWHERE_GROUP_ID,
-          {capacity: 1, groupRoleId: GROUP_ROLE_ID_PARTICIPANT});
-        promise.then(function() {
-          // Invitation acceptance was successful
-          vm.accepted = true;
-        }, function(error) {
-          // An error happened accepting the invitation
-          vm.rejected = true;
-        });
+      // check of invalid date_times
+      _.each(vm.responses.date_time_week, function(value, id) {
+        if (value && vm.lookupContains(id, 'can\'t meet')) {
+          meetTime.week = false;
+        }
+      });
+
+      // check of invalid date_times
+      _.each(vm.responses.date_time_weekend, function(value, id) {
+        if (value && vm.lookupContains(id, 'can\'t meet')) {
+          meetTime.weekend = false;
+        }
+      });
+
+      vm.invalidTime = (!meetTime.week && !meetTime.weekend);
+
+      if (vm.responses.location && vm.responses.location.zip) {
+        vm.zipcode = parseInt(vm.responses.location.zip);
+        if (ZipcodeService.isLocalZipcode(vm.zipcode) === false) {
+          vm.showUpsell = false;
+          vm.showResults = false;
+          vm.contactCrds = true;
+
+          var promise = GroupInvitationService.acceptInvitation(ANYWHERE_GROUP_ID,
+            {capacity: 1, groupRoleId: GROUP_ROLE_ID_PARTICIPANT});
+          promise.then(function() {
+            // Invitation acceptance was successful
+            vm.accepted = true;
+          }, function(error) {
+            // An error happened accepting the invitation
+            vm.rejected = true;
+          });
+        }
+      }
+
+      if (vm.showResults === true && vm.contactCrds === false) {
+        $state.go('group_finder.join.results');
+      }
+
+      if (parseInt(vm.responses.relationship_status) === 2) {
+        $scope.showInvite = true;
       }
     }
+    
+    function getResponses() {
+      var responses = Responses.data;
+      if (responses.completed_flow === true) {
+        sessionStorage.setItem('participant', angular.toJson(responses));
+      } else {
+        responses = angular.fromJson(sessionStorage.getItem('participant'));
+      }
 
-    if (vm.showResults === true && vm.contactCrds === false) {
-      $state.go('group_finder.join.results');
+      return responses;
     }
 
-    if (parseInt(vm.responses.data.relationship_status) === 2) {
-      $scope.showInvite = true;
-    }
-
-    vm.goToHost = function() {
+    function goToHost() {
       $state.go('group_finder.host');
-    };
+    }
 
-    vm.goToResults = function() {
+    function goToResults() {
       $state.go('group_finder.join.results');
-    };
+    }
+
+    function lookupContains(id, keyword) {
+      return vm.lookup[id].name.toLowerCase().indexOf(keyword) > -1;
+    }
+    
+    vm.initialize();
 
   }
 
