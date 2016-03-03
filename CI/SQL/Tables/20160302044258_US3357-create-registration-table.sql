10087f7a71663b23a7d9c7f598a4f0dbd8f7cf4d
IngageMaddoxT <tony.maddox@ingagepartners.com>
2016-03-03 14:23:51 -0500

    US3357 rename role from group role to role

--- CI/SQL/Tables/20160302044258_US3357-create-registration-table.sql
+++ CI/SQL/Tables/20160302044258_US3357-create-registration-table.sql
@@ -7,7 +7,7 @@ USE [MinistryPlatform]
  SET QUOTED_IDENTIFIER ON
  GO
 
- DROP TABLE [dbo].[cr_Registrations]
+ --DROP TABLE [dbo].[cr_Registrations]
  
  IF NOT EXISTS(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[cr_Registrations]') AND type in (N'U'))
  BEGIN
@@ -19,19 +19,15 @@ USE [MinistryPlatform]
 	[Initiative_ID] [int] NOT NULL,
 	[Spouse_Participation] [bit] NOT NULL,
  	[Additional_Information] nvarchar(500) NULL,
- 	[Group_Role_ID] [int] NOT NULL, 
+ 	[Role_ID] [int] NOT NULL, 
     [Domain_ID] [int] NOT NULL,
  	
  	CONSTRAINT [FK_Registrations_Initiative] FOREIGN KEY ([Initiative_ID]) REFERENCES [cr_Initiatives]([Initiative_ID]), 
  	CONSTRAINT [FK_Registrations_Location] FOREIGN KEY (Location_ID) REFERENCES Locations(Location_ID), 
  	CONSTRAINT [FK_Registrations_Organization] FOREIGN KEY (Organization_ID) REFERENCES cr_Organizations(Organization_ID), 
  	CONSTRAINT [FK_Registrations_Participant] FOREIGN KEY ([Participant_ID]) REFERENCES [Participants]([Participant_ID]),
-	CONSTRAINT [FK_Registrations_Role] FOREIGN KEY ([Group_Role_ID]) REFERENCES [Group_Roles]([Group_Role_ID]),
+	CONSTRAINT [FK_Registrations_Role] FOREIGN KEY ([Role_ID]) REFERENCES [Group_Roles]([Group_Role_ID]),
+	CONSTRAINT [FK_Registrations_Domain] FOREIGN KEY ([Domain_ID]) REFERENCES dp_Domains(Domain_ID),
  	CONSTRAINT [PK_Registrations] PRIMARY KEY CLUSTERED([Registration_ID] ASC)
  )
-
- ALTER TABLE dbo.cr_Registrations ADD CONSTRAINT
-	FK_cr_Registrations_dp_Domains FOREIGN KEY
-	(Domain_ID) REFERENCES dbo.dp_Domains (Domain_ID) 
-
 END
