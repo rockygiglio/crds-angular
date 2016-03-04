d254493147f1509f67210270db5b30535d98c1b5
IngageMaddoxT <tony.maddox@ingagepartners.com>
2016-03-04 09:20:53 -0500

    US3357

--- CI/SQL/Tables/20160302044258_US3357-create-registration-table.sql
+++ CI/SQL/Tables/20160302044258_US3357-create-registration-table.sql
@@ -7,14 +7,12 @@ USE [MinistryPlatform]
  SET QUOTED_IDENTIFIER ON
  GO
 
- --DROP TABLE [dbo].[cr_Registrations]
- 
  IF NOT EXISTS(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[cr_Registrations]') AND type in (N'U'))
  BEGIN
  CREATE TABLE [dbo].[cr_Registrations](
  	[Registration_ID] [int] IDENTITY(1,1) NOT NULL,
 	[Organization_ID] [int] NOT NULL,
-	[Location_ID] [int] NOT NULL,
+	Preferred_Launch_Site_ID [int] NOT NULL,
  	[Participant_ID] [int] NOT NULL,
 	[Initiative_ID] [int] NOT NULL,
 	[Spouse_Participation] [bit] NOT NULL,
@@ -23,7 +21,7 @@ USE [MinistryPlatform]
     [Domain_ID] [int] NOT NULL,
  	
  	CONSTRAINT [FK_Registrations_Initiative] FOREIGN KEY ([Initiative_ID]) REFERENCES [cr_Initiatives]([Initiative_ID]), 
- 	CONSTRAINT [FK_Registrations_Location] FOREIGN KEY (Location_ID) REFERENCES Locations(Location_ID), 
+ 	CONSTRAINT [FK_Registrations_Location] FOREIGN KEY (Preferred_Launch_Site_ID) REFERENCES Locations(Location_ID), 
  	CONSTRAINT [FK_Registrations_Organization] FOREIGN KEY (Organization_ID) REFERENCES cr_Organizations(Organization_ID), 
  	CONSTRAINT [FK_Registrations_Participant] FOREIGN KEY ([Participant_ID]) REFERENCES [Participants]([Participant_ID]),
 	CONSTRAINT [FK_Registrations_Role] FOREIGN KEY ([Role_ID]) REFERENCES [Group_Roles]([Group_Role_ID]),
