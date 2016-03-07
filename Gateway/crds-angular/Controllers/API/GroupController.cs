3c8dfb12280b95975a6e18a2f2f6ee93dd7369f8
Charlie Retzler <charlie.retzler@ingagepartners.com>
2016-03-07 08:38:47 -0500

    US3217: Adding empty endpoint

--- Gateway/crds-angular/Controllers/API/GroupController.cs
+++ Gateway/crds-angular/Controllers/API/GroupController.cs
@@ -207,6 +207,33 @@ namespace crds_angular.Controllers.API
             });
         }
 
+        /// <summary>
+        /// This finds groups that match a participants answers for a specific group type.
+        /// If one or more groups are found, then a list of the group are returned.
+        /// If no groups are found, then an empty list will be returned.
+        /// </summary>
+        /// <param name="groupTypeId">Group Type ID of the groups to search</param>
+        /// <param name="participant">Participants answers to find matching groups</param>
+        /// <returns></returns>
+        [RequiresAuthorization]
+        [ResponseType(typeof(List<GroupDTO>))]
+        [Route("api/group/groupType/{groupTypeId}/search")]
+        public IHttpActionResult GetSearchMatches(int groupTypeId, [FromBody] GroupParticipantDTO participant)
+        {
+            return Authorized(token =>
+            {
+                try
+                {
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    var apiError = new ApiErrorDto("Error searching for matching groups for group type ID " + groupTypeId, ex);
+                    throw new HttpResponseException(apiError.HttpResponseMessage);
+                }
+            });
+        }
+
         /// Takes in a Group ID and retrieves all active participants for the group id.
         /// </summary>
         /// <param name="groupId">GroupId of the group.</param>
