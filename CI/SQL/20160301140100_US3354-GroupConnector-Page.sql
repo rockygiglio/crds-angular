1fc0a755e19d8c2a994dde7ea406f2606a1a33fd
IngageMaddoxT <tony.maddox@ingagepartners.com>
2016-03-09 11:57:49 -0500

    DE1255 - fixing group connector issues

--- CI/SQL/20160301140100_US3354-GroupConnector-Page.sql
+++ CI/SQL/20160301140100_US3354-GroupConnector-Page.sql
@@ -2,7 +2,19 @@ USE MinistryPlatform;
 GO
 
 DECLARE @PAGE_ID int = 13;
+DECLARE @PAGE_SECTION_ID INT = 21;
+DECLARE @ACCESS_LEVEL_FULL INT = 3;
+DECLARE @ROLE_ID INT = 107; --System Administrator - CRDS
+
 DECLARE @PAGE_NAME nvarchar(100) = N'Group Connectors';
+
+DECLARE @DEFAULT_FIELD_LIST NVARCHAR(1000) = N'Created_By_Table_Participant_ID_Table_Contact_ID_Table.[Display_Name] AS [Created_By]
+		, Project_ID_Table.[Project_Name] AS [Project Name]
+		, Created_By_Table_Preferred_Launch_Site_ID_Table.[Location_Name] AS [Preferred Launch Site]
+		, Created_By_Table_Organization_ID_Table.[Name] AS [Organization Name]
+		, Created_By_Table_Initiative_ID_Table.[Initiative_Name] AS [Initiative Name]';
+
+DECLARE @SELECTED_RECORD_EXPRESSION NVARCHAR(50) = N'Created_By_Table_Participant_ID_Table_Contact_ID_Table.[Display_Name]';
 
 IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Pages] where [Display_Name] = @PAGE_NAME)
 BEGIN
@@ -27,14 +39,22 @@ BEGIN
 		,N'GroupConnector_ID'
 		,N'Groups of folks that want to volunteer together'
 		,N'cr_GroupConnectors'		
-		,N'cr_GroupConnectors.Name  AS [Group Connector], Project_ID_Table.[Project_Name] AS [Project Name]
-		, Organization_ID_Table.Name AS [Organization Name]
-		, Initiative_ID_Table.Initiative_Name AS [Initiative Name]
-		, Primary_Contact_Table.Display_Name AS [Primary Contact]'
-		,N'cr_GroupConnectors.Name'
+		,@DEFAULT_FIELD_LIST
+		,@SELECTED_RECORD_EXPRESSION
 		,0
 		,100
 	)
 
 	SET IDENTITY_INSERT [dbo].[dp_Pages] OFF
 END
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Page_Section_Pages] WHERE Page_Section_ID = 21 AND Page_ID = @PAGE_ID)
+BEGIN
+INSERT INTO [dbo].[dp_Page_Section_Pages]
+           ([Page_ID]
+           ,[Page_Section_ID])
+     VALUES
+           (@PAGE_ID, @PAGE_SECTION_ID)
+END
+
+INSERT INTO dp_Role_Pages (Role_ID, Page_ID, Access_Level) VALUES (@ROLE_ID, @PAGE_ID, @ACCESS_LEVEL_FULL)
---
+++ CI/SQL/20160309105101_US3354-groupconnector-registrants-sub-page.sql
@@ -0,0 +1,52 @@
+USE MinistryPlatform;
+GO
+
+DECLARE @SUB_PAGE_ID int = 4;
+DECLARE @DISPLAY_NAME NVARCHAR(50) = 'Registrations';
+DECLARE @SINGULAR_NAME NVARCHAR(50) = 'Registration';
+DECLARE @PAGE_ID INT = 13;
+DECLARE @VIEW_ORDER SMALLINT = 10;
+DECLARE @PRIMARY_TABLE NVARCHAR(50) = 'cr_GroupConnectorRegistrations';
+DECLARE @PRIMARY_KEY NVARCHAR(50) = 'GroupConnectorRegistration_ID';
+DECLARE @SELECTED_RECORD_EXPRESSION NVARCHAR(500) = 'Registration_ID_Table_Participant_ID_Table_Contact_ID_Table.[Display_Name]';
+DECLARE @FILTER_KEY NVARCHAR(50) = 'GroupConnector_ID';
+DECLARE @Relation_Type_ManyToMany INT = '2';
+DECLARE @DISPLAY_COPY BIT = 0;
+DECLARE @ACCESS_LEVEL_FULL INT = 3;
+DECLARE @ROLE_ID INT = 107; --System Administrator - CRDS
+
+DECLARE @DEFAULT_FIELD_LIST NVARCHAR(4000) = N'Registration_ID_Table_Participant_ID_Table_Contact_ID_Table.[Display_Name] AS [Registrant]';
+
+SET IDENTITY_INSERT [dbo].[dp_Sub_Pages] ON;
+
+INSERT INTO [dbo].[dp_Sub_Pages]
+    ([Sub_Page_ID]
+    ,[Display_Name]
+    ,[Singular_Name]
+    ,[Page_ID]
+    ,[View_Order]
+    ,[Primary_Table]
+    ,[Default_Field_List]
+    ,[Selected_Record_Expression]
+    ,[Filter_Key]
+    ,[Relation_Type_ID]
+    ,[Display_Copy]
+)
+VALUES
+    (@SUB_PAGE_ID
+    ,@DISPLAY_NAME
+    ,@SINGULAR_NAME
+    ,@PAGE_ID
+    ,@VIEW_ORDER
+    ,@PRIMARY_TABLE
+    ,@DEFAULT_FIELD_LIST
+    ,@SELECTED_RECORD_EXPRESSION
+    ,@FILTER_KEY
+    ,@Relation_Type_ManyToMany
+    ,@DISPLAY_COPY
+);
+
+SET IDENTITY_INSERT [dbo].[dp_Sub_Pages] OFF;
+
+
+INSERT INTO dp_Role_Sub_Pages (Role_ID, Sub_Page_ID, Access_Level) VALUES (@ROLE_ID, @SUB_PAGE_ID, @ACCESS_LEVEL_FULL);
