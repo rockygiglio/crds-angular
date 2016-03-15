8cbc0fa76b08c2e7a2476c515f818d9e79c1c110
IngageMaddoxT <tony.maddox@ingagepartners.com>
2016-03-15 12:04:47 -0400

    us3354 updating create table script.  already ran in demo.

--- CI/SQL/20160301132600_US3354-GroupConnector.sql
+++ CI/SQL/20160301132600_US3354-GroupConnector.sql
@@ -7,16 +7,21 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[cr_GroupConnectors](
-	[GroupConnector_ID] [int] IDENTITY(1,1) NOT NULL,
-	[Project_ID] [int] NULL,
-	[Primary_Registration] INT NOT NULL, 
-	[Domain_ID] [int] NOT NULL,
-	CONSTRAINT [FK_GroupConnector_Project] FOREIGN KEY ([Project_ID]) REFERENCES [cr_Projects]([Project_ID]),  
-	CONSTRAINT [FK_GroupConnector_PrimaryRegistration] FOREIGN KEY ([Primary_Registration]) REFERENCES [cr_Registrations]([Registration_ID]),
-	CONSTRAINT [FK_GroupConnector_Domains] FOREIGN KEY ([Domain_ID]) REFERENCES [dp_Domains]([Domain_ID]),
-	CONSTRAINT [PK_GroupConnector] PRIMARY KEY CLUSTERED([GroupConnector_ID] ASC)
-)
+IF OBJECT_ID('dbo.cr_GroupConnectors', 'U') IS NULL
+BEGIN
+
+    CREATE TABLE [dbo].[cr_GroupConnectors](
+	    [GroupConnector_ID] [int] IDENTITY(1,1) NOT NULL,
+	    [Project_ID] [int] NULL,
+	    [Primary_Registration] INT NOT NULL, 
+	    [Domain_ID] [int] NOT NULL,
+	    CONSTRAINT [FK_GroupConnector_Project] FOREIGN KEY ([Project_ID]) REFERENCES [cr_Projects]([Project_ID]),  
+	    CONSTRAINT [FK_GroupConnector_PrimaryRegistration] FOREIGN KEY ([Primary_Registration]) REFERENCES [cr_Registrations]([Registration_ID]),
+	    CONSTRAINT [FK_GroupConnector_Domains] FOREIGN KEY ([Domain_ID]) REFERENCES [dp_Domains]([Domain_ID]),
+	    CONSTRAINT [PK_GroupConnector] PRIMARY KEY CLUSTERED([GroupConnector_ID] ASC)
+    )
+
+END
 
 
 
