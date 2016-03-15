b874929f1a69ec3f2322b529df4b2ba7f916b99e
IngageMaddoxT <tony.maddox@ingagepartners.com>
2016-03-15 13:09:58 -0400

    stub

--- Gateway/MinistryPlatform.Translation/Services/GoCincinnati/GroupConnectorService.cs
+++ Gateway/MinistryPlatform.Translation/Services/GoCincinnati/GroupConnectorService.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Crossroads.Utilities.Interfaces;
+using log4net;
+using MinistryPlatform.Translation.Services.Interfaces;
 
 namespace MinistryPlatform.Translation.Services.GoCincinnati
 {
-    class GroupConnectorService
+    public class GroupConnectorService : BaseService
     {
+        private readonly IMinistryPlatformService _ministryPlatformService;
+
+
+        private readonly ILog _logger = LogManager.GetLogger(typeof(RoomService));
+
+        public GroupConnectorService(IMinistryPlatformService ministryPlatformService, IAuthenticationService authenticationService, IConfigurationWrapper configuration)
+            : base(authenticationService, configuration)
+        {
+            _ministryPlatformService = ministryPlatformService;
+        }
+
+        public int CreateGroupConnector()
+        {
+            
+        }
     }
 }
