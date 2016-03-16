97a6c1cc6a0059570f021b0860671c6f742cca8a
Matt Silbernagel <silbermm@gmail.com>
2016-03-16 10:06:58 -0400

    US3392 - Enter Organization Name
    * moved dummy lookup data to controller

--- crossroads.net/app/go_volunteer/page/orgName/goVolunteerOrgName.component.js
+++ crossroads.net/app/go_volunteer/page/orgName/goVolunteerOrgName.component.js
@@ -17,7 +17,7 @@
 
     function GoVolunteerOrgNameController() {
       var vm = this;
-
+      vm.availableOptions = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
     }
   }
 
