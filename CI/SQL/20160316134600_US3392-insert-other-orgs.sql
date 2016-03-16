838f1d5f54a5329fb987e4446504e936cda7166f
silbermm <silbermm@gmail.com>
2016-03-16 13:54:50 -0400

    US3392 Enter Other Org Info
    * added some organizations into the table

---
+++ CI/SQL/20160316134600_US3392-insert-other-orgs.sql
@@ -0,0 +1,38 @@
+USE [MinistryPlatform]
+GO
+
+INSERT INTO [dbo].[cr_Other_Organizations]
+           ([Other_Organization])
+     VALUES
+			('Xenos Christian Fellowship'),
+			('Xenia Nazarene'),
+			('Whitewater Crossing'),
+			('White Oak Christian Church'),
+			('Wellspring Community Church'),
+			('Vineyard Westside'),
+			('Vineyard in eastgate'),
+			('Vineyard Cincinnati'),
+			('Vineyard'),
+			('Urbancrest Baptist'),
+			('Tryed Stone New Beginning Church'),
+			('Trinity Christian'),
+			('The Church of Jesus Christ of Latter-day Saints'),
+			('Sycamore Presbyterian Church'),
+			('Summit Church of Christ'),
+			('Southwest Church'),
+			('Southern baptist'),
+			('Solid Rock'),
+			('Sharonville United Method'),
+			('Seven Hills'),
+			('Rockdale Temple'),
+			('Rivers Crossing Community Church'),
+			('River of life'),
+			('Revival Center Ministries'),
+			('Redemption bible church'),
+			('Redeemer'),
+			('Quinn Chapel AME Church'),
+			('Prince of Peace'),
+			('Peoples Church')
+GO
+
+
