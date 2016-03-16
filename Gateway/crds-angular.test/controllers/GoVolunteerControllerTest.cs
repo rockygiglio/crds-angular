80f9b7964daa0b3e18385369e36a597784e035d9
silbermm <silbermm@gmail.com>
2016-03-16 09:53:21 -0400

    US3346 Indicate Org Association

--- Gateway/crds-angular.test/controllers/GoVolunteerControllerTest.cs
+++ Gateway/crds-angular.test/controllers/GoVolunteerControllerTest.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Web.Http.Controllers;
 using System.Web.Http.Results;
 using crds_angular.Controllers.API;
 using crds_angular.Models.Crossroads.GoVolunteer;
-using crds_angular.Services;
 using crds_angular.Services.Interfaces;
 using Moq;
 using NUnit.Framework;
-using System.Web.Http;
 using FsCheck;
 
 namespace crds_angular.test.controllers
@@ -26,25 +21,30 @@ namespace crds_angular.test.controllers
         public void Setup()
         {
             _organizationService = new Mock<IOrganizationService>();
-            _fixture = new GoVolunteerController(_organizationService.Object);
-            _fixture.Request = new HttpRequestMessage();
-            _fixture.RequestContext = new HttpRequestContext();
+            _fixture = new GoVolunteerController(_organizationService.Object)
+            {
+                Request = new HttpRequestMessage(),
+                RequestContext = new HttpRequestContext()
+            };
         }
 
         [Test]
         public void ShouldGetOrganizationByName()
         {            
-            const string name = "whatever";
-            var returnValue = ValidOrganization(name);
-            _organizationService.Setup(m => m.GetOrganizationByName(name)).Returns(returnValue);
-            var response = _fixture.GetOrganization(name);
+    
+            Prop.ForAll<int, int, string>((contactId, orgId, name) =>
+            {
+                var returnValue = ValidOrganization(contactId, orgId, name);
+                _organizationService.Setup(m => m.GetOrganizationByName(name)).Returns(returnValue);
+                var response = _fixture.GetOrganization(name);
+                Assert.IsNotNull(response);
+                Assert.IsInstanceOf<OkNegotiatedContentResult<Organization>>(response);
+                // ReSharper disable once SuspiciousTypeConversion.Global
+                var r = (OkNegotiatedContentResult<Organization>)response;
+                Assert.IsNotNull(r.Content);
+                Assert.AreSame(returnValue, r.Content);
+            }).VerboseCheckThrowOnFailure();
 
-            Assert.IsNotNull(response);
-            Assert.IsInstanceOf<OkNegotiatedContentResult<Organization>>(response);
-            // ReSharper disable once SuspiciousTypeConversion.Global
-            var r = (OkNegotiatedContentResult<Organization>) response;
-            Assert.IsNotNull(r.Content);
-            Assert.AreSame(returnValue, r.Content);
         }
 
         [Test]
@@ -56,25 +56,21 @@ namespace crds_angular.test.controllers
                 var response = _fixture.GetOrganization(st);
                 Assert.IsNotNull(response);
                 Assert.IsInstanceOf<NotFoundResult>(response);
-            }).QuickCheckThrowOnFailure();               
+            }).QuickCheckThrowOnFailure();            
         }
 
-        [Test]
-        public void ShouldUsFSCheck()
-        {
-            Prop.ForAll<int[]>(xs => xs.Reverse().Reverse().SequenceEqual(xs)).QuickCheckThrowOnFailure();
-        }
 
-        private static Organization ValidOrganization(string name)
+
+        private static Organization ValidOrganization(int contactId, int orgId, string name)
         {
             return new Organization()
             {
-                ContactId = 12345,
+                ContactId = contactId,
                 EndDate = new DateTime(),
                 StartDate = new DateTime(),
                 Name = name,
                 OpenSignup = true,
-                OrganizationId = 1234567890
+                OrganizationId = orgId
             };
         }
     }
