7cfaf6bbfd2023bfdae3abddbccffbfe419790e3
Brian Miller <bzmiller82@gmail.com>
2016-03-17 11:02:09 -0400

    Added Elderly mock image. Fixed mock for all project pref pages.
    US3456

--- crossroads.net/app/go_volunteer/page/projectPrefOne/goVolunteerProjectPrefOne.component.js
+++ crossroads.net/app/go_volunteer/page/projectPrefOne/goVolunteerProjectPrefOne.component.js
@@ -24,11 +24,11 @@
         { title: 'Gardening', state: '', age: '2', img: 'gard' },
         { title: 'Landscaping', state: '', age: '8', img: 'gardening' },
         { title: 'Organizing and Cleaning', state: '', age: '2', img: 'org' },
-        { title: 'Painting', state: 'disabled', age: '13', img: 'painting' },
+        { title: 'Painting', state: '', age: '13', img: 'painting' },
         { title: 'Prayer', state: '', age: '2', img: 'prayer' },
         { title: 'Serving Meals or Throw A Party', state: '', age: '8', img: 'cooking' },
         { title: 'Working with Children', state: '', age: '2', img: 'kids' },
-        { title: 'Working with the Elderly', state: '', age: '2', img: 'kids' }
+        { title: 'Working with the Elderly', state: '', age: '2', img: 'elder' }
       ];
 
     }
