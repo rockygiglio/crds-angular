a98c290c0a49ef513ee357495e1e7a5f975dbe0f
lknair2015 <lknair2015@gmail.com>
2016-03-28 13:09:14 -0400

    Updated GroupConnectorServiceTest

--- Gateway/MinistryPlatform.Translation.Test/Services/GroupConnectorServiceTest.cs
+++ Gateway/MinistryPlatform.Translation.Test/Services/GroupConnectorServiceTest.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography.X509Certificates;
 using Crossroads.Utilities.Interfaces;
-using Crossroads.Utilities.Services;
-using MinistryPlatform.Models;
 using MinistryPlatform.Translation.Services.GoCincinnati;
 using MinistryPlatform.Translation.Services.Interfaces;
-using MinistryPlatform.Translation.Services.Interfaces.GoCincinnati;
 using Moq;
 using NUnit.Framework;
 
@@ -32,31 +25,51 @@ namespace MinistryPlatform.Translation.Test.Services
             _configuration = new Mock<IConfigurationWrapper>();
 
             _configuration.Setup(mocked => mocked.GetConfigIntValue("GroupConnectorPageId")).Returns(467);
+            _configuration.Setup(mocked => mocked.GetConfigValue("GroupConnectorRegistrationPageId")).Returns("GroupConnectorRegistrationPageId");
             _configuration.Setup(m => m.GetEnvironmentVarAsString("API_USER")).Returns("uid");
             _configuration.Setup(m => m.GetEnvironmentVarAsString("API_PASSWORD")).Returns("pwd");
-
             _authService.Setup(m => m.Authenticate(It.IsAny<string>(), It.IsAny<string>())).Returns(new Dictionary<string, object> { { "token", "ABC" }, { "exp", "123" } });
-
-
             _fixture = new GroupConnectorService(_ministryPlatformService.Object, _authService.Object, _configuration.Object);
-
+            
         }
 
         [Test]
         public void ShouldCreateGroupConnector()
         {
             const int registrationId = 9999;
+            const int expGroupConnectorId = 8888;
 
             _ministryPlatformService.Setup(
               mocked => mocked.CreateRecord(467, It.IsAny<Dictionary<string, object>>(), "ABC", true))
-              .Returns(123);
+              .Returns(expGroupConnectorId);
+            _ministryPlatformService.Setup(
+             mocked => mocked.CreateSubRecord("GroupConnectorRegistrationPageId", expGroupConnectorId, It.IsAny<Dictionary<string, object>>(), "ABC", true))
+             .Returns(123);
 
             var groupConnector = _fixture.CreateGroupConnector(registrationId);
-            Assert.AreEqual(123, groupConnector);
+            Assert.AreEqual(expGroupConnectorId, groupConnector);
 
             _ministryPlatformService.VerifyAll();
-            Assert.IsNotNull(groupConnector);
-            Assert.AreEqual(9999, registrationId);
+        }
+
+        [Test]
+        public void ShouldThrowApplicationExceptionWhenGroupConnectorCreationFails()
+        {
+            Exception ex = new Exception("GroupConnecotr creation failed");
+            _ministryPlatformService.Setup(
+                mocked => mocked.CreateRecord(467, It.IsAny<Dictionary<string, object>>(), "ABC", true))
+                .Throws(ex);
+
+            try
+            {
+                _fixture.CreateGroupConnector(9999);
+                Assert.Fail("Expected exception was not thrown");
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOf(typeof(ApplicationException), e);
+                Assert.AreSame(ex, e.InnerException);
+            }
         }
 
         [Test]
@@ -64,18 +77,38 @@ namespace MinistryPlatform.Translation.Test.Services
         {
             const int registrationId = 9999;
             const int groupConnectorId = 8888;
-            _configuration.Setup(mocked => mocked.GetConfigValue("GroupConnectorRegistrationPageId")).Returns("GroupConnectorRegistrationPageId");
 
             _ministryPlatformService.Setup(
-              mocked => mocked.CreateSubRecord("GroupConnectorRegistrationPageId", groupConnectorId,It.IsAny<Dictionary<string, object>>(), "ABC", true))
+              mocked => mocked.CreateSubRecord("GroupConnectorRegistrationPageId", groupConnectorId, It.IsAny<Dictionary<string, object>>(), "ABC", true))
               .Returns(123);
+
+
 
             var groupConnectorRegistration = _fixture.CreateGroupConnectorRegistration(groupConnectorId, registrationId);
             Assert.AreEqual(123, groupConnectorRegistration);
 
             _ministryPlatformService.VerifyAll();
-            Assert.IsNotNull(groupConnectorRegistration);
-            Assert.AreEqual(9999, registrationId);
+            
+        }
+
+        [Test]
+        public void ShouldThrowApplicationExceptionWhenGroupConnectorRegistrationCreationFails()
+        {
+            Exception ex = new Exception("GroupConnecotrRegistration creation failed");
+            _ministryPlatformService.Setup(
+                mocked => mocked.CreateSubRecord("GroupConnectorRegistrationPageId",8888, It.IsAny<Dictionary<string, object>>(), "ABC", true))
+                .Throws(ex);
+
+            try
+            {
+                _fixture.CreateGroupConnectorRegistration(8888, 9999);
+                Assert.Fail("Expected exception was not thrown");
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOf(typeof(ApplicationException), e);
+                Assert.AreSame(ex, e.InnerException);
+            }
         }
 
     }
