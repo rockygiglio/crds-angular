37fa91bd1505027937e35b903c8e37ab2b01611d
andycanterbury <andrew.canterbury@ingagepartners.com>
2016-04-01 11:24:05 -0400

    DE1367 fixed children getting lost when coming back

--- crossroads.net/app/go_volunteer/page/childrenCount/goVolunteerChildrenCount.component.js
+++ crossroads.net/app/go_volunteer/page/childrenCount/goVolunteerChildrenCount.component.js
@@ -19,21 +19,19 @@
 
     function GoVolunteerChildrenCountController() {
       var vm = this;
-      vm.childTwoSeven = 0;
-      vm.childEightTwelve = 0;
-      vm.childThirteenEighteen = 0;
+      vm.childrenAttending = GoVolunteerService.childrenAttending;
       vm.submit = submit;
       vm.totalChildren = totalChildren;
 
       function submit() {
-        GoVolunteerService.childrenAttending.childTwoSeven = vm.childTwoSeven;
-        GoVolunteerService.childrenAttending.childEightTwelve = vm.childEightTwelve;
-        GoVolunteerService.childrenAttending.childThirteenEighteen = vm.childThirteenEighteen;
+        //GoVolunteerService.childrenAttending.childTwoSeven = vm.childTwoSeven;
+        //GoVolunteerService.childrenAttending.childEightTwelve = vm.childEightTwelve;
+        //GoVolunteerService.childrenAttending.childThirteenEighteen = vm.childThirteenEighteen;
         vm.onSubmit({nextState: 'group-connector'});
       }
 
       function totalChildren() {
-        return vm.childTwoSeven + vm.childEightTwelve + vm.childThirteenEighteen;
+        return vm.childrenAttending.childTwoSeven + vm.childrenAttending.childEightTwelve + vm.childrenAttending.childThirteenEighteen;
       }
     }
   }
