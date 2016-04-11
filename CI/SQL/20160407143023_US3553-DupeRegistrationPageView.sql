78fd0faddd7439ef3e080500408de348d5f58ffa
andycanterbury <andrew.canterbury@ingagepartners.com>
2016-04-11 11:48:44 -0400

    US3553

--- CI/SQL/20160407143023_US3553-DupeRegistrationPageView.sql
+++ CI/SQL/20160407143023_US3553-DupeRegistrationPageView.sql
@@ -19,5 +19,6 @@ BEGIN
            ,'Participant_ID_Table_Contact_ID_Table.[Display_Name], Participant_ID_Table_Contact_ID_Table.[Email_Address]
 , Initiative_ID_Table.[Initiative_Name], [dp_Created].[Date_Time] AS [Registration Date], Organization_ID_Table.[Name] AS [Organization]
 , Preferred_Launch_Site_ID_Table.[Location_Name] AS [Preferred Launch Site], cr_Registrations.[_Family_Count], cr_Registrations.[Cancelled]'
+           ,'ISNULL(cr_Registrations.Cancelled, 0) != 1 AND Participant_ID_Table_Contact_ID_Table.Email_Address IN (SELECT Email_Address FROM dbo.cr_Registrations AS R JOIN dbo.Participants AS P ON R.Participant_ID = P.Participant_ID JOIN dbo.Contacts AS C ON C.Contact_ID = P.Contact_ID GROUP BY Email_Address HAVING Count(1) > 1)')
 	SET IDENTITY_INSERT [dbo].[dp_Page_Views] OFF
 END
