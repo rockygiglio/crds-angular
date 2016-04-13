3455c1fc7f136b7e0bb4b70d1741c228dc138354
IngageMaddoxT <tony.maddox@ingagepartners.com>
2016-04-13 10:25:20 -0400

    linting changes

--- crossroads.net/app/go_volunteer/page/groupFindConnector/goVolunteerGroupFindConnector.component.js
+++ crossroads.net/app/go_volunteer/page/groupFindConnector/goVolunteerGroupFindConnector.component.js
@@ -62,8 +62,10 @@
           if ((group.projectMaximumVolunteers - group.volunteerCount) < 1) {
             return true;
           }
+
           var regCount = registrationCount();
-          if (group.absoluteMaximumVolunteers > group.projectMaximumVolunteers && regCount > (group.absoluteMaximumVolunteers - group.volunteerCount)) {
+          if (group.absoluteMaximumVolunteers > group.projectMaximumVolunteers &&
+            regCount > (group.absoluteMaximumVolunteers - group.volunteerCount)) {
             return true;
           }
         }
@@ -130,18 +132,23 @@
          * attribute 7042 = 14-18
          */
 
-        var youngest = _.reduce(GoVolunteerService.childrenOptions, function(curr, next){
+        var youngest = _.reduce(GoVolunteerService.childrenOptions, function(curr, next) {
           if (next.attributeId === 7040) {
             return 2;
           }
+
           if (next.attributeId === 7041 && curr > 8) {
             return 8;
           }
+
           if (next.attributeId === 7042 && curr > 13) {
             return 13;
           }
+
           return curr;
-        }, 18);
+        },
+
+        18);
         return youngest;
       }
     }
