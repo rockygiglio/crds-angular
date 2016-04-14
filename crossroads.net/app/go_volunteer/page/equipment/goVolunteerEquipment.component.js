c54d877a8ccfde1e8183cc879dd74e1c44037e45
Matt Silbernagel <silbermm@gmail.com>
2016-04-14 10:39:36 -0400

    DE1427 DE1431

--- crossroads.net/app/go_volunteer/page/equipment/goVolunteerEquipment.component.js
+++ crossroads.net/app/go_volunteer/page/equipment/goVolunteerEquipment.component.js
@@ -19,12 +19,23 @@
 
     function GoVolunteerEquipmentController() {
       var vm = this;
+
+      vm.service = GoVolunteerService;
+
       vm.addEquipment = addEquipment;
       vm.equipment = GoVolunteerService.availableEquipment;
       vm.otherField = false;
-      vm.otherEquipment = [];
+      vm.otherEquipment = GoVolunteerService.otherEquipment;
       vm.showOtherEquipment = showOtherEquipment;
       vm.submit = submit;
+
+      activate();
+
+      function activate() {
+        if (vm.otherEquipment.length > 0) {
+          vm.otherField = true;
+        }
+      }
 
       function addEquipment() {
         vm.otherEquipment.push({equipment: {name: null}});
@@ -34,13 +45,14 @@
         vm.otherField = !vm.otherField;
         if (vm.otherField && vm.otherEquipment.length === 0) {
           vm.addEquipment();
+        } else if (!vm.otherField) {
+          GoVolunteerService.otherEquipment = [];
+          vm.otherEquipment = GoVolunteerService.otherEquipment;
         }
       }
 
       function submit() {
         GoVolunteerService.equipment = _.where(vm.equipment, {checked: true});
-        GoVolunteerService.otherEquipment = vm.otherEquipment;
-
         vm.onSubmit({nextState: 'additional-info'});
       }
     }
