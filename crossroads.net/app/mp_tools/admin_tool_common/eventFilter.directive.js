e29d071d67ea5503d225b75f98bfa280e72be225
dtkocher <dtkocher@gmail.com>
2016-04-27 10:37:30 -0400

    Add event filter directive.

---
+++ crossroads.net/app/mp_tools/admin_tool_common/eventFilter.directive.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  module.exports = EventFilter;
+
+  EventFilter.$inject = ['$log', 'EventService'];
+
+  function EventFilter($log, EventService) {
+    return {
+      restrict: 'EA',
+      replace: true,
+      scope: {
+        label: '=',
+        event: '=',
+        events: '='
+      },
+      templateUrl: 'templates/eventFilter.html',
+    };
+  }
+})();
