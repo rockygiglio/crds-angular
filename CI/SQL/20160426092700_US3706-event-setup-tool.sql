6fab37bef3d6402a2af070e77da5addde47abd89
Jim Kriz <jim.kriz@ingagepartners.com>
2016-04-27 16:32:29 -0400

    US3706: Update view to include templates

--- CI/SQL/20160426092700_US3706-event-setup-tool.sql
+++ CI/SQL/20160426092700_US3706-event-setup-tool.sql
@@ -8,11 +8,14 @@ DECLARE @PageViewId INT = 2222;
 DECLARE @PageId INT = 308;
 DECLARE @PageViewTitle VARCHAR(50) = 'EventsBySite';
 DECLARE @FieldList VARCHAR(1000) = 'Events.[Event_ID] AS [Event ID], 
-	CONCAT(Events.[Event_Title], '' '', Events.[Event_Start_Date]) AS [Event Title], 
+	CASE COALESCE(Events.[Template], 0)
+	    WHEN 1 THEN Events.[Event_Title]
+		ELSE CONCAT(Events.[Event_Title], '' - '', Events.[Event_Start_Date])
+	END AS [Event Title],
 	Congregation_ID_Table.[Congregation_ID] AS [Congregation ID], 
 	Congregation_ID_Table.[Congregation_Name] AS [Congregation Name], 
 	COALESCE(Events.[Template], ''False'') AS [Template]';
-DECLARE @ViewClause VARCHAR(1000) = 'Events.Event_Start_Date >= GetDate() AND Events.Event_End_Date <= DATEADD(month,1,GetDate()) AND Events.Cancelled = 0';
+DECLARE @ViewClause VARCHAR(1000) = '(Events.Event_Start_Date >= GetDate() AND Events.Event_End_Date <= DATEADD(month,1,GetDate()) AND Events.Cancelled = 0) OR COALESCE(Events.[Template], 0) = 1';
 DECLARE @Description VARCHAR(1000) = 'API View';
 
 DELETE FROM [dbo].[dp_Page_Views] WHERE Page_View_ID = @PageViewId;
