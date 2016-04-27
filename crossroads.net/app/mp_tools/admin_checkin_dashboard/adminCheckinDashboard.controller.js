59f31d4a7e17fea786ed4257ec095b7df9104a96
dtkocher <dtkocher@gmail.com>
2016-04-27 11:18:17 -0400

    Add event calls and ability to select the event.

--- crossroads.net/app/mp_tools/admin_checkin_dashboard/adminCheckinDashboard.controller.js
+++ crossroads.net/app/mp_tools/admin_checkin_dashboard/adminCheckinDashboard.controller.js
@@ -3,25 +3,34 @@
 
   module.exports = AdminCheckinDashboardController;
 
-  AdminCheckinDashboardController.$inject = ['$log', 'AuthService', 'CRDS_TOOLS_CONSTANTS', 'AdminCheckinDashboardService'];
+  AdminCheckinDashboardController.$inject = ['$log', '$scope', 'AuthService', 'CRDS_TOOLS_CONSTANTS', 'AdminCheckinDashboardService'];
 
-  function AdminCheckinDashboardController($log, AuthService, CRDS_TOOLS_CONSTANTS, AdminCheckinDashboardService) {
+  function AdminCheckinDashboardController($log, $scope, AuthService, CRDS_TOOLS_CONSTANTS, AdminCheckinDashboardService) {
     var vm = this;
+    vm.site = {id: undefined};
+    vm.event = {id: undefined};
+    vm.events = [];
     vm.viewReady = false;
     vm.eventRooms = [];
+    vm.loadEvents = loadEvents;
 
     activate();
 
     function activate() {
-        AdminCheckinDashboardService.checkinDashboard.get({ eventId: 4515378},
-          function (data) {
-            debugger;
-            vm.eventRooms = data.rooms;
-            vm.viewReady = true;
-          },
-          function (error) {
-            setErrorState(error);
-        });
+      AdminCheckinDashboardService.checkinDashboard.get({ eventId: 4515378},
+        function (data) {
+          vm.eventRooms = data.rooms;
+          vm.viewReady = true;
+        },
+        function (error) {
+          setErrorState(error);
+      });
+    }
+
+    function loadEvents() {
+      EventService.eventsBySite.query({ site : vm.site.id, template: false }, function(data) {
+        vm.events = data;
+      });
     }
 
     vm.allowAdminAccess = function() {
