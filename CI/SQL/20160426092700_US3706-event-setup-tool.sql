d7a049c25a0471beebf5f37f9334426dbfcf29c0
dtkocher <dtkocher@gmail.com>
2016-04-29 13:23:39 -0400

    Fix the sql file to use location table and not congregation table.

--- CI/SQL/20160426092700_US3706-event-setup-tool.sql
+++ CI/SQL/20160426092700_US3706-event-setup-tool.sql
@@ -7,14 +7,14 @@ GO
 DECLARE @PageViewId INT = 2222;
 DECLARE @PageId INT = 308;
 DECLARE @PageViewTitle VARCHAR(50) = 'EventsBySite';
-DECLARE @FieldList VARCHAR(1000) = 'Events.[Event_ID] AS [Event ID], 
-	CASE COALESCE(Events.[Template], 0)
-	    WHEN 1 THEN Events.[Event_Title]
-		ELSE CONCAT(Events.[Event_Title], '' - '', Events.[Event_Start_Date])
-	END AS [Event Title],
-	Congregation_ID_Table.[Congregation_ID] AS [Congregation ID], 
-	Congregation_ID_Table.[Congregation_Name] AS [Congregation Name], 
-	COALESCE(Events.[Template], ''False'') AS [Template]';
+DECLARE @FieldList VARCHAR(1000) = 'Events.[Event_ID] AS [Event_ID],
+  CASE COALESCE(Events.[Template], 0)
+      WHEN 1 THEN Events.[Event_Title]
+    ELSE CONCAT(Events.[Event_Title], '' - '', Events.[Event_Start_Date])
+  END AS [Event Title],
+  Location_ID_Table.[Location_ID] AS [Location_ID],
+  Location_ID_Table.[Location_Name] AS [Location_Name],
+  COALESCE(Events.[Template], ''False'') AS [Template]';
 DECLARE @ViewClause VARCHAR(1000) = '(Events.Event_Start_Date >= GetDate() AND Events.Event_End_Date <= DATEADD(month,1,GetDate()) AND Events.Cancelled = 0) OR COALESCE(Events.[Template], 0) = 1';
 DECLARE @Description VARCHAR(1000) = 'API View';
 
