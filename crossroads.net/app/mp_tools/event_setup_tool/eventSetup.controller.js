c7298d62e2ad2408a5370f15202af616782e84ad
dtkocher <dtkocher@gmail.com>
2016-05-02 11:42:19 -0400

    Fix DE1464 Settup Button Doesn't go into processing state.

--- crossroads.net/app/mp_tools/event_setup_tool/eventSetup.controller.js
+++ crossroads.net/app/mp_tools/event_setup_tool/eventSetup.controller.js
@@ -26,6 +26,7 @@
     vm.eventsReady = false;
     vm.eventsLoading = true;
     vm.eventTemplatesLoading = true;
+    vm.saving = false;
 
     vm.allowAdminAccess = function() {
       return (AuthService.isAuthenticated() && AuthService.isAuthorized(CRDS_TOOLS_CONSTANTS.SECURITY_ROLES.KidsClubTools));
@@ -50,6 +51,7 @@
     }
 
     function setup() {
+      vm.saving = true;
       EventService.eventSetup.save({eventtemplateid: vm.template.id, eventid: vm.event.id},
         function(response) {
           $rootScope.$emit('notify', $rootScope.MESSAGES.eventUpdateSuccess);
