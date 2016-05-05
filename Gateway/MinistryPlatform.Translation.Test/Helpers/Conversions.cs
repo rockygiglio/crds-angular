1faa6dd012ea90236ab32b43d8f8b9307794dbf4
jpc11623 <john.cleaver@ingagepartners.com>
2016-05-05 12:46:44 -0400

    Unable to Copy Templates
    DE1465 - Updated utility function to remove foreach loop

--- Gateway/MinistryPlatform.Translation.Test/Helpers/Conversions.cs
+++ Gateway/MinistryPlatform.Translation.Test/Helpers/Conversions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MinistryPlatform.Translation.Extensions;
 
 namespace MinistryPlatform.Translation.Test.Helpers
 {
@@ -10,14 +11,15 @@ namespace MinistryPlatform.Translation.Test.Helpers
     {
         public static List<int> BuildIntArrayFromKeyValue(List<Dictionary<string, object>> dictionaryList, string key)
         {
-            List<int> keyValues = new List<int>();
+            //List<int> keyValues = new List<int>();
 
-            foreach (var item in dictionaryList)
-            {
-                keyValues.Add(int.Parse(item[key].ToString()));
-            }
+            //foreach (var item in dictionaryList)
+            //{
+            //    keyValues.Add(int.Parse(item[key].ToString()));
+            //}
 
-            return keyValues;
+            //return keyValues;
+            return dictionaryList.Select(item => item.ToInt(key)).ToList();
         }
     }
 }
