cf5a4e4e723a43352cc3da825afbdfc9ca24f72f
dtkocher <dtkocher@gmail.com>
2016-05-05 14:02:33 -0400

    Add views to get the Volunteers and save.

---
+++ CI/SQL/20160505115843_US3711-AddVolunteersToAPIGetRoomReservationPageView.sql
@@ -0,0 +1,8 @@
+USE [MinistryPlatform];
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Page_Views] WHERE [Page_View_ID] = 1108 AND [Field_List] LIKE '%Allow_Checkin%')
+BEGIN
+	UPDATE [dbo].[dp_Page_Views]
+	SET [Field_List] = CONCAT([Field_List], ', Event_Rooms.[Volunteers]')
+	WHERE [Page_View_ID] = 1108;
+END
