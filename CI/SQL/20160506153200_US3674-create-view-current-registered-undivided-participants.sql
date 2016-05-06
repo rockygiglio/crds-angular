0e2345f8764adda76dfe2d2130f6f216984b538f
Sara Seissiger <saraseissiger@gmail.com>
2016-05-06 13:29:16 -0400

    US3674 Ethnicity

--- CI/SQL/20160506153200_US3674-create-view-current-registered-undivided-participants.sql
+++ CI/SQL/20160506153200_US3674-create-view-current-registered-undivided-participants.sql
@@ -1,19 +1,6 @@
---SELECT Attribute_Name, C.First_Name  
---     FROM Contact_Attributes CA
---      INNER JOIN Attributes A ON A.Attribute_ID = CA.Attribute_ID	
---	  INNER JOIN Contacts C ON C.Contact_ID = CA.Contact_ID	
---     WHERE A.Attribute_ID = CA.Attribute_ID
---	   AND C.Contact_ID = CA.Contact_ID	
---	   AND A.Attribute_Type_ID = 20			
-		
-		--,STUFF((Attribute_Name  
-			 --FROM Contact_Attributes CA
-			 -- INNER JOIN Attributes A ON A.Attribute_ID = CA.Attribute_ID	
-			 -- INNER JOIN Contacts C ON C.Contact_ID = CA.Contact_ID	
-			 --WHERE A.Attribute_ID = CA.Attribute_ID
-			 --  AND C.Contact_ID = CA.Contact_ID	
-			 --  AND A.Attribute_Type_ID = 20
-			 --FOR XML PATH('''')), 1, 1, '''') AS [Ethnicity]	
+--********************************************************
+--Add start_date and end_date to attribute sub-queries
+--********************************************************
 
 USE [MinistryPlatform]	
 GO
@@ -26,6 +13,16 @@ VALUES(92145,'Undivided - Registered Participants',316,'Staff members can view t
 		,Participant_ID_Table_Contact_ID_Table.[First_Name]
 		,Participant_ID_Table_Contact_ID_Table.[Last_Name]
 		,Participant_ID_Table_Contact_ID_Table_Gender_ID_Table.[Gender]
+
+		,(SELECT TOP 1 Attribute_Name 
+     FROM Contact_Attributes CA
+      INNER JOIN Attributes A ON A.Attribute_ID = CA.Attribute_ID	
+	  INNER JOIN Contacts C ON C.Contact_ID = CA.Contact_ID	
+     WHERE A.Attribute_ID = CA.Attribute_ID
+	   AND C.Contact_ID = CA.Contact_ID	
+	   AND CA.Contact_ID = Participant_ID_Table_Contact_ID_Table.[Contact_ID]
+	   AND A.Attribute_Type_ID = 20)  AS [Ethnicity]
+
 		,DATEDIFF(hour,Participant_ID_Table_Contact_ID_Table.[Date_of_Birth],GETDATE())/8766 AS [AGE]
 		,(SELECT Attribute_Name 
 		 FROM Group_Participant_Attributes GPA, Attributes A 
