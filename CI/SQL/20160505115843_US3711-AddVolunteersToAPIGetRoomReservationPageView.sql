20fb75ea8ea0d684e64638ec843240e4fb8c5f41
dtkocher <dtkocher@gmail.com>
2016-05-06 10:53:33 -0400

    Fix script.

--- CI/SQL/20160505115843_US3711-AddVolunteersToAPIGetRoomReservationPageView.sql
+++ CI/SQL/20160505115843_US3711-AddVolunteersToAPIGetRoomReservationPageView.sql
@@ -1,6 +1,6 @@
 USE [MinistryPlatform];
 
-IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Page_Views] WHERE [Page_View_ID] = 1108 AND [Field_List] LIKE '%Allow_Checkin%')
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Page_Views] WHERE [Page_View_ID] = 1108 AND [Field_List] LIKE '%Volunteers%')
 BEGIN
 	UPDATE [dbo].[dp_Page_Views]
 	SET [Field_List] = CONCAT([Field_List], ', Event_Rooms.[Volunteers]')
