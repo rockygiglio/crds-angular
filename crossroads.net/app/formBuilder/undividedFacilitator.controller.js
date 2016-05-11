a1eb83a0ce304313ff0d77fd79cda1526761d05a
Charlie Retzler <charlie.retzler@ingagepartners.com>
2016-05-11 11:18:25 -0400

    US3669: Saving co-facilitator

--- crossroads.net/app/formBuilder/undividedFacilitator.controller.js
+++ crossroads.net/app/formBuilder/undividedFacilitator.controller.js
@@ -7,21 +7,42 @@
 
   function UndividedFacilitatorCtrl($rootScope, Group, Session) {
     var vm = this;
+    
+    var constants = require('crds-constants');
 
     vm.saving = false;
     vm.save = save;
+    // TODO: Make this a constant
     vm.groupId = 166572;
-    vm.groupRole = require('crds-constants').GROUP.ROLES;
+
     vm.responses = {};
 
     function save() {
       vm.save = true;
 
+      debugger;
+
+      // TODO: Determine way to remove this hardcoded value here
+      var coFacilitator = vm.responses[167];
+
+      if (coFacilitator && coFacilitator !== '') {
+
+        var item = {
+          attribute: {
+            attributeId: constants.ATTRIBUTE_IDS.COFACILITATOR
+          },
+          notes: coFacilitator,
+        };
+
+        // TODO: Revisit this in case save fails since we are adding stuff to model directly
+        // consider cloning before doing this, so it's clean for the next save
+        vm.responses.singleAttributes[constants.ATTRIBUTE_TYPE_IDS.COFACILITATOR] = item;
+      }
+
       var participant = [{
         capacity: 1,
         contactId: parseInt(Session.exists('userId')),
-        groupRoleId: vm.groupRole.LEADER,
-
+        groupRoleId: constants.GROUP.ROLES.LEADER,
         childCareNeeded: vm.responses.Childcare,
         sendConfirmationEmail: false,
         singleAttributes: vm.responses.singleAttributes,
