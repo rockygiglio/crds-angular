d0cfdafb5d3b29d44f7226c1731a661fc55bbca8
Jim Kriz <jim.kriz@ingagepartners.com>
2016-05-12 10:28:46 -0400

    US3928: Additional documentation

--- Gateway/MinistryPlatform.Translation/Extensions/RestSharpExtensions.cs
+++ Gateway/MinistryPlatform.Translation/Extensions/RestSharpExtensions.cs
@@ -6,15 +6,36 @@ namespace MinistryPlatform.Translation.Extensions
 {
     public static class RestSharpExtensions
     {
+        /// <summary>
+        /// Determine if this response is an error.  It looks at the ResponseStatus and the HTTP StatusCode to make the determination.
+        /// </summary>
+        /// <param name="response">The response to check</param>
+        /// <param name="errorNotFound">Indicates if a 404 should be considered an error</param>
+        /// <returns></returns>
         public static bool IsError(this IRestResponse response, bool errorNotFound = false)
         {
-            return (response.ResponseStatus != ResponseStatus.Completed
-                    || (errorNotFound && response.StatusCode == HttpStatusCode.NotFound)
-                    || response.StatusCode == HttpStatusCode.Unauthorized
-                    || response.StatusCode == HttpStatusCode.BadRequest
-                    || response.StatusCode == HttpStatusCode.PaymentRequired);
+            // If the request is not completed, this is an error
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                return true;
+            }
+
+            // If we got a 404, and we're considering that an error, then it's an error
+            if (response.StatusCode == HttpStatusCode.NotFound && errorNotFound)
+            {
+                return true;
+            }
+
+            // If we have a bad response code, then it's an error
+            return (int) response.StatusCode < 200 || (int) response.StatusCode > 399;
         }
 
+        /// <summary>
+        /// Checks for error response, and throws a RestResponseException if the response is in error.
+        /// </summary>
+        /// <param name="response">The response to check</param>
+        /// <param name="errorMessage">The error message to include in the exception, if error</param>
+        /// <param name="errorNotFound">Indicates if a 404 should be considered an error</param>
         public static void CheckForErrors(this IRestResponse response, string errorMessage, bool errorNotFound = false)
         {
             if (!IsError(response, errorNotFound))
