2e6d52c0fd2faa8001845967838fda3f89780cb3
lknair2015 <lknair2015@gmail.com>
2016-05-12 15:39:20 -0400

    Updated the cr_Serve_Restrictions_Reasons alter query

--- CI/SQL/20160511043956_DE1312-missing-domain-entry-in-table-definitions.sql
+++ CI/SQL/20160511043956_DE1312-missing-domain-entry-in-table-definitions.sql
@@ -8,8 +8,9 @@ BEGIN
 END
 
 IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Destinations_Domains]') AND parent_object_id = OBJECT_ID(N'[dbo].[cr_Onboarding_Statuses]'))
+
 ALTER TABLE [dbo].[cr_Destinations]  WITH CHECK ADD  CONSTRAINT [FK_Destinations] FOREIGN KEY([Domain_ID])
-REFERENCES [dbo].[dp_Domains] ([Domain_ID])
+REFERENCES [dbo].[dp_Domains] ([Domain_ID]);
 GO
 
 IF NOT EXISTS(SELECT * FROM sys.columns WHERE Name = N'Domain_ID' AND Object_ID = Object_ID(N'cr_Onboarding_Statuses'))
@@ -68,14 +69,14 @@ ALTER TABLE [dbo].[cr_Serve_Restrictions_Reasons]  WITH CHECK ADD  CONSTRAINT [F
 REFERENCES [dbo].[dp_Domains] ([Domain_ID])
 GO
 
-IF NOT EXISTS(SELECT * FROM sys.columns WHERE Name = N'Domain_ID' AND Object_ID = Object_ID(N'cr_Serve_Restrictions_Reasons'))
+IF NOT EXISTS(SELECT * FROM sys.columns WHERE Name = N'Domain_ID' AND Object_ID = Object_ID(N'cr_Serve_Restrictions_Reasons_Domain'))
 BEGIN
 	ALTER TABLE [dbo].[cr_Serve_Restrictions_Reasons]
 	ADD Domain_ID [int] NOT NULL DEFAULT(1)
 END
 
-IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Serve_Restriction_Reasons_Domains]') AND parent_object_id = OBJECT_ID(N'[dbo].[cr_Serve_Restrictions_Reasons]'))
-ALTER TABLE [dbo].[cr_Serve_Restrictions_Reasons]  WITH CHECK ADD  CONSTRAINT [FK_Serve_Restrictions_Reasons] FOREIGN KEY([Domain_ID])
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Serve_Restriction_Reasons_Domain_Domains]') AND parent_object_id = OBJECT_ID(N'[dbo].[cr_Serve_Restrictions_Reasons]'))
+ALTER TABLE [dbo].[cr_Serve_Restrictions_Reasons]  WITH CHECK ADD  CONSTRAINT [FK_Serve_Restrictions_Reasons_Domain] FOREIGN KEY([Domain_ID])
 REFERENCES [dbo].[dp_Domains] ([Domain_ID])
 GO
 
