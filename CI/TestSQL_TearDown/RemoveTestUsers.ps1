42873567760678feb4d0d925b2f6483ea58db7c6
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-05-19 15:32:09 -0400

    Still not working.

--- CI/TestSQL_TearDown/RemoveTestUsers.ps1
+++ CI/TestSQL_TearDown/RemoveTestUsers.ps1
@@ -15,9 +15,10 @@ param (
 {
 	if(![string]::IsNullOrEmpty($user.email))
 	{
+        $email = $user.email
 		write-host "Removing User" $user.first $user.last "with email" $user.email;
-		write-host "Debug: " $SQLcmd @SQLCommonParams -Q "EXEC [MinistryPlatform].[dbo].[cr_QADeleteData] `$em -v em = '$email'"
-		$output = & $SQLcmd @SQLCommonParams -Q "EXEC [MinistryPlatform].[dbo].[cr_QADeleteData] `$em -v em = '$email'"
+		write-host "Debug: " $SQLcmd @SQLCommonParams -Q """EXEC [MinistryPlatform].[dbo].[cr_QADeleteData] '$email'"""
+		$output = & $SQLcmd @SQLCommonParams -Q """EXEC [MinistryPlatform].[dbo].[cr_QADeleteData] '$email'"""
 		
 		if($LASTEXITCODE -ne 0){
 				write-host "User: "$user.email
