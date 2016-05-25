d1867ab2ed379433505794b5e12b26ccdd11afb5
Charlie Retzler <charlie.retzler@ingagepartners.com>
2016-05-25 19:41:10 -0400

    US4001: Fixing issue from merging development

--- crossroads.net/app/formBuilder/formBuilder.controller.js
+++ crossroads.net/app/formBuilder/formBuilder.controller.js
@@ -112,10 +112,6 @@
       return (page && page.fields && page.fields.length > 1);
     }
 
-    function hasForm() {
-      return (vm.page && vm.page.fields && vm.page.fields.length > 1);
-    }
-
     function save() {
       vm.saving = true;
       try {
