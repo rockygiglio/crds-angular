a17fe8335fe028d7e9f938b9c54685b3d90022f5
lknair2015 <lknair2015@gmail.com>
2016-05-26 10:08:43 -0400

    US3854 NEW CHILDCARE REQUEST NOTIFICATION
    adding files

---
+++ CI/SQL/20160525170133_US3854-create-pageview-for-emailnotification.sql
@@ -0,0 +1,32 @@
+USE [MinistryPlatform]
+GO
+
+SET IDENTITY_INSERT [dp_Page_Views] ON
+
+INSERT INTO [dbo].[dp_Page_Views]
+           ([Page_View_ID]
+		   ,[View_Title]
+           ,[Page_ID]
+           ,[Description]
+           ,[Field_List]
+           ,[View_Clause]
+           ,[Order_By])
+     VALUES
+           (2300
+		   ,'Email Page View'
+           ,36
+           ,'Columns for Email Notification'
+           ,'cr_Childcare_Requests.[Childcare_Request_ID]
+				, Requester_ID_Table.[Contact_ID]
+				, Requester_ID_Table.[Email_Address]
+				, Requester_ID_Table.[Display_Name]
+				, Ministry_ID_Table.[Ministry_Name]
+				, Congregation_ID_Table.[Congregation_Name]
+				, cr_Childcare_Requests.[Start_Date]
+				, cr_Childcare_Requests.[End_Date]
+				, cr_Childcare_Requests.[Childcare_Session]'
+           ,'cr_Childcare_Requests.[Childcare_Request_ID] > 0'
+           ,null)
+GO
+
+
---
+++ Gateway/MinistryPlatform.Translation/Models/Childcare/ChildcareRequestEmail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinistryPlatform.Translation.Models.Childcare
+{
+    public class ChildcareRequestEmail
+    {
+
+        public int RequesterId { get; set; }
+        public String RequesterEmail { get; set; }
+        public String MinistryName { get; set; }
+        public String GroupName{ get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Frequency { get; set; }
+        public string ChildcareSession { get; set; }
+        public int EstimatedChildren { get; set; }
+        public int CongregationId { get; set; }
+        public string Requester { get; set; }
+       
+
+    }
+}
+    
