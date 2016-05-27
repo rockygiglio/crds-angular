c916664819575d586d7cc44e9f4066e370b8c1a4
Sandi Ritter <sandi.ritter@ingagepartners.com>
2016-05-26 20:43:40 -0400

    US4001 update page views, not insert

--- CI/SQL/20160523110638_US4001-RefactorUndividedRegisteredParticipantsView.sql
+++ CI/SQL/20160523110638_US4001-RefactorUndividedRegisteredParticipantsView.sql
@@ -1,11 +1,8 @@
 USE [MinistryPlatform]	
 GO
 
-SET IDENTITY_INSERT [dbo].[dp_Page_Views] ON
-
-INSERT INTO [dp_page_views]([Page_View_ID],[View_Title],[Page_ID],[Description],[Field_List],[View_Clause],[Order_By],[User_ID],[User_Group_ID])
-VALUES(92145,'Undivided - Registered Participants',316,'Staff members can view the users that have registered to be an Undivided Participants.'
-	,'Group_Participants.[Start_Date] AS [Registration_Date] 
+UPDATE [dp_page_views]
+SET [Field_List] = 	'Group_Participants.[Start_Date] AS [Registration_Date] 
 	,Participant_ID_Table_Contact_ID_Table.[First_Name]
 	,Participant_ID_Table_Contact_ID_Table.[Last_Name]
 	,Participant_ID_Table_Contact_ID_Table_Gender_ID_Table.[Gender]
@@ -27,9 +24,8 @@ VALUES(92145,'Undivided - Registered Participants',316,'Staff members can view t
 	  AND GETDATE() BETWEEN GPA.Start_Date AND ISNULL(GPA.End_Date,GETDATE()) 
 	  AND Attribute_Type_ID = 88) AS [Preferred_Co-Participant]	
 	,Group_Participants.[Child_Care_Requested] AS [Requested_Child_Care]'
-    ,'Group_ID_Table_Group_Type_ID_Table.Group_Type_ID = 26 AND Group_Role_ID_Table.Group_Role_ID = 16 AND
-     (Group_Participants.End_Date > GetDate() OR Group_Participants.End_Date IS NULL) AND (Group_ID_Table.End_Date > GetDate() OR Group_ID_Table.End_Date IS NULL)'
-    ,'Group_Participants.[Start_Date]' 
-    ,NULL
-    ,NULL )
-SET IDENTITY_INSERT [dbo].[dp_Page_Views] OFF
+,[View_Clause] ='Group_ID_Table_Group_Type_ID_Table.Group_Type_ID = 26 AND Group_Role_ID_Table.Group_Role_ID = 16 AND
+     (Group_Participants.End_Date > GetDate() OR Group_Participants.End_Date IS NULL) 
+		 AND (Group_ID_Table.End_Date > GetDate() OR Group_ID_Table.End_Date IS NULL)'
+		
+WHERE Page_View_ID = '92145'			
