d713537c24ddbb773d6d22e1aa6c629bb252cfbb
lknair2015 <lknair2015@gmail.com>
2016-06-03 15:20:42 -0400

    display the group

--- crossroads.net/app/mp_tools/childcare_decision/childcareDecision.controller.js
+++ crossroads.net/app/mp_tools/childcare_decision/childcareDecision.controller.js
@@ -1,13 +1,35 @@
 class ChildcareDecisionController {
     /*@ngInject*/
     constructor(
+        ChildcareDecisionService,
         MPTools,
         CRDS_TOOLS_CONSTANTS
     ) {
         this.mptools = MPTools;
         this.allowAccess = MPTools.allowAccess(CRDS_TOOLS_CONSTANTS.SECURITY_ROLES.ChildcareDecisionTool);
+        this.decisionService = ChildcareDecisionService;
         this.name = 'childcare-decision';
         this.viewReady = true;
+        this.getChildcareRequest = ChildcareDecisionService.getChildcareRequest();
+        this.requestId = Number(MPTools.getParams().recordId);
+        this.requestData = {};
+        this.group = '';
+        this.startDate = '';
+        this.endDate = '';
+        this.childcareSession = '';
+        getChildcareRequest();
+
+
+        getChildcareRequest()
+        {
+            getChildcareRequest.get({requestId}, function (q) {
+                requestData = q;
+                group = requestData.GroupName;
+                startDate = requestData.StartDate;
+                endDate = requestData.EndDate;
+                childcareSession = requestData.ChildcareSession;
+            });
+        }
     }
 }
 export default ChildcareDecisionController;
