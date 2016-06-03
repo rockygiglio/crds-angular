2d6ee5d51c5ff1a20fddf21b4afd367d32bc8a08
andycanterbury <andrew.canterbury@ingagepartners.com>
2016-06-03 14:25:25 -0400

    US4056 cleaned up validator that wasn't working or used.

--- crossroads.net/app/mp_tools/request_childcare/index.js
+++ crossroads.net/app/mp_tools/request_childcare/index.js
@@ -1,11 +1,9 @@
 import requestChildcareComponent from './requestChildcare.component';
 import RequestChildcareService from './requestChildcare.service';
-import endTimeValidation from './sessionTime.validation.directive';
 import CONSTANTS from 'crds-constants';
 
 angular.module(CONSTANTS.MODULES.MPTOOLS)
     .directive('requestChildcare', requestChildcareComponent)
-    .directive('endTime', endTimeValidation)
     .service('RequestChildcareService', RequestChildcareService)
     ;
 
