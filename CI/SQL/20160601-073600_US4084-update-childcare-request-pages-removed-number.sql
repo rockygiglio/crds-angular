45c0a12a68a44a8769bab3ece4aeef3ae881d27c
Matt Silbernagel <silbermm@ucmail.uc.edu>
2016-06-06 12:25:26 -0400

    Update 20160601-073600_US4084-update-childcare-request-pages-removed-number.sql

--- CI/SQL/20160601-073600_US4084-update-childcare-request-pages-removed-number.sql
+++ CI/SQL/20160601-073600_US4084-update-childcare-request-pages-removed-number.sql
@@ -2,7 +2,7 @@ USE MinistryPlatform
 GO
 
 -- update My Childcare requests
-UPDATE dp_pages SET Default_Field_List='Group_ID_Table.[Group_Name] AS [Group]  ,cr_Childcare_Requests.[Start_Date] AS [Start Date]  ,cr_Childcare_Requests.[End_Date] AS [End Date] ,Request_Status_ID_Table.[Request_Status] AS [Request Status]'
+UPDATE dp_pages SET Default_Field_List='Group_ID_Table.[Group_Name] AS [Group]  ,cr_Childcare_Requests.[Start_Date],cr_Childcare_Requests.[End_Date],Request_Status_ID_Table.[Request_Status]'
        WHERE page_id=555;
 
 -- update childcare requests
@@ -10,9 +10,9 @@ UPDATE dp_pages SET Default_Field_List='Requester_ID_Table.[Display_Name] AS [Re
                                         Group_ID_Table.[Group_Name] AS [Group], 
 										Ministry_ID_Table.[Ministry_Name] AS [Ministry],
 										Congregation_ID_Table.[Congregation_Name] AS [Congregation],
-										cr_Childcare_Requests.[Start_Date] AS [Start Date],
-										cr_Childcare_Requests.[End_Date] AS [End Date],
-										cr_Childcare_Requests.[Frequency] AS [Frequency],
-										cr_Childcare_Requests.[Childcare_Session] AS [Childcare Session],
+										cr_Childcare_Requests.[Start_Date],
+										cr_Childcare_Requests.[End_Date],
+										cr_Childcare_Requests.[Frequency],
+										cr_Childcare_Requests.[Childcare_Session],
 										cr_Childcare_Requests.[Notes] AS [Notes],
-										Request_Status_ID_Table.[Request_Status] AS [Request Status]' WHERE page_id=36;
+										Request_Status_ID_Table.[Request_Status]' WHERE page_id=36;
