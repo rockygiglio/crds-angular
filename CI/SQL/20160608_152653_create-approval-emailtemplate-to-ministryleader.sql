ccf104f36a4b199360a47c60172364c760b6e0e4
lknair2015 <lknair2015@gmail.com>
2016-06-08 15:31:52 -0400

    Created new approval email template

---
+++ CI/SQL/20160608_152653_create-approval-emailtemplate-to-ministryleader.sql
@@ -0,0 +1,34 @@
+USE [MinistryPlatform]
+GO
+
+SET IDENTITY_INSERT dp_Communications ON;
+
+INSERT INTO [dbo].[dp_Communications]
+           (
+		    [Communication_ID],
+			[Author_User_ID],
+			[Subject],
+			[Body],
+			[Domain_ID],
+			[Start_Date],
+			[Communication_Status_ID],
+			[From_Contact],
+			[Reply_to_Contact],
+			[Template],
+			[Active]
+		  )
+     VALUES
+           (
+		    5600,
+			5,
+			'Your Childcare Request has been approved',
+			'<span style="font-family: ''Helvetica Neue'', Helvetica, Arial, sans-serif; font-size: 13px; background-color: rgb(255, 255, 255);">Your request for Childcare for [Group] at [Site] has been approved for the following dates [Dates]. Please click on the link below to review.</span><div><a href="https://[Base_Url]/ministryplatform#/36/[RequestId]" target="_self">Approved-Childcare-Request</a><font face="Helvetica Neue, Helvetica, Arial, sans-serif"><br /></font><div><br /></div></div>',
+			1,
+			Getdate(),
+			1,
+			7542465,
+			7542465,
+			1,
+			1
+		   )
+GO
