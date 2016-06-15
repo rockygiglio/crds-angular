bd8c3dd19e95fcb9cef521cf161230254bbf2816
Sandi Ritter <sandi.ritter@ingagepartners.com>
2016-06-15 16:03:22 -0400

    Us4143 pushing it REAL good

--- Gateway/MinistryPlatform.Translation/Services/Interfaces/IOpportunityService.cs
+++ Gateway/MinistryPlatform.Translation/Services/Interfaces/IOpportunityService.cs
@@ -16,23 +16,13 @@ namespace MinistryPlatform.Translation.Services.Interfaces
         DateTime GetLastOpportunityDate(int opportunityId, string token);
         int DeleteResponseToOpportunities(int participantId, int opportunityId, int eventId);
         int RespondToOpportunity(string token, int opportunityId, string comments);
-<<<<<<< HEAD
         Response GetMyOpportunityResponses(int contactId, int opportunityId);
         Response GetOpportunityResponse(int contactId, int opportunityId);
         Response GetOpportunityResponse(int opportunityId, int eventId, Participant participant);
-        List<MPResponse> SearchResponseByGroupAndEvent(String searchString);
-        List<MPResponse> GetContactsOpportunityResponseByGroupAndEvent(int groupId, int eventId);
-        List<Response> GetOpportunityResponses(int opportunityId, string token);
-        void RespondToOpportunity(RespondToOpportunityDto opportunityResponse);
-=======
-        MpResponse GetMyOpportunityResponses(int contactId, int opportunityId);
-        MpResponse GetOpportunityResponse(int contactId, int opportunityId);
-        MpResponse GetOpportunityResponse(int opportunityId, int eventId, Participant participant);
         List<MpResponse> SearchResponseByGroupAndEvent(String searchString);
         List<MpResponse> GetContactsOpportunityResponseByGroupAndEvent(int groupId, int eventId);
-        List<MpResponse> GetOpportunityResponses(int opportunityId, string token);
+        List<Response> GetOpportunityResponses(int opportunityId, string token);
         void RespondToOpportunity(MpRespondToOpportunityDto opportunityResponse);
->>>>>>> 5dadf78e63ddb619d5ef18503bf17205c6624abd
         int RespondToOpportunity(int participantId, int opportunityId, string comments, int eventId, bool response);
     }
 }
