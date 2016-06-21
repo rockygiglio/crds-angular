e56af506ecc41ecb847dda6c14239a27605821e8
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-06-21 15:18:40 -0400

    Dynamically set processorId based on environment

--- CI/TestSQL/01.TestUsers/QATEST_ACHUserAccount.sql
+++ CI/TestSQL/01.TestUsers/QATEST_ACHUserAccount.sql
@@ -2,13 +2,20 @@
 USE [MinistryPlatform]
 GO
 
+DECLARE @processorID as varchar(255);
+
+IF (SELECT URL from DP_Bookmarks where name = 'crossroads.net') like '%demo%'
+	SET @processorID = 'cus_6cAhwmmKyaw9D5';
+ELSE
+	SET @processorID = 'cus_8gBB5FUZFB9b0n';
+
 --Mpcrds+32@gmail.com contact record
 DECLARE @contactID as int
 set @contactID = (select contact_id from contacts where Email_Address = 'mpcrds+32@gmail.com' and Last_Name = 'Fry');
 
 INSERT INTO [dbo].donors 
-(Contact_ID,Statement_Frequency_ID,Statement_Type_ID,Statement_Method_ID,Setup_Date                ,Envelope_No,Cancel_Envelopes,Notes,First_Contact_Made,Domain_ID,__ExternalPersonID,_First_Donation_Date,_Last_Donation_Date,Processor_ID        ) VALUES
-(@contactID,1                     ,1                ,2                  ,{ts '2015-07-15 16:19:22'},null       ,0               ,null ,null              ,1        ,null              ,null                ,null               ,'cus_6cAhwmmKyaw9D5');
+(Contact_ID,Statement_Frequency_ID,Statement_Type_ID,Statement_Method_ID,Setup_Date                ,Envelope_No,Cancel_Envelopes,Notes,First_Contact_Made,Domain_ID,__ExternalPersonID,_First_Donation_Date,_Last_Donation_Date,Processor_ID) VALUES
+(@contactID,1                     ,1                ,2                  ,{ts '2015-07-15 16:19:22'},null       ,0               ,null ,null              ,1        ,null              ,null                ,null               ,@processorID);
 
 DECLARE @donor_id as int
 set @donor_id = (Select donor_ID from donors where contact_id = @contactID);
