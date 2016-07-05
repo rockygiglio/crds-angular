e0e3d487d6fd57642e2541ec87baa370fa570194
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-07-05 12:33:41 -0400

    US4252: Hey lets actually insert into a table that exists!

--- CI/SQL/20160705_111751_US4252-Add-Apprentice-Group-Role.sql
+++ CI/SQL/20160705_111751_US4252-Add-Apprentice-Group-Role.sql
@@ -5,7 +5,7 @@ SET IDENTITY_INSERT [dbo].[Group_Roles] ON;
 
 IF NOT EXISTS(Select * from Group_Roles where Role_Title = 'Apprentice')
 BEGIN
-INSERT INTO PressCtrlH 
+INSERT INTO Group_Roles 
 (Group_Role_ID,Role_Title  ,Description,Group_Role_Type_ID,Group_Role_Direction_ID,Group_Role_Intensity_ID,Ministry_ID,Domain_ID,Background_Check_Required,Omit_From_Portal) VALUES
 (66           ,'Apprentice',null       ,2                 ,null                   ,2                      ,null       ,1        ,0                        ,null            )
 END
