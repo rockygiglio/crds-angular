860db9f4d44ddf56490a2ef24fd0d5c539fd2ece
Jim Kriz <jim.kriz@ingagepartners.com>
2016-07-07 17:05:43 -0400

    US4263: Group Participants page HTML mock

--- crossroads.net/app/group_tool/group_detail/participants/groupDetail.participants.controller.js
+++ crossroads.net/app/group_tool/group_detail/participants/groupDetail.participants.controller.js
@@ -1,5 +1,45 @@
 export default class GroupDetailParticipantsController {
   /*@ngInject*/
-  constructor() {
+  constructor(Group, ImageService, $state) {
+    this.groupService = Group;
+    this.imageService = ImageService;
+    this.state = $state;
+
+    this.groupId = this.state.params.groupId;
+    this.ready = false;
+    this.error = false;
+    this.currentView = 'List';
+  }
+
+  $onInit() {
+    var imageBaseUrl = this.imageService.ProfileImageBaseURL;
+    this.groupService.getGroupParticipants(this.groupId).then((data) => {
+      this.data = data;
+      _.forEach(this.data.participants, function(participant) {
+          participant.imageUrl = `${imageBaseUrl}${participant.contactId}`;
+      });
+      this.ready = true;
+    },
+    (err) => {
+      this.log.error(`Unable to get group participants: ${err.status} - ${err.statusText}`);
+      this.error = true;
+      this.ready = true;
+    });
+  }
+
+  setView(newView) {
+      this.currentView = newView;
+  }
+
+  deleteParticipants() {
+    _.remove(this.data.participants, function(participant) {
+        return participant.deleted === true;
+    });
+    this.currentView = 'List';  
+  }
+
+  undeleteParticipants() {
+    this.data.participants.map(p => p.deleted = false);
+    this.currentView = 'List';  
   }
 }
---
+++ crossroads.net/app/group_tool/group_detail/participants/participant_card/groupDetail.participant.card.component.js
@@ -0,0 +1,21 @@
+import controller from './groupDetail.participant.card.controller';
+
+GroupDetailParticipantCardComponent.$inject = [];
+
+export default function GroupDetailParticipantCardComponent() {
+
+  let groupDetailParticipantCardComponent = {
+    bindings: {
+      participant: '<',
+      edit: '<'
+    },
+    restrict: 'E',
+    templateUrl: 'participant_card/groupDetail.participant.card.html',
+    controller: controller,
+    controllerAs: 'groupDetailParticipantCard',
+    bindToController: true
+  };
+
+  return groupDetailParticipantCardComponent;
+
+}
