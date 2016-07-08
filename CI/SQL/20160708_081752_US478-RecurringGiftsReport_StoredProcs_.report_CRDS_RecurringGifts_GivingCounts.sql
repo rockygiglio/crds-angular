506107d432bd547806dd15b5cd514fd0ed449d84
Katie Dwyer <Katie Dwyer>
2016-07-08 10:34:26 -0400

    Changing join to inner join to make Charlie happy

--- CI/SQL/20160708_081752_US478-RecurringGiftsReport_StoredProcs_.report_CRDS_RecurringGifts_GivingCounts.sql
+++ CI/SQL/20160708_081752_US478-RecurringGiftsReport_StoredProcs_.report_CRDS_RecurringGifts_GivingCounts.sql
@@ -16,18 +16,18 @@ As Begin
 
 select 'Unique Givers' as type,count(distinct d.Donor_ID) num_of_gifts
 		from dbo.Recurring_Gifts rg
-		join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID
-		join dbo.Programs p on rg.Program_ID = p.Program_ID
-		join dbo.Donors d on rg.Donor_ID = d.Donor_ID
+		inner join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID
+		inner join dbo.Programs p on rg.Program_ID = p.Program_ID
+		inner join dbo.Donors d on rg.Donor_ID = d.Donor_ID
 		where 
 		  rg.end_date is null
 		union all
 --gives total OVERALL givers 
 		select 'Total Givers Setup' as type,count(d.Donor_ID) num_of_gifts 
 		from dbo.Recurring_Gifts rg
-		join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID
-		join dbo.Programs p on rg.Program_ID = p.Program_ID
-		join dbo.Donors d on rg.Donor_ID = d.Donor_ID
+		inner join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID
+		inner join dbo.Programs p on rg.Program_ID = p.Program_ID
+		inner join dbo.Donors d on rg.Donor_ID = d.Donor_ID
 		where 
 		 rg.end_date is null
 
--- CI/SQL/20160708_082352_US478-RecurringGiftsReport_StoredProcs_.report_CRDS_RecurringGifts_GivingII.sql
+++ CI/SQL/20160708_082352_US478-RecurringGiftsReport_StoredProcs_.report_CRDS_RecurringGifts_GivingII.sql
@@ -17,9 +17,9 @@ As Begin
 
 select 'MONTHLY II' as recurrance,count(d.Donor_ID) num_of_gifts, sum(rg.amount) gifts_for_II, sum(rg.amount * 1) total_monthly_II_expected, sum(rg.amount * 12) total_yearly_II_expected
 		from dbo.Recurring_Gifts rg
-		join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID 
-		join dbo.Programs p on rg.Program_ID = p.Program_ID 
-		join dbo.Donors d on rg.Donor_ID = d.Donor_ID
+		inner join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID 
+		inner join dbo.Programs p on rg.Program_ID = p.Program_ID 
+		inner join dbo.Donors d on rg.Donor_ID = d.Donor_ID
 		where 	
 		  rgf.Frequency_ID = 2 --monthly
 		  and p.Program_ID = 146 --General Giving
@@ -27,9 +27,9 @@ select 'MONTHLY II' as recurrance,count(d.Donor_ID) num_of_gifts, sum(rg.amount)
 		union all 
 		select 'WEEKLY II' as recurrance,count(d.Donor_ID) num_of_gifts, sum(rg.amount) gifts_for_II,sum(rg.amount *52/12) total_monthly_II_expected, sum(rg.amount * 52) total_yearly_II_expected
 		from dbo.Recurring_Gifts rg
-		join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID
-		join dbo.Programs p on rg.Program_ID = p.Program_ID
-		join dbo.Donors d on  rg.Donor_ID = d.Donor_ID
+		inner join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID
+		inner join dbo.Programs p on rg.Program_ID = p.Program_ID
+		inner join dbo.Donors d on  rg.Donor_ID = d.Donor_ID
 		where 
 		 rgf.Frequency_ID = 1 --weekly
 		 and p.Program_ID = 146 --General Giving
