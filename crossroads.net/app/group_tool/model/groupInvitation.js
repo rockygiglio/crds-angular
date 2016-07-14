347c179e3fc11dcc790ef1c2c049e16da6c8f334
Jim Kriz <jim.kriz@ingagepartners.com>
2016-07-14 01:58:17 -0400

    US4428: Add unit tests for groupInvitation object

--- crossroads.net/app/group_tool/model/groupInvitation.js
+++ crossroads.net/app/group_tool/model/groupInvitation.js
@@ -16,20 +16,4 @@ export default class GroupInvitation {
     // Always set to a Group invitation type    
     this.invitationType = CONSTANTS.INVITATION.TYPES.GROUP;
   }
-
-  isLeader() {
-   return this.groupRoleId === CONSTANTS.GROUP.ROLES.LEADER; 
-  }
-
-  role() {
-    if(this.groupRoleId === CONSTANTS.GROUP.ROLES.LEADER) {
-      return 'Leader';
-    } else if (this.groupRoleId === CONSTANTS.GROUP.ROLES.APPRENTICE) {
-      return 'Apprentice';
-    } else if (this.groupRoleId === CONSTANTS.GROUP.ROLES.MEMBER) {
-      return 'Participant';
-    }
-
-    return null;
-  }
 }
