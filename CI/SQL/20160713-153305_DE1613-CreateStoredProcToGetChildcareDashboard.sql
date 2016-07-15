bae0da3222757e323be59c45576c2597842b793a
silbermm <silbermm@gmail.com>
2016-07-15 12:27:21 -0400

    Update stored proc to use Domain_ID

--- CI/SQL/20160713-153305_DE1613-CreateStoredProcToGetChildcareDashboard.sql
+++ CI/SQL/20160713-153305_DE1613-CreateStoredProcToGetChildcareDashboard.sql
@@ -2,12 +2,18 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
--- =============================================
--- Author:		
+-- ===============================================================
+-- Author: Andrew Canterbury<andy.canterbury@ingagepartners.com>	
 -- Create date: 7/13/2016
 -- Description:	Gets data for childcare dashboard
--- =============================================
-CREATE PROCEDURE api_crds_GetChildcareDashboard 
+-- ===============================================================
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[api_crds_GetChildcareDashboard]') AND type in (N'P', N'PC'))
+BEGIN
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[api_crds_GetChildcareDashboard] AS' 
+END
+GO
+
+ALTER PROCEDURE [dbo].[api_crds_GetChildcareDashboard]
 	-- Add the parameters for the stored procedure here
 	@Domain_ID int,
 	@Contact_ID int
@@ -23,12 +29,12 @@ BEGIN
 	SELECT @Household_ID = Household_ID FROM dbo.Contacts
 	WHERE Contact_ID = @Contact_ID
 
-	SELECT Display_Name, gp.Group_ID, Group_Name, Group_Type_ID, e.Event_ID, e.Event_Title, e.Cancelled, e.Event_Start_Date, e.Event_End_Date, e.Congregation_ID FROM dbo.Group_Participants gp
-	JOIN dbo.Groups g on g.Group_ID = gp.Group_ID
-	JOIN dbo.Participants p on gp.Participant_ID = p.Participant_ID
-	JOIN dbo.Contacts c on c.Contact_ID = p.Contact_ID AND c.Household_ID = @Household_ID
-	JOIN dbo.Event_Groups eg on eg.Group_ID = g.Group_ID
-	JOIN dbo.Events e on e.Event_ID = eg.Event_ID AND Event_Type_ID = @ChildcareEventType
+	SELECT c.Nickname, c.Last_Name, gp.Group_ID, Group_Name, Group_Type_ID, e.Event_ID, e.Event_Title, e.Cancelled, e.Event_Start_Date, e.Event_End_Date, e.Congregation_ID FROM dbo.Group_Participants gp
+	JOIN dbo.Groups g on g.Group_ID = gp.Group_ID and g.Domain_ID = gp.Domain_ID and g.Domain_ID = @Domain_ID
+	JOIN dbo.Participants p on gp.Participant_ID = p.Participant_ID and gp.Domain_ID = p.Domain_ID
+	JOIN dbo.Contacts c on c.Contact_ID = p.Contact_ID AND c.Household_ID = @Household_ID and c.Domain_ID = p.Domain_ID
+	JOIN dbo.Event_Groups eg on eg.Group_ID = g.Group_ID and eg.Domain_ID = g.Domain_ID
+	JOIN dbo.Events e on e.Event_ID = eg.Event_ID AND Event_Type_ID = @ChildcareEventType and eg.Domain_ID = e.Domain_ID
 	WHERE (gp.End_Date IS NULL OR gp.End_Date > GetDate())
 	AND c.Household_Position_ID IN (1, 7)
 END
