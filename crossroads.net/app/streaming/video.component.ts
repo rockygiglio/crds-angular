41fa9ab1dbdc8d28a488da72f492137a38f9a2af
Samuel Mello <samueljmello@gmail.com>
2016-07-19 15:21:13 -0400

    US4290: implimenting videojs component

--- crossroads.net/app/streaming/video.component.ts
+++ crossroads.net/app/streaming/video.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { VideoJSComponent } from './videojs.component';
 
 @Component({
   selector: 'live-stream',
-  templateUrl: './video.component.html'
+  templateUrl: './video.component.html',
+  directives: [VideoJSComponent]
 })
 
 export class VideoComponent {
