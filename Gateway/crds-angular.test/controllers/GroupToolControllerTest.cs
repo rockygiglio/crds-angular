ae01cf741831ac556d76b7695c28f30f1e918a98
Jim Kriz <jim.kriz@ingagepartners.com>
2016-07-20 10:07:42 -0400

    US4465: Unit test for ApproveDenyInquiryFromMyGroup

--- Gateway/crds-angular.test/controllers/GroupToolControllerTest.cs
+++ Gateway/crds-angular.test/controllers/GroupToolControllerTest.cs
@@ -4,6 +4,7 @@ using System.Web.Http;
 using System.Web.Http.Results;
 using crds_angular.Controllers.API;
 using crds_angular.Exceptions;
+using crds_angular.Models.Crossroads;
 using crds_angular.Services.Interfaces;
 using Moq;
 using NUnit.Framework;
@@ -73,5 +74,72 @@ namespace crds_angular.test.controllers
 
             _groupToolService.VerifyAll();
         }
+
+        [Test]
+        public void TestApproveDenyInquiryFromMyGroup()
+        {
+            const int groupTypeId = 1;
+            const int groupId = 2;
+            const bool approve = true;
+            var inquiry = new Inquiry();
+            const string message = "message";
+
+            _groupToolService.Setup(mocked => mocked.ApproveDenyInquiryFromMyGroup(_auth, groupTypeId, groupId, approve, inquiry, message)).Verifiable();
+
+            var result = _fixture.ApproveDenyInquiryFromMyGroup(groupTypeId, groupId, approve, inquiry, message);
+            _groupToolService.VerifyAll();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+
+        [Test]
+        public void TestApproveDenyInquiryFromMyGroupWithGroupParticipantRemovalException()
+        {
+            var ex = new GroupParticipantRemovalException("message");
+            const int groupTypeId = 1;
+            const int groupId = 2;
+            const bool approve = true;
+            var inquiry = new Inquiry();
+            const string message = "message";
+
+            _groupToolService.Setup(mocked => mocked.ApproveDenyInquiryFromMyGroup(_auth, groupTypeId, groupId, approve, inquiry, message)).Throws(ex);
+            try
+            {
+                _fixture.ApproveDenyInquiryFromMyGroup(groupTypeId, groupId, approve, inquiry, message);
+                Assert.Fail("Expected exception was not thrown");
+            }
+            catch (HttpResponseException e)
+            {
+                Assert.AreEqual(ex.StatusCode, e.Response.StatusCode);
+            }
+
+            _groupToolService.VerifyAll();
+        }
+
+        [Test]
+        public void TestApproveDenyInquiryFromMyGroupWithOtherException()
+        {
+            var ex = new Exception();
+            const int groupTypeId = 1;
+            const int groupId = 2;
+            const bool approve = true;
+            var inquiry = new Inquiry();
+            const string message = "message";
+
+            _groupToolService.Setup(mocked => mocked.ApproveDenyInquiryFromMyGroup(_auth, groupTypeId, groupId, approve, inquiry, message)).Throws(ex);
+            try
+            {
+                _fixture.ApproveDenyInquiryFromMyGroup(groupTypeId, groupId, approve, inquiry, message);
+                Assert.Fail("Expected exception was not thrown");
+            }
+            catch (HttpResponseException e)
+            {
+                Assert.AreEqual(HttpStatusCode.BadRequest, e.Response.StatusCode);
+            }
+
+            _groupToolService.VerifyAll();
+        }
+
     }
 }
