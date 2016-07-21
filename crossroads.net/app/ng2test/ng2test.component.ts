564232f16abd2ea60f0febead9bf7e0b88cb1436
Ken Baum <ken.baum@ingagepartners.com>
2016-07-21 13:05:11 -0400

    US4234 Added disappearing text back to file

---
+++ crossroads.net/app/ng2test/ng2test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+declare var moment: any;
+
+@Component({
+  selector: 'ng2-test',
+  template: `
+    <h2>Welcome from our first angular2 component</h2>
+    <h2>{{today}}</h2>
+  `
+})
+export class Ng2TestComponent {
+  today: string = moment().format('D MMM YYYY');
+}
