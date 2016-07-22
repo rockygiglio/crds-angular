194f8551589d4daf867ae68743781641d7d6abd9
Chris Tallent <chris@tallented.net>
2016-07-22 08:17:05 -0400

    US4519 - Layout of the Find a Group / Lead a Group cards based on whether there are even or odd number of groups

--- crossroads.net/app/group_tool/my_groups/myGroups.controller.js
+++ crossroads.net/app/group_tool/my_groups/myGroups.controller.js
@@ -8,20 +8,29 @@ export default class MyGroupsController {
     this.ready = false;
     this.error = false;
     this.errorMsg = '';
+
+    this.groupsEven = function() {
+      return this.groups.length % 2 == 0;
+    };
+
+    this.groupsOdd = function() {
+      return this.groups.length % 2 == 1;
+    };
   }
 
   $onInit() {
     this.groupService.getMyGroups().then((smGroups) => {
-      // this.groups = smGroups;
-      // this.groups = smGroups.slice(0,1);
-
-      let testId = 1;
       this.groups = smGroups;
-      angular.forEach(this.groups, function(group) {
-        let fake = angular.copy(group);
-        fake.groupId = testId++;
-        smGroups.push(fake);
-      });
+
+      // let testId = 1;
+      // this.groups = smGroups;
+      // angular.forEach(this.groups, function(group) {
+      //   let fake = angular.copy(group);
+      //   fake.groupId = testId++;
+      //   smGroups.push(fake);
+      // });
+      //
+      // this.groups = smGroups.slice(0,1);
 
       this.ready = true;
     },
