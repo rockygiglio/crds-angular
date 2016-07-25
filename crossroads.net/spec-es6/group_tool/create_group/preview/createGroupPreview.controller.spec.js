1ff93232898901e5ef7e998c42c6db21f42adaaa
dtkocher <dtkocher@gmail.com>
2016-07-25 10:25:47 -0400

    Fix tests.

--- crossroads.net/spec-es6/group_tool/create_group/preview/createGroupPreview.controller.spec.js
+++ crossroads.net/spec-es6/group_tool/create_group/preview/createGroupPreview.controller.spec.js
@@ -1,56 +1,57 @@
 import constants from 'crds-constants';
 import CreateGroupPreviewController from '../../../../app/group_tool/create_group/preview/createGroup.preview.controller'
 describe('CreateGroupPreviewController', () => {
-    let fixture,
-        groupService, 
-        createGroupService, 
-        group, 
-        imageService, 
-        state, 
-        log, 
-        rootScope,
-        mockProfile,
-        api;
+  let fixture,
+      groupService, 
+      createGroupService, 
+      group, 
+      imageService, 
+      state, 
+      log, 
+      rootScope,
+      api;
 
-    beforeEach(angular.mock.module(constants.MODULES.GROUP_TOOL));
+  var mockProfile;
 
-    beforeEach(angular.mock.module(($provide)=> {
-      mockProfile = jasmine.createSpyObj('Profile', ['Personal']);
-      $provide.value('Profile', mockProfile);
-    }));
+  beforeEach(angular.mock.module(constants.MODULES.GROUP_TOOL));
 
-    beforeEach(inject(($injector)=> {
-      groupService = $injector.get('GroupService');
-      createGroupService = $injector.get('CreateGroupService');
-      group = $injector.get('Group');
-      imageService = $injector.get('ImageService');
-      state = $injector.get('$state');
-      location = $injector.get('$location');
-      log = $injector.get('$log');
-      rootScope = $injector.get('$rootScope');
-      api = $injector.get('$q');
+  beforeEach(angular.mock.module(($provide)=> {
+    mockProfile = jasmine.createSpyObj('Profile', ['Personal']);
+    $provide.value('Profile', mockProfile);
+  }));
 
-      fixture = new CreateGroupPreviewController(groupService, createGroupService, group, imageService, state, log, rootScope);
+  beforeEach(inject(($injector)=> {
+    groupService = $injector.get('GroupService');
+    createGroupService = $injector.get('CreateGroupService');
+    group = $injector.get('Group');
+    imageService = $injector.get('ImageService');
+    state = $injector.get('$state');
+    log = $injector.get('$log');
+    rootScope = $injector.get('$rootScope');
+    api = $injector.get('$q');
 
-    }));
+    fixture = new CreateGroupPreviewController(groupService, createGroupService, group, imageService, state, log, rootScope);
 
-    describe('save', () => {
-        it('should call save', () => {
-            let promised = api.defer();
+  }));
 
-            spyOn(createGroupService, 'saveCreateGroupForm').and.callFake( function() {
-                return promised.promise; 
-            })
-            fixture.groupData = "Hi Dustin";
-            fixture.save();
-            expect(createGroupService.saveCreateGroupForm).toHaveBeenCalledWith(fixture.groupData);
-        })
-    });
-    // it('should call participant service', () => {
-    //   fixture.$onInit();
-    //   expect(participantService.get).toHaveBennCalled();
-    // });
+  describe('save', () => {
+    it('should call save', () => {
+      let deferred = api.defer();
+      deferred.resolve({});
 
+      spyOn(groupService, 'saveCreateGroupForm').and.callFake( function() {
+        return deferred.promise;
+      });
 
+      spyOn(state, 'go').and.callFake(function() {});
+      spyOn(createGroupService, 'mapSmallGroup').and.callFake(function() {
+        return {};
+      });
+
+      fixture.$onInit();
+      fixture.save();
+      expect(fixture.groupService.saveCreateGroupForm).toHaveBeenCalledWith(fixture.groupData);
+    })
+  });
 });
 
