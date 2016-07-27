05f071305bb6acc9d84e46a03ea640ffdb49d3c8
Dustin Kocher <dtkocher@gmail.com>
2016-07-26 21:53:47 -0400

    Update 20160726_211500_US4497-add-invitations-to-api-user.sql
    Make Update Statement

--- CI/SQL/20160726_211500_US4497-add-invitations-to-api-user.sql
+++ CI/SQL/20160726_211500_US4497-add-invitations-to-api-user.sql
@@ -5,28 +5,8 @@ SET ANSI_NULLS ON
 GO
 
 BEGIN
-INSERT [dbo].[dp_Role_Pages]
-    ([Role_ID],
-     [Page_ID],
-     [Access_Level],
-     [Scope_All],
-     [Approver],
-     [File_Attacher],
-     [Data_Importer],
-     [Data_Exporter],
-     [Secure_Records],
-     [Allow_Comments],
-     [Quick_Add])
-VALUES
-    (62,
-     515,
-     0,
-     0,
-     0,
-     0,
-     0,
-     0,
-     0,
-     0,
-     0);
+UPDATE [dbo].[dp_Role_Pages]
+SET [Access_Level] = 0
+WHERE [Role_ID] = 62 and [Page_ID] = 515;
+
 END
