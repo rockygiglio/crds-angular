73275e1f693bf49a0b2025de17549c71cfb5f02c
Gabriel Smith <gabriel.smith01@gmail.com>
2016-07-28 14:41:07 -0400

    Create SocialSharing component and implement on streaming landing

--- crossroads.net/app/streaming/streaming.component.ts
+++ crossroads.net/app/streaming/streaming.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ScheduleComponent } from './schedule.component';
 import { CountdownComponent } from './countdown.component';
+import { SocialSharingComponent } from './social-sharing.component';
 import { StreamspotService } from './streamspot.service';
 import { DynamicContentNg2Component } from '../../core/dynamic_content/dynamic-content-ng2.component'
 
@@ -8,7 +9,7 @@ var WOW = require('wow.js/dist/wow.min.js');
 
 @Component({
   selector: 'streaming',
-  directives: [DynamicContentNg2Component, ScheduleComponent, CountdownComponent],
+  directives: [DynamicContentNg2Component, ScheduleComponent, CountdownComponent, SocialSharingComponent],
   providers: [StreamspotService],
   templateUrl: './streaming.ng2component.html'
 })
