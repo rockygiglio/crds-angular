a20f3c66e1ac8a28135739a461dc4f412afc9303
Richard Barker <rbarker@Administrators-MacBook-Pro-2.local>
2016-08-01 10:35:47 -0400

    Unit tests for CMS data endpoints

--- crossroads.net/spec-ts/core/services/CMSData.service.spec.ts
+++ crossroads.net/spec-ts/core/services/CMSData.service.spec.ts
@@ -1,41 +1,102 @@
 /* tslint:disable:no-unused-variable */
-
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-import { Http, HTTP_PROVIDERS } from '@angular/http';
 import { provide } from '@angular/core';
+import {
+  ResponseOptions,
+  Response,
+  Http,
+  BaseRequestOptions,
+  RequestMethod
+} from '@angular/http';
+
 import {
   describe,
   expect,
-  beforeEach,
   it,
   inject,
-  async,
-  beforeEachProviders  
+  fakeAsync,
+  beforeEachProviders
 } from '@angular/core/testing';
 
-import { CMSDataService } from '../../../core/services/CMSData.service'
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+import { CMSDataService } from '../../../core/services/CMSData.service';
+
+const mockHttpProvider = {
+  deps: [ MockBackend, BaseRequestOptions ],
+  useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
+    return new Http(backend, defaultOptions);
+  }
+}
+
 
 describe('Service: CMSData', () => {
   let cmsService;
 
-  beforeEachProviders(() => [
-    HTTP_PROVIDERS,
-    CMSDataService
-  ]);
-
-  beforeEach(inject([CMSDataService], s => {
-    cmsService = s;
-  }))
-
-  it('creates an instance of the service', () => {
-    expect(cmsService).toBeFalsy;
+  beforeEachProviders(() => {
+    return [
+      MockBackend,
+      BaseRequestOptions,
+      provide(Http, mockHttpProvider),
+      CMSDataService
+    ];
   });
 
-  it('is able to retrieve the first series', async(() => {
-    cmsService.getFirstInSeries().subscribe(x => {
-      expect(x.title).toMatch("Foobar");
-    });
-  }));
+  it('should use an HTTP call to obtain series by id',
+    inject(
+      [CMSDataService, MockBackend],
+      fakeAsync((service: CMSDataService, backend: MockBackend) => {
+        backend.connections.subscribe((connection: MockConnection) => {
+
+          expect(connection.request.method).toBe(RequestMethod.Get);
+          expect(connection.request.url).toBe(
+            'https://contentint.crossroads.net/api/series/228');
+        });
+
+        service.getSeriesById(228);
+      })));
+
+  it('should use an HTTP call to obtain series by title',
+    inject(
+      [CMSDataService, MockBackend],
+      fakeAsync((service: CMSDataService, backend: MockBackend) => {
+        backend.connections.subscribe((connection: MockConnection) => {
+
+          expect(connection.request.method).toBe(RequestMethod.Get);
+          expect(connection.request.url).toBe(
+            'https://contentint.crossroads.net/api/series?title=Death%20to%20Religion');
+        });
+
+        service.getSeriesByTitle("Death to Religion");
+      })));
+
+  it('should use an HTTP call to obtain message by title',
+    inject(
+      [CMSDataService, MockBackend],
+      fakeAsync((service: CMSDataService, backend: MockBackend) => {
+        backend.connections.subscribe((connection: MockConnection) => {
+
+          expect(connection.request.method).toBe(RequestMethod.Get);
+          expect(connection.request.url).toBe(
+            'https://contentint.crossroads.net/api/messages?title=Death%20to%20Rules');
+        });
+
+        service.getMessageByTitle("Death to Rules");
+      })));
+
+  it('should use an HTTP call to obtain most recent 4 messages',
+    inject(
+      [CMSDataService, MockBackend],
+      fakeAsync((service: CMSDataService, backend: MockBackend) => {
+        backend.connections.subscribe((connection: MockConnection) => {
+
+          expect(connection.request.method).toBe(RequestMethod.Get);
+          expect(connection.request.url).toBe(
+            'https://contentint.crossroads.net/api/messages?date__sort=DESC&__limit[]=4');
+        });
+
+        service.getMostRecent4Messages();
+      })));
+
+
 });
 
