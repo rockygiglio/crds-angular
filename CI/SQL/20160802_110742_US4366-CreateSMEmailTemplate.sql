22fcacb6e9a7a0d152578238a40bb3515779aa71
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-08-02 12:04:47 -0400

    US4366: Scripts to create studentministry@crossroads.net contact and email template if they don't exist (they were created and scripted from PROD)

---
+++ CI/SQL/20160802_110742_US4366-CreateSMEmailTemplate.sql
@@ -0,0 +1,14 @@
+USE [MinistryPlatform]
+GO
+
+IF NOT EXISTS(SELECT * FROM [dbo].dp_Communications WHERE communication_id = 280854)
+BEGIN
+
+SET IDENTITY_INSERT [dbo].[dp_Communications] ON;
+
+INSERT INTO [dbo].dp_Communications 
+(Communication_ID,Author_User_ID,Subject                                ,Body  ,Domain_ID,Start_Date                ,Communication_Status_ID,From_Contact,To_Contact,Reply_to_Contact,Template,Active) VALUES
+(280854          ,5             ,'Small Group Background Check Required','temp',1        ,{ts '2016-08-02 00:00:00'},1                      ,7           ,7676101   ,7               ,1       ,1     );
+
+SET IDENTITY_INSERT [dbo].[dp_Communications] OFF;
+END
