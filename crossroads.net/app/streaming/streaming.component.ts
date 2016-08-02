22c94d85bbb71d73dd20ab23bff2085a67837438
Taylor C. MacDonald <tcmacdonald@gmail.com>
2016-08-02 17:11:20 -0400

    US4454: Refactor Schedule to Content Blocks Implement smooth scroll on schedule button per comments in US4454

--- crossroads.net/app/streaming/streaming.component.ts
+++ crossroads.net/app/streaming/streaming.component.ts
@@ -6,6 +6,7 @@ import { StreamspotService } from './streamspot.service';
 import { DynamicContentNg2Component } from '../../core/dynamic_content/dynamic-content-ng2.component'
 import { StickyHeaderDirective } from './sticky-header.directive';
 import { PageScroll } from '../ng2-page-scroll/ng2-page-scroll.component';
+import { PageScrollConfig } from '../ng2-page-scroll/ng2-page-scroll-config';
 
 var WOW = require('wow.js/dist/wow.min.js');
 var $:any = require('jquery');
@@ -21,6 +22,14 @@ export class StreamingComponent {
   inProgress: boolean = false;
 
   constructor(private streamspotService: StreamspotService) {
+
+    PageScrollConfig.defaultScrollOffset = -50; // NOTE – this value will need to change per DE1705 
+    PageScrollConfig.defaultEasingFunction = (t:number, b:number, c:number, d:number):number => {
+      if (t === 0) return b;
+      if (t === d) return b + c;
+      if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+      return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    };
 
     this.streamspotService.isBroadcasting.subscribe((inProgress: boolean) => {
       this.inProgress = inProgress;
