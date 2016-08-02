092f8c92a8cd731794553b29b0fc28ec8c344ff9
Sandi Ritter <sandi.ritter@ingagepartners.com>
2016-08-02 11:45:44 -0400

    DE1616 how about actually making a change??

--- CI/SQL/20160802-112300_DE1616-NewPledgeAssignmentView.sql
+++ CI/SQL/20160802-112300_DE1616-NewPledgeAssignmentView.sql
@@ -15,7 +15,7 @@ INSERT INTO [dbo].[dp_Page_Views]
 		   ,'Pledge Assignment Needed - Deposited'
            ,296
 		   ,'These distributions are against a program that is part of a capital campaign the donor is participating in and has been deposited.  Check to see if the pledge should be credited.'
-           ,'Donation_Distributions.Pledge_ID IS NULL AND EXISTS (SELECT * FROM Pledges P INNER JOIN Pledge_Campaigns PC ON PC.Pledge_Campaign_ID = P.Pledge_Campaign_ID INNER JOIN Programs PR ON PR.Pledge_Campaign_ID = PC.Pledge_Campaign_ID WHERE PR.Program_ID = Donation_Distributions.Program_ID AND P.Pledge_Status_ID IN (1,4) AND P.Donor_ID = (SELECT Donor_ID FROM Donations WHERE Donations.Donation_ID = Donation_Distributions.Donation_ID))'
+           ,'Donation_Distributions.Pledge_ID IS NULL AND EXISTS (SELECT * FROM Pledges P INNER JOIN Pledge_Campaigns PC ON PC.Pledge_Campaign_ID = P.Pledge_Campaign_ID INNER JOIN Programs PR ON PR.Pledge_Campaign_ID = PC.Pledge_Campaign_ID WHERE PR.Program_ID = Donation_Distributions.Program_ID AND P.Pledge_Status_ID IN (1,4) AND P.Donor_ID = (SELECT Donor_ID FROM Donations WHERE Donations.Donation_ID = Donation_Distributions.Donation_ID AND Donation_Status_ID = 2))'
 		   );
 
 SET IDENTITY_INSERT [dbo].[dp_Page_Views] OFF
