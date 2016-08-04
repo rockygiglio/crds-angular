55faf4da5b4200fe67fef10de4d9a41f76c178ee
Jim Kriz <jim.kriz@ingagepartners.com>
2016-08-04 10:38:32 -0400

    US4620: Integration tests for proximity service

--- Gateway/crds-angular.test/Services/AddressServiceTest.cs
+++ Gateway/crds-angular.test/Services/AddressServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Device.Location;
 using crds_angular.App_Start;
 using crds_angular.Models.Crossroads;
 using crds_angular.Services;
@@ -10,7 +11,7 @@ using MPServices = MinistryPlatform.Translation.Repositories.Interfaces;
 
 namespace crds_angular.test.Services
 {
-    class AddressServiceTest
+    public class AddressServiceTest
     {
 
         private AddressService _fixture;
@@ -62,6 +63,81 @@ namespace crds_angular.test.Services
         }
 
         [Test]
+        public void TestFindExistingAddressWithoutGeoCoordinates()
+        {
+            var address = new AddressDTO()
+            {
+                AddressLine1 = "123 Sesame St",
+                AddressLine2 = "",
+                City = "South Side",
+                State = "OH",
+                PostalCode = "12312"
+            };
+
+            var addressResults = new List<MpAddress>()
+            {
+                new MpAddress()
+                {
+                    Address_ID = 12345
+                },
+                new MpAddress()
+                {
+                    Address_ID = 232323
+                }
+            };
+
+            var coords = new GeoCoordinate(39.15946159999999, -84.42336390000003);
+            _mpAddressServiceMock.Setup(mocked => mocked.FindMatches(It.IsAny<MpAddress>())).Returns(addressResults);
+            _addressProximityService.Setup(mocked => mocked.GetGeoCoordinates(It.IsAny<AddressDTO>())).Returns(coords);
+            _mpAddressServiceMock.Setup(mocked => mocked.Update(It.IsAny<MpAddress>())).Returns(67890);
+
+            _fixture.FindOrCreateAddress(address, true);
+
+            _mpAddressServiceMock.Verify(x => x.FindMatches(It.IsAny<MpAddress>()), Times.Once);
+            _mpAddressServiceMock.Verify(x => x.Update(It.Is<MpAddress>(a => a.Latitude == coords.Latitude && a.Longitude == coords.Longitude)), Times.Once);
+            _addressProximityService.Verify(mocked => mocked.GetGeoCoordinates(address));
+            Assert.AreEqual(address.AddressID, 67890);
+            Assert.AreEqual(address.Latitude, coords.Latitude);
+            Assert.AreEqual(address.Longitude, coords.Longitude);
+        }
+
+        [Test]
+        public void TestFindExistingAddressWithGeoCoordinates()
+        {
+            var address = new AddressDTO()
+            {
+                AddressLine1 = "123 Sesame St",
+                AddressLine2 = "",
+                City = "South Side",
+                State = "OH",
+                PostalCode = "12312"
+            };
+
+            var addressResults = new List<MpAddress>()
+            {
+                new MpAddress()
+                {
+                    Address_ID = 12345,
+                    Latitude = 39.15946159999999,
+                    Longitude = -84.42336390000003
+                },
+                new MpAddress()
+                {
+                    Address_ID = 232323
+                }
+            };
+
+            _mpAddressServiceMock.Setup(mocked => mocked.FindMatches(It.IsAny<MpAddress>())).Returns(addressResults);
+
+            _fixture.FindOrCreateAddress(address, true);
+
+            _mpAddressServiceMock.Verify(x => x.FindMatches(It.IsAny<MpAddress>()), Times.Once);
+            _mpAddressServiceMock.Verify(x => x.Update(It.IsAny<MpAddress>()), Times.Never);
+            _addressProximityService.Verify(mocked => mocked.GetGeoCoordinates(It.IsAny<AddressDTO>()), Times.Never());
+            Assert.AreEqual(address.AddressID, 12345);
+        }
+
+        [Test]
         public void Given_An_Address_That_Does_Not_Exist_It_Should_Create_A_New_Address()
         {
             var address = new AddressDTO()
@@ -83,6 +159,62 @@ namespace crds_angular.test.Services
             _mpAddressServiceMock.Verify(x => x.FindMatches(It.IsAny<MpAddress>()), Times.Once);
             _mpAddressServiceMock.Verify(x => x.Create(It.IsAny<MpAddress>()), Times.Once);
             
+            Assert.AreEqual(address.AddressID, 12345);
+        }
+
+        [Test]
+        public void TestFindNewAddressSetGeoCoordinates()
+        {
+            var address = new AddressDTO()
+            {
+                AddressLine1 = "123 Sesame St",
+                AddressLine2 = "",
+                City = "South Side",
+                State = "OH",
+                PostalCode = "12312"
+            };
+
+            var addressResults = new List<MpAddress>();
+            var coords = new GeoCoordinate(39.15946159999999, -84.42336390000003);
+
+            _mpAddressServiceMock.Setup(mocked => mocked.FindMatches(It.IsAny<MpAddress>())).Returns(addressResults);
+            _addressProximityService.Setup(mocked => mocked.GetGeoCoordinates(It.IsAny<AddressDTO>())).Returns(coords);
+            _mpAddressServiceMock.Setup(mocked => mocked.Create(It.IsAny<MpAddress>())).Returns(12345);
+
+            _fixture.FindOrCreateAddress(address, true);
+
+            _mpAddressServiceMock.Verify(x => x.FindMatches(It.IsAny<MpAddress>()), Times.Once);
+            _mpAddressServiceMock.Verify(x => x.Create(It.Is<MpAddress>(a => a.Latitude == coords.Latitude && a.Longitude == coords.Longitude)), Times.Once);
+            _addressProximityService.Verify(mocked => mocked.GetGeoCoordinates(address));
+
+            Assert.AreEqual(address.AddressID, 12345);
+            Assert.AreEqual(address.Latitude, coords.Latitude);
+            Assert.AreEqual(address.Longitude, coords.Longitude);
+        }
+
+        [Test]
+        public void TestFindNewAddressDoNotSetGeoCoordinates()
+        {
+            var address = new AddressDTO()
+            {
+                AddressLine1 = "123 Sesame St",
+                AddressLine2 = "",
+                City = "South Side",
+                State = "OH",
+                PostalCode = "12312"
+            };
+
+            var addressResults = new List<MpAddress>();
+
+            _mpAddressServiceMock.Setup(mocked => mocked.FindMatches(It.IsAny<MpAddress>())).Returns(addressResults);
+            _mpAddressServiceMock.Setup(mocked => mocked.Create(It.IsAny<MpAddress>())).Returns(12345);
+
+            _fixture.FindOrCreateAddress(address);
+
+            _mpAddressServiceMock.Verify(x => x.FindMatches(It.IsAny<MpAddress>()), Times.Once);
+            _mpAddressServiceMock.Verify(x => x.Create(It.IsAny<MpAddress>()), Times.Once);
+            _addressProximityService.Verify(mocked => mocked.GetGeoCoordinates(It.IsAny<AddressDTO>()), Times.Never);
+
             Assert.AreEqual(address.AddressID, 12345);
         }
     }
