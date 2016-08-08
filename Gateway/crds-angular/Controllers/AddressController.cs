5b743b2c1d095f3790eae3ee9255e3a8c6511fd7
Jim Kriz <jim.kriz@ingagepartners.com>
2016-08-08 10:23:54 -0400

    US4653: Address Validation controller

---
+++ Gateway/crds-angular/Controllers/AddressController.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+using crds_angular.Exceptions;
+using crds_angular.Exceptions.Models;
+using crds_angular.Models.Crossroads;
+using crds_angular.Security;
+using crds_angular.Services.Interfaces;
+
+namespace crds_angular.Controllers
+{
+    public class AddressController : MPAuth
+    {
+        private readonly IAddressProximityService _addressProximityService;
+
+        public AddressController(IAddressProximityService addressProximityService)
+        {
+            _addressProximityService = addressProximityService;
+        }
+
+        /// <summary>
+        /// Validate an input address by first geocoding, then returning the parsed address components, including the latitude and longitude.  Note that this is not a true street-level address validation.  This simply verifies that a given input can be geocoded, which might mean that address components are moved in order to get to a city/zip center, if the given street address is not found.
+        /// </summary>
+        /// <param name="address">An <see cref="AddressDTO">AddressDTO</see> with individual components populated</param>
+        /// <returns>An <see cref="AddressDTO">AddressDTO</see> with parsed components including latitude and longitude.  This will return a 404/Not Found if the input address could not be located.</returns>
+        [ResponseType(typeof(AddressDTO))]
+        [Route("api/address/validate")]
+        [HttpPost]
+        public IHttpActionResult ValidateAddress(AddressDTO address)
+        {
+            return ValidateAddress(address.ToString());
+        }
+
+        /// <summary>
+        /// Validate an input address by first geocoding, then returning the parsed address components, including the latitude and longitude.  Note that this is not a true street-level address validation.  This simply verifies that a given input can be geocoded, which might mean that address components are moved in order to get to a city/zip center, if the given street address is not found.
+        /// </summary>
+        /// <param name="address">An address string</param>
+        /// <returns>An <see cref="AddressDTO">AddressDTO</see> with parsed components including latitude and longitude.  This will return a 404/Not Found if the input address could not be located.</returns>
+        [ResponseType(typeof(AddressDTO))]
+        [Route("api/address/validate")]
+        [HttpGet]
+        public IHttpActionResult ValidateAddress([FromUri(Name = "address")]string address)
+        {
+            try
+            {
+                return Ok(_addressProximityService.ValidateAddress(address));
+            }
+            catch (InvalidAddressException)
+            {
+                var apiError = new ApiErrorDto($"Invalid address {address}", code: HttpStatusCode.NotFound);
+                return ResponseMessage(apiError.HttpResponseMessage);
+            }
+        }
+    }
+}
