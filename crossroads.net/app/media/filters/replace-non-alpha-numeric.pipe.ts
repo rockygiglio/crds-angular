3b03dedd7532abe59372cfa130b36b58778327f2
Gabriel Smith <gabriel.smith01@gmail.com>
2016-08-08 15:03:28 -0400

    Create ng2 pipe from existing ng filter

---
+++ crossroads.net/app/media/filters/replace-non-alpha-numeric.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core'
+
+@Pipe({
+  name: 'replaceNonAlphaNumeric',
+  pure: true
+})
+
+export class ReplaceNonAlphaNumericPipe implements PipeTransform {
+  transform(input: string): string {
+    return input.replace(/[^a-zA-Z0-9_]/g, '-');
+  }
+}
