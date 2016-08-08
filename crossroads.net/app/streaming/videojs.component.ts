bdd5e21bc91531c89c4b962d9145860bbde47868
Michael Vezzani <mvezzani@callibrity.com>
2016-08-08 11:31:25 -0400

    Comment out references to SSTracker and enter the correct url for production video source.

--- crossroads.net/app/streaming/videojs.component.ts
+++ crossroads.net/app/streaming/videojs.component.ts
@@ -2,11 +2,11 @@ import { Component, AfterViewInit } from '@angular/core';
 import { StreamspotService } from './streamspot.service';
 
 var videojs = require('video.js/dist/video');
-var sstracker = require('https://d2i0qcc2ysg3s9.cloudfront.net/analytics-pre-release.js');
+//var sstracker = require('https://d2i0qcc2ysg3s9.cloudfront.net/analytics-pre-release.js');
 
 declare var window: any;
 window.videojs = videojs;
-window.Tracker = sstracker;
+//window.Tracker = sstracker;
 
 require('videojs-contrib-hls/dist/videojs-contrib-hls');
 
@@ -20,7 +20,7 @@ export class VideoJSComponent implements AfterViewInit {
   player: any;
 
   nonPublicUrl: string = "//limelight1.streamspot.com/dvr/smil:crossr30e3.smil/playlist.m3u8";
-  productionUrl: string = "//limelight1.streamspot.com/url/smil:.smil/playlist.m3u8";
+  productionUrl: string = "//limelight1.streamspot.com/url/smil:crossr4915.smil/playlist.m3u8";
   testUrl: string = "//qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8";
 
   ssid: string = "crossr30e3";
@@ -47,16 +47,16 @@ export class VideoJSComponent implements AfterViewInit {
     // create play handler (analytics)
     this.player.on('play', () => {
       var streamName = 'crossr30e3_mbr2'
-      window.SSTracker = window.SSTracker ? window.SSTracker : new window.Tracker(this.ssid);
-      window.SSTracker.start(this.url, true, this.ssid);
+//      window.SSTracker = window.SSTracker ? window.SSTracker : new window.Tracker(this.ssid);
+ //     window.SSTracker.start(this.url, true, this.ssid);
     })
 
     // create stop handler (analytics)
     this.player.on('pause', () => {
-      if(window.SSTracker){
-        window.SSTracker.stop();
-        window.SSTracker = null;
-      }
+//      if(window.SSTracker){
+//        window.SSTracker.stop();
+//        window.SSTracker = null;
+//      }
     })
 
     // do callback for broadcasting event
