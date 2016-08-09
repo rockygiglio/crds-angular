64763e15dac54cd9ba750a17312d66dfa3448e8c
Gabriel Smith <gabriel.smith01@gmail.com>
2016-08-09 13:51:26 -0400

    Move ng2 pipes to separate folders; Create generic pipes

--- crossroads.net/app/media/pipes/replace-non-alpha-numeric.pipe.ts
+++ crossroads.net/app/media/pipes/replace-non-alpha-numeric.pipe.ts
@@ -1,8 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core'
 
 @Pipe({
-  name: 'replaceNonAlphaNumeric',
-  pure: true
+  name: 'replaceNonAlphaNumeric'
 })
 
 export class ReplaceNonAlphaNumericPipe implements PipeTransform {
