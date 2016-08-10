61828329271fb832591941e201e1ce5a8ffaac64
Ken Baum <ken.baum@ingagepartners.com>
2016-08-10 10:12:13 -0400

    DE1742 Rearranged chain of promises when saving a group

--- crossroads.net/app/group_tool/create_group/preview/createGroup.preview.controller.js
+++ crossroads.net/app/group_tool/create_group/preview/createGroup.preview.controller.js
@@ -35,7 +35,13 @@ export default class CreateGroupPreviewController {
     this.successfulSave = false;
     try {
       var promise = this.groupService.saveCreateGroupForm(this.groupData)
-        .then( (data) => {
+        .then((data) => {
+          return this.groupService.saveParticipant(this.groupData.participants, data.groupId);
+        })
+        .then((data) => {
+          return this.groupService.saveProfile(this.groupData.profile);          
+        })
+        .then((data) => {
           this.rootScope.$emit('notify', this.rootScope.MESSAGES.groupToolCreateGroupSuccess);
           this.saving = false;
           this.successfulSave = true;
@@ -57,7 +63,7 @@ export default class CreateGroupPreviewController {
     this.successfulSave = false;
     try {
       var promise = this.groupService.saveEditGroupForm(this.groupData)
-        .then( (data) => {
+        .then((data) => {
           this.rootScope.$emit('notify', this.rootScope.MESSAGES.groupToolEditGroupSuccess);
           this.saving = false;
           this.successfulSave = true;
@@ -76,6 +82,10 @@ export default class CreateGroupPreviewController {
 
   submit() {
     this.edit ? this.saveEdits() : this.save();
+  }
+
+  redirectToDashboard() {
+    this.state.go('grouptool.mygroups');
   }
 
 }
