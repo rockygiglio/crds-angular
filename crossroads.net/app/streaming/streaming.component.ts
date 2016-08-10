32848c7ab9e503678db4ebdd8b9527dad3e64075
Gabriel Smith <gabriel.smith01@gmail.com>
2016-08-10 12:40:05 -0400

    Rename variable for better clarity

--- crossroads.net/app/streaming/streaming.component.ts
+++ crossroads.net/app/streaming/streaming.component.ts
@@ -62,7 +62,7 @@ export class StreamingComponent {
 
           _.each(mostRecent, (event, key) => {
             if (typeof event.series !== "undefined") {
-              event.delay = key * 100;
+              event.wowDelay = key * 100;
 
               event.image = 'https://crds-cms-uploads.imgix.net/content/images/register-bg.jpg'
               if (typeof event.messageVideo.still !== 'undefined') {
