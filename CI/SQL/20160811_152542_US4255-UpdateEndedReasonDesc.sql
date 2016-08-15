348e38dc5118b8b7b5ceb1afc920d27b8fa332f0
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-08-15 13:29:27 -0400

    US4255: Script reason name update from copy document .

--- CI/SQL/20160811_152542_US4255-UpdateEndedReasonDesc.sql
+++ CI/SQL/20160811_152542_US4255-UpdateEndedReasonDesc.sql
@@ -1,7 +1,9 @@
 USE [MinistryPlatform]
 GO
 
-UPDATE [dbo].[Group_Ended_Reasons] SET Description = '(We recently split off in new directions and saw solid growth within our group.)'
+UPDATE [dbo].[Group_Ended_Reasons] 
+SET Description = '(We recently split off in new directions and saw solid growth within our group.)',
+Group_Ended_Reason = 'We''re branching out'
 WHERE Group_Ended_Reason_ID = 1;
 
 UPDATE [dbo].[Group_Ended_Reasons] SET Description = '(We didn''t intentionally multiply, or have a fight and break up. It was just time to move on.)'
@@ -10,3 +12,10 @@ WHERE Group_Ended_Reason_ID = 2;
 UPDATE [dbo].[Group_Ended_Reasons] SET Description = '(It wasn''t a good fit, so we''re starting over. No biggie...it happens.)'
 WHERE Group_Ended_Reason_ID = 3;
 GO
+
+If NOT EXISTS(SELECT * FROM [dbo].[Group_Ended_Reasons] WHERE Group_Ended_Reason = 'Other')
+BEGIN
+INSERT INTO [dbo].[Group_Ended_Reasons] 
+(Group_Ended_Reason, Domain_ID) VALUES
+('Other'           , 1        );
+END
