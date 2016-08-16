8cfa70077e3d8c8777dd60d435432e546536904b
Ken Baum <ken.baum@ingagepartners.com>
2016-08-16 16:35:35 -0400

    US4654 WIP

--- crossroads.net/app/group_tool/group_detail/participants/change_participant_role/changeParticipantRole.controller.js
+++ crossroads.net/app/group_tool/group_detail/participants/change_participant_role/changeParticipantRole.controller.js
@@ -1,29 +1,51 @@
+import CONSTANTS from '../../../../constants';
+
 export default class ChangeParticipantRoleController {
-  constructor() {
+  constructor(GroupService) {
+    this.groupService = GroupService;
     let participant = this.participant;
     this.processing = false;
   }
 
   submit() {
     this.processing = true;
+    if (this.isParticipant()){
+      this.setParticipantRole();
+    }
+    else if (this.isLeader()){
+      this.setLeaderRole();
+    }
+    else if (this.isApprentice()) {
+      this.setApprenticeRole();
+    }
+    var promise = this.groupService.updateParticipant(this.participant)
+      .then((data) => {
+        this.processing = false;
+        this.state.go('grouptool.mygroups');
+      });
+  }
 
-    // TODO: Mock-up only, remove for implementation
-    window.setTimeout(() => {
-      // Invoke the parent callback function
-      this.submitAction();
-    }, 2000);
+  setParticipantRole() {
+    this.participant.groupRoleId = CONSTANTS.GROUP.ROLES.MEMBER;
   }
 
   isParticipant() {
-    return ! (this.participant.isLeader() && this.participant.isApprentice());
+    return this.participant.groupRoleId === CONSTANTS.GROUP.ROLES.MEMBER;
+  }
+
+  setLeaderRole() {
+    this.participant.groupRoleId = CONSTANTS.GROUP.ROLES.LEADER;
   }
 
   isLeader() {
-    return this.participant.isLeader();
+    return (this.participant.groupRoleId === CONSTANTS.GROUP.ROLES.LEADER);
   }
 
+  setApprenticeRole() {
+    this.participant.groupRoleId = CONSTANTS.GROUP.ROLES.APPRENTICE;
+  }
   isApprentice() {
-    return this.participant.isApprentice();
+    return (this.participant.groupRoleId === CONSTANTS.GROUP.ROLES.APPRENTICE);
   }
 
   leaderDisabled() {
