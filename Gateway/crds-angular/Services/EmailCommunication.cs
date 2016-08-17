0bb8080f72015ea8e4c0df255f5b6db8137d9d6d
Rick Barker <rbarker@callibrity.com>
2016-08-17 09:43:46 -0400

    US4536-send-email-reminder

--- Gateway/crds-angular/Services/EmailCommunication.cs
+++ Gateway/crds-angular/Services/EmailCommunication.cs
@@ -42,15 +42,12 @@ namespace crds_angular.Services
                 throw (new InvalidOperationException("Must provide either email.FromUserId from  or an authentication token."));
             }
 
-            //ReplyToContact - If passed in, use it.  Otherwise, get from template.
             var replyToContactId = email.ReplyToContactId ?? template.ReplyToContactId;
             var replyTo = new MpContact { ContactId = replyToContactId, EmailAddress = _communicationService.GetEmailFromContactId(replyToContactId) };
 
-            //FromContact - If passed in, use it.  Otherwise, get from template.
             var fromContactId = email.FromContactId ?? template.FromContactId;
-            var from = new MpContact { ContactId = DefaultContactEmailId, EmailAddress = _communicationService.GetEmailFromContactId(fromContactId) };
+            var from = new MpContact { ContactId = fromContactId, EmailAddress = _communicationService.GetEmailFromContactId(fromContactId) };
 
-            //ToContacts - If passed in, use it.  Otherwise, get from emailAddress.  If no contact for the emailAddress, use the default.
             MpContact to = GetMpContactFromEmailCommunicationDto(email);
             
             var communication = new MpCommunication
@@ -79,32 +76,33 @@ namespace crds_angular.Services
         private MpContact GetMpContactFromEmailCommunicationDto(EmailCommunicationDTO email)
         {
             MpContact to;
-            if (email.ToContactId.HasValue)
-            {
-                to = new MpContact { ContactId = email.ToContactId.Value, EmailAddress = _communicationService.GetEmailFromContactId(email.ToContactId.Value) };
-            }
-            else if (email.emailAddress != null)
-            {
-                var contactId = 0;
-                try
-                {
-                    contactId = _contactService.GetContactIdByEmail(email.emailAddress);
-                }
-                catch (Exception)
-                {
-                    //work around incorrectly handled case where multiple contacts exists for a single contact
-                    contactId = 0;
-                }
-                if (contactId == 0)
-                {
-                    contactId = DefaultContactEmailId;
-                }
-                to = new MpContact { ContactId = contactId, EmailAddress = email.emailAddress };
-            }
-            else
+
+            if (!email.ToContactId.HasValue && email.emailAddress == null)
             {
                 throw (new InvalidOperationException("Must provide either ToContactId or emailAddress."));
             }
+
+            if (email.ToContactId.HasValue)
+            {
+                return new MpContact { ContactId = email.ToContactId.Value, EmailAddress = _communicationService.GetEmailFromContactId(email.ToContactId.Value) };
+            }
+           
+            var contactId = 0;
+            try
+            {
+                contactId = _contactService.GetContactIdByEmail(email.emailAddress);
+            }
+            catch (Exception)
+            {
+                //work around incorrectly handled case where multiple contacts exists for a single contact
+                contactId = 0;
+            }
+            if (contactId == 0)
+            {
+                contactId = DefaultContactEmailId;
+            }
+            to = new MpContact { ContactId = contactId, EmailAddress = email.emailAddress };
+
             return to;
         }
 
