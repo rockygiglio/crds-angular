a39acf2ce463dd49b2a790526dae1ae318edec8a
Gabriel Smith <gabriel.smith01@gmail.com>
2016-08-18 14:41:49 -0400

    Working on fixes

--- crossroads.net/app/streaming/sticky-header.directive.ts
+++ crossroads.net/app/streaming/sticky-header.directive.ts
@@ -11,11 +11,26 @@ export class StickyHeaderDirective {
     this.el = el.nativeElement;
   }
 
-  @HostListener('window:scroll', ['$event']) handleScrollEvent(e) {
-    if (window.pageYOffset > 92) {
+  @HostListener('window:scroll', ['$event']) 
+  handleScrollEvent(e) {
+    let header = document.getElementById('countdown');
+    let intro  = document.getElementById('intro');
+    let fixed  = this.el.classList.contains('fixed-header')
+    let offset = this.el.getBoundingClientRect().top;
+    // console.log(`offset: ${window.pageYOffset}, rect: ${this.el.getBoundingClientRect().top}`);
+    
+    if (offset <= 0 && !fixed) {
+      console.log('adding')
       this.el.classList.add('fixed-header');
-    } else {
+      header.classList.add('animated');
+      header.classList.add('fadeInTop');
+      intro.style.marginTop = header.offsetHeight.toString();
+    } else if (offset > 0 && fixed) {
+      console.log('removing')
       this.el.classList.remove('fixed-header');
+      header.classList.remove('animated');
+      header.classList.remove('fadeInTop');
+      intro.style.marginTop = '';
     }
   }
 }
--- crossroads.net/app/streaming/streaming.component.ts
+++ crossroads.net/app/streaming/streaming.component.ts
@@ -4,8 +4,8 @@ import { CountdownComponent } from './countdown.component';
 import { SocialSharingComponent } from './social-sharing.component';
 import { StreamspotService } from './streamspot.service';
 import { DynamicContentNg2Component } from '../../core/dynamic_content/dynamic-content-ng2.component'
-import { PageScroll } from '../ng2-page-scroll/ng2-page-scroll.component';
-import { PageScrollConfig } from '../ng2-page-scroll/ng2-page-scroll-config';
+// import { PageScroll } from '../ng2-page-scroll/ng2-page-scroll.component';
+// import { PageScrollConfig } from '../ng2-page-scroll/ng2-page-scroll-config';
 import { StickyHeaderDirective } from './sticky-header.directive';
 
 var WOW = require('wow.js/dist/wow.min.js');
@@ -13,7 +13,8 @@ var $:any = require('jquery');
 
 @Component({
   selector: 'streaming',
-  directives: [DynamicContentNg2Component, ScheduleComponent, CountdownComponent, SocialSharingComponent, PageScroll, StickyHeaderDirective],
+  // directives: [DynamicContentNg2Component, ScheduleComponent, CountdownComponent, SocialSharingComponent, PageScroll, StickyHeaderDirective],
+  directives: [DynamicContentNg2Component, ScheduleComponent, CountdownComponent, SocialSharingComponent, StickyHeaderDirective],
   templateUrl: './streaming.ng2component.html'
 })
 
@@ -22,21 +23,38 @@ export class StreamingComponent {
 
   constructor(private streamspotService: StreamspotService) {
 
-    PageScrollConfig.defaultScrollOffset = -10; 
-    PageScrollConfig.defaultEasingFunction = (t:number, b:number, c:number, d:number):number => {
-      if (t === 0) return b;
-      if (t === d) return b + c;
-      if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-      return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    };
+    // PageScrollConfig.defaultScrollOffset = -10; 
+    // PageScrollConfig.defaultEasingFunction = (t:number, b:number, c:number, d:number):number => {
+    //   if (t === 0) return b;
+    //   if (t === d) return b + c;
+    //   if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+    //   return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    // };
 
     this.streamspotService.isBroadcasting.subscribe((inProgress: boolean) => {
       this.inProgress = inProgress;
     });
 
-    new WOW({
-      offset: 100,
-      mobile: false
-    }).init();
+    window.addEventListener('scroll', function(e) {
+      var wrapper   = document.getElementById('wrapper');
+      var header    = document.getElementById('countdown');
+      var intro     = document.getElementById('intro');
+      if (wrapper.getBoundingClientRect().top <= 0) {
+        wrapper.classList.add('fixed-header');
+        header.classList.add('animated');
+        header.classList.add('fadeInTop');
+        intro.style.marginTop = intro.offsetHeight.toString();
+      } else {
+        wrapper.classList.remove('fixed-header');
+        header.classList.remove('animated');
+        header.classList.remove('fadeInTop');
+        intro.style.marginTop = '';
+      }
+    });
+
+    // new WOW({
+    //   offset: 100,
+    //   mobile: false
+    // }).init();
   }
 }
