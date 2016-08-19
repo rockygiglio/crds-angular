cf8b76bdd34057d67fba25ae126fa16011b140be
Samuel Mello <samueljmello@gmail.com>
2016-08-19 12:59:40 -0400

    US4584: Adjustments

--- CI/SQL/20160818_000000_US4584-add-streaming-notification-email-template.sql
+++ CI/SQL/20160818_000000_US4584-add-streaming-notification-email-template.sql
@@ -1,9 +1,34 @@
 USE [MinistryPlatform]
 GO
 
-DECLARE @TemplateID int = 264567
-DECLARE @Body VARCHAR(max) = "<div style='font-family: Verdana; font-size: 12px;'><div>Hi there!<br/><br/></div><div>Welcome to the [Event_Date] [Event_Start_Time] Crossroads service.<br/><br/></div><div><a href='https://[BaseUrl]/live/stream'>Click here to join us.</a><br/><br/></div><div>We're glad you're here,<br/></div><div>-Crossroads</br></div></div>"
-DECLARE @Subject VARCHAR(max) = 'Crossroads Live stream is starting'
+DECLARE @ContactID int = 768371;
+DECLARE @TemplateID int = 264567;
+DECLARE @Body VARCHAR(max) = '<div style="font-family: Verdana; font-size: 12px;"><div>Hi there!<br/><br/></div><div>Welcome to the [Event_Date] [Event_Start_Time] Crossroads service.<br/><br/></div><div><a href="https://[BaseUrl]/live/stream">Click here to join us.</a><br/><br/></div><div>We''re glad you''re here,<br/></div><div>-Crossroads</br></div></div>';
+DECLARE @Subject VARCHAR(max) = 'Crossroads Live stream is starting';
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Contacts] WHERE [Contact_ID] = @ContactID)
+BEGIN
+	SET IDENTITY_INSERT [dbo].[Contacts] ON
+	INSERT INTO [Contacts]
+	(
+		[Contact_ID],
+		[Company],
+		[Display_Name],
+		[Contact_Status_ID],
+		[Email_Address],
+		[Domain_ID]
+	)
+	VALUES
+	(
+		@ContactID,
+		1,
+		'Crossroads Anywhere',
+		1,
+		'anywhere@crossroads.net',
+		1
+	)
+	SET IDENTITY_INSERT [dbo].[Contacts] OFF
+END
 
 IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Communications] WHERE Communication_ID = @TemplateID)
 BEGIN
@@ -29,8 +54,8 @@ BEGIN
 		,@Body
 		,1
 		,GetDate()
-		,1519180
-		,1519180
+		,@ContactID
+		,@ContactID
 		,1
 		,1
 	)
@@ -42,8 +67,8 @@ BEGIN
    UPDATE [dbo].[dp_Communications]
    SET [Body] = @Body
       ,[Subject] = @Subject
-	  ,[From_Contact] = 1519180
-	  ,[Reply_to_Contact] = 1519180
+	  ,[From_Contact] = @ContactID
+	  ,[Reply_to_Contact] = @ContactID
 	  ,[Template] = 1
 	  ,[Active] = 1
    WHERE Communication_ID = @TemplateID
