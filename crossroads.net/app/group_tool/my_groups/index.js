e0c67aa13735ed3a48f8ecd4fc233e2ed354351c
Samuel Mello <samueljmello@gmail.com>
2016-08-26 09:56:16 -0400

    US4753: Disable debug

--- crossroads.net/app/group_tool/my_groups/index.js
+++
@@ -1,9 +0,0 @@
-import MyGroupsComponent from './myGroups.component';
-import CONSTANTS from 'crds-constants';
-import html from './myGroups.html';
-
-export default angular.
-  module(CONSTANTS.MODULES.GROUP_TOOL).
-  component('myGroups', MyGroupsComponent());
-
-import groupCard from './group_card'
--- crossroads.net/spec-core/spec_index.js
+++
@@ -1,8 +0,0 @@
-require('../core/core');
-require('../core/ang');
-
-// require all modules ending in ".spec" from the
-// current directory and all subdirectories
-
-var testsContext = require.context('./', true, /.spec$/);
-testsContext.keys().forEach(testsContext);
