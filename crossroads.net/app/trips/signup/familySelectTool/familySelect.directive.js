787424dd5212827a70b862b504d396b57da28615
Matt Silbernagel <silbermm@gmail.com>
2016-08-26 13:36:14 -0400

    Family Select tool resets form
    [US4715](https://rally1.rallydev.com/#/27593501023d/detail/userstory/60826348107)
    
    When click to signup new family member, reset the form

--- crossroads.net/app/trips/signup/familySelectTool/familySelect.directive.js
+++ crossroads.net/app/trips/signup/familySelectTool/familySelect.directive.js
@@ -39,6 +39,8 @@
       }
 
       function goToApp(contactId) {
+        var campaign = scope.signupService.campaign;
+        scope.signupService.reset(campaign);
         scope.signupService.contactId = contactId;
         $state.go('tripsignup.application.page',
           {
