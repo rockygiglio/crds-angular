c2fce24b77bb99fbb80914287a202a3b9798d8fa
Jim Kriz <jim.kriz@ingagepartners.com>
2016-08-29 09:06:57 -0400

    US4763: Undo changes to svgicon directive, was breaking other places

--- crossroads.net/core/components/svgIcon.directive.js
+++ crossroads.net/core/components/svgIcon.directive.js
@@ -8,12 +8,11 @@
     return {
       restrict: 'E',
       scope: {
-        'icon': '@?icon',
-        'classes': '@?classes'
+        'icon': '@?icon'
       },
       link: function link(scope, el, attr){
-        el.replaceWith('<svg viewBox=\'0 0 32 32\' class=\'icon icon-' + 
-            scope.icon + (scope.classes ? ' ' + scope.classes : '') +  
+        el.append('<svg viewBox=\'0 0 32 32\' class=\'icon icon-' + 
+            scope.icon   +  
             '\'><use xlink:href=\'#' + 
             scope.icon + '\'></use> </svg>'); 
       }
