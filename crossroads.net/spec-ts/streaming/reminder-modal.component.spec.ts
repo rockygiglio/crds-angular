ec4ef284721f8a53a6bc49347ba159714bec14aa
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-01 13:45:49 -0400

    Tests for components and reminder service

--- crossroads.net/spec-ts/streaming/reminder-modal.component.spec.ts
+++ crossroads.net/spec-ts/streaming/reminder-modal.component.spec.ts
@@ -1,27 +1,35 @@
 import { provide } from '@angular/core';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { describe, it, expect, inject, beforeEach, addProviders } from '@angular/core/testing';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
+import { fakeAsync, beforeEachProviders, describe, it, expect, inject, beforeEach, addProviders } from '@angular/core/testing';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 // import { TestComponentBuilder, ComponentFixture } from '@angular/compiler/testing';
 
 import { ReminderModalComponent } from '../../app/streaming/reminder-modal.component';
 import { StreamspotService } from '../../app/streaming/streamspot.service';
 import { MockStreamspotService } from '../core/mocks/mock-streamspot.service';
+import { mockHttpProvider } from '../core/mocks/mock-http.provider.ts'
 
 var moment = require('moment-timezone');
 
 describe('Component: ReminderModal', () => {
 
-  beforeEach(() =>
-    addProviders([
-      { provide: StreamspotService, useClass: MockStreamspotService }
-    ])
-  );
+  beforeEachProviders(() => {
+    return [
+      provide(StreamspotService, MockStreamspotService),
+      provide(Http, mockHttpProvider)
+    ]
+  })
 
   it('should create an instance', () => {
-    let streamspotService = new MockStreamspotService();
-    let component = new ReminderModalComponent(streamspotService);
-    expect(component).toBeTruthy();
+    inject(
+      [Http],
+      (http: Http) => {
+        let streamspotService = new MockStreamspotService();
+        let component = new ReminderModalComponent(streamspotService, http);
+        expect(component).toBeTruthy();
+      }
+    )
+    
   });
   
 });
