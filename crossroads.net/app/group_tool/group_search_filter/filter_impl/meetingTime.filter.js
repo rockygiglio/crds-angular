10914ec1801e30913fcdbb3cd38689259a0c6982
Jim Kriz <jim.kriz@ingagepartners.com>
2016-09-07 12:02:54 -0400

    US4914: Initial group search filter for time

---
+++ crossroads.net/app/group_tool/group_search_filter/filter_impl/meetingTime.filter.js
@@ -0,0 +1,41 @@
+
+import {SearchFilter, SearchFilterValue} from './searchFilter'; 
+
+class TimeRange {
+  constructor(beginTime, endTime) {
+    this.beginTime = moment(beginTime, 'HH:mm:ss');
+    this.endTime = moment(endTime, 'HH:mm:ss');
+  }
+
+  isWithinTimeRange(meetingTime) {
+    let m = moment(meetingTime, 'HH:mm:ss');
+    return m.isBetween(this.beginTime, this.endTime);
+  }
+}
+
+export default class MeetingTimeFilter extends SearchFilter {
+  constructor(filterName) {
+    let filterValues = [
+      new SearchFilterValue('Mornings (before noon)', new TimeRange('00:00:00', '12:00:00'), false),
+      new SearchFilterValue('Afternoons (12 - 5pm)', new TimeRange('11:59:59', '17:00:01'), false),
+      new SearchFilterValue('Evenings (after 5pm)', new TimeRange('17:00:00', '00:00:00'), false)
+    ];
+
+    super(filterName, filterValues, this._matchingFunction);
+  }
+
+  _matchingFunction(result) {
+    // Guard against errors if group has no meeting time set.  Shouldn't happen, but just in case...
+    if(result.meetingTime === undefined) {
+      return false;
+    }
+
+    let selectedMeetingTime = this.getSelectedValues();
+    
+    let filtered = selectedMeetingTime.filter((v) => {
+      return v.getValue().isWithinTimeRange(result.meetingTime);
+    });
+
+    return filtered !== undefined && filtered.length > 0;
+  }
+}
