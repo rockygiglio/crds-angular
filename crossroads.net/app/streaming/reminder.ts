4f7a617721be094028acaa90a5199bc15740fc50
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-07 14:51:29 -0400

    Add timezone to unit tests

--- crossroads.net/app/streaming/reminder.ts
+++ crossroads.net/app/streaming/reminder.ts
@@ -9,7 +9,7 @@ export class Reminder {
   public phone:        string;
   public email:        string;
   public type:         string  = 'phone';
-  private startDate:   string;
+  public startDate:    string;
   public isDayValid:   boolean;
   public isTimeValid:  boolean;
   public isPhoneValid: Boolean;
@@ -25,7 +25,7 @@ export class Reminder {
     let result = null;
     let time = this.time.slice(0, this.time.length - 4); // strip of tz code and leading space
 
-    this.startDate = moment().tz(`${this.day} ${time}`, "America/New_York").format();
+    this.startDate = moment.tz(`${this.day} ${time}`, 'MM/DD/YYYY h:mma', "America/New_York").format();
 
     if (this.isValid()) {
       switch(this.type) {
