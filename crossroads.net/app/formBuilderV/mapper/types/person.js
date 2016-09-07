7dca080b83e2f1f96de6782dd95352f07ec94a6e
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2016-09-07 09:37:53 -0400

    mockJSON for person form fields

--- crossroads.net/app/formBuilderV/mapper/types/person.js
+++ crossroads.net/app/formBuilderV/mapper/types/person.js
@@ -2,61 +2,75 @@
 
 export default class Person {
 
-  constructor(jsonObject) {
-    if (jsonObject) {
+    constructor(jsonObject) {
+        if (jsonObject) {
 
-      Object.assign(this, jsonObject);
+            Object.assign(this, jsonObject);
+        }
     }
-  }
+
+    createTestObject() {
+        let mockJSON = {
+            'Person': {
+                'saveURL': '/api/formBuilder/saveStrategy',
+                'fields': [
+                    {
+                        'data-type': 'string',
+                        'max-length': 50,
+                        'multi-select': false,
+                        'name': 'firstName',
+                        'nullable': true,
+                        'validations': [
+                            {
+                                'description': 'required to save',
+                                'rule': 'required',
+                            },
+                            {
+                                'description': 'max length',
+                                'rule': 40,
+                            },
+                        ]
+                    },
+                    {
+                        'data-type': 'string',
+                        'max-length': 50,
+                        'multi-select': false,
+                        'name': 'lastName',
+                        'nullable': true,
+                        'validations': [
+                            {
+                                'description': 'required to save',
+                                'rule': 'required',
+                            },
+                            {
+                                'description': 'max length',
+                                'rule': 45,
+                            },
+                        ]
+                    },
+                    {
+                        'data-type': 'string',
+                        'max-length': 50,
+                        'multi-select': false,
+                        'name': 'nickName',
+                        'nullable': true,
+                        'validations': [
+                            {
+                                'description': 'required to save',
+                                'rule': 'required',
+                            },
+                            {
+                                'description': 'max length',
+                                'rule': 30,
+                            },
+                        ]
+                    },
+                ]
+            }
+        };
+
+        return new person(mockJSON);
+    }
 
 }
 
-
-// {
-//     'Object': {
-//         'name': '',
-//         'saveURL': '',
-//         'fields': [
-//             {
-//                 'data-type': '',
-//                 'max-length': '',
-//                 'multi-select': '',
-//                 'name': '',
-//                 'nullable': true,
-//                 'saveURL': '',
-//                 'validations': [
-//                     {
-//                         'description': '',
-//                         'rule': '',
-//                         'type': '',
-//                     },
-//                     {
-//                         'description': '',
-//                         'rule': '',
-//                         'type': '',
-//                     },
-//                 ]
-//             },
-//             {
-//                 'data-type': '',
-//                 'max-length': '',
-//                 'multi-select': '',
-//                 'name': '',
-//                 'nullable': true,
-//                 'saveURL': '',
-//                 'validations': [
-//                     {
-//                         'description': '',
-//                         'rule': '',
-//                         'type': '',  
-//                     },
-//                     {
-//                         'description': '',
-//                         'rule': '',
-//                         'type': '',
-//                     },
-//                 ]
-//             },
-//         ]
-//     }
-// }
