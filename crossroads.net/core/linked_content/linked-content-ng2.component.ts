c29b6c1d147c800cce4e0f7f3a4c73205f3b0f43
Taylor C. MacDonald <tcmacdonald@gmail.com>
2016-09-09 15:46:57 -0400

    DE1894 - DP Images Refactor crds-card element for background images

--- crossroads.net/core/linked_content/linked-content-ng2.component.ts
+++ crossroads.net/core/linked_content/linked-content-ng2.component.ts
@@ -5,7 +5,7 @@ import { NgClass } from '@angular/common';
   selector: 'linked-content',
   directives: [NgClass],
   template: `
-    <a href="{{href}}" (click)="onClick($event)" [ngClass]="{ unclickable: !isLinked() }" class="linked-content {{class}}" title="{{title}}" target="{{target}}">
+    <a href="{{href}}" (click)="onClick($event)" [ngClass]="{ unclickable: !isLinked() }" class="linked-content {{class}}" title="{{title}}" target="{{target}}" [attr.data-src]="background">
       <ng-content></ng-content>
     </a>
   `,
@@ -23,7 +23,8 @@ export class LinkedContentNg2Component {
   @Input() target = '_self';  
   @Input() title = '';
   @Input() class = '';
-  
+  @Input() background;
+
   onClick(event) {
     if(!this.isLinked()) {
       event.preventDefault();
@@ -32,10 +33,16 @@ export class LinkedContentNg2Component {
 
   ngOnInit() {
     this.href = this.isLinked() ? this.href : '#';
+    this.parseBackground();
   }
 
   isLinked() {
     return (this.href !== undefined && this.href !== '' && this.href !== '#' && this.href !== 'javascript:;');
   }
 
+  parseBackground() {
+    if(this.background != undefined) {
+      this.background = this.background.replace(/https?:\/\/[^/]*\/(crds-cms-uploads\/)?/, "//crds-cms-uploads.imgix.net/");
+    }
+  }
 }
