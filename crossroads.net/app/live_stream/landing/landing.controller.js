bcf5a34ef3fc7c7ef5f5b7d4d380c0d3d4e290c8
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-13 14:38:21 -0400

    Implement wow js

--- crossroads.net/app/live_stream/landing/landing.controller.js
+++ crossroads.net/app/live_stream/landing/landing.controller.js
@@ -1,7 +1,14 @@
+var WOW = require('wow.js/dist/wow.min.js');
+
 export default class LandingController {
   /*@ngInject*/
   constructor($state, $rootScope) {
     this.state = $state;
     this.rootScope = $rootScope;
+
+    new WOW({
+      offset: 100,
+      mobile: false
+    }).init();
   }
 }
