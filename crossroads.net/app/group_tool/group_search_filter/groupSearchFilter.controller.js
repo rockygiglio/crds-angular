b657f4eedd59eeb94a7786d50480ee214b4b005e
Jim Kriz <jim.kriz@ingagepartners.com>
2016-09-14 14:54:15 -0400

    US4978: Render and control day/time/frequency filters together

--- crossroads.net/app/group_tool/group_search_filter/groupSearchFilter.controller.js
+++ crossroads.net/app/group_tool/group_search_filter/groupSearchFilter.controller.js
@@ -1,6 +1,6 @@
+
 import debounce from 'lodash/function/debounce';
 
-import {SearchFilterValue} from './filter_impl/searchFilter';
 import AgeRangeFilter from './filter_impl/ageRange.filter'; 
 import CategoryFilter from './filter_impl/category.filter'; 
 import KidsWelcomeFilter from './filter_impl/kidsWelcome.filter'; 
@@ -9,7 +9,8 @@ import GroupTypeFilter from './filter_impl/groupType.filter';
 import MeetingDayFilter from './filter_impl/meetingDay.filter';
 import MeetingTimeFilter from './filter_impl/meetingTime.filter';
 import FrequencyFilter from './filter_impl/frequency.filter'; 
-import LeadersSiteFilter from './filter_impl/leadersSite.filter'; 
+import LeadersSiteFilter from './filter_impl/leadersSite.filter';
+import FilterGroup from './filter_impl/filterGroup'; 
 
 const APPLY_FILTER_DEBOUNCE = 750;
 
@@ -18,14 +19,8 @@ export default class GroupSearchResultsController {
   constructor(GroupService, CreateGroupService) {
     this.groupService = GroupService;
     this.createGroupService = CreateGroupService;
-    this.ageRanges = [];
-    this.groupTypes = [];
-    this.days = [];
-    this.categories = [];
-    this.frequencies = [];
-    this.leadersSite = [];
     this.expanded = false;
-    this.allFilters = [];
+    this.allFilters = undefined;
     this.expandedFilter = null;
 
     this.applyFilters = debounce(this._internalApplyFilters, APPLY_FILTER_DEBOUNCE);
@@ -41,32 +36,26 @@ export default class GroupSearchResultsController {
   }
 
   initializeFilters() {
-    this.allFilters = [
-      // TODO - When new filters are implemented, add them here - they will display in the order specified in this array
-      new AgeRangeFilter('Age Range', this.ageRanges),
-      new CategoryFilter('Category', this.categories),
-      new GroupTypeFilter('Group Type', this.groupTypes),
+    this.allFilters = new FilterGroup('All Filters', [
+      new AgeRangeFilter('Age Range', this.groupService),
+      new CategoryFilter('Category', this.groupService),
+      new GroupTypeFilter('Group Type', this.groupService),
       new KidsWelcomeFilter('Kids Welcome'),
       new LocationFilter('Location'),
-      new MeetingDayFilter('Day', this.days),
-      new MeetingTimeFilter('Time'),
-      new FrequencyFilter('Frequency', this.frequencies),
-      new LeadersSiteFilter('Leaders Site', this.leadersSite)
-    ];
-
-    this.loadAgeRanges();
-    this.loadGroupTypes();
-    this.loadDays();
-    this.loadCategories();
-    this.loadFrequencies();
-    this.loadLeadersSite();
+      new FilterGroup('Day / Time / Frequency', [
+        new MeetingDayFilter('Day', this.groupService),
+        new MeetingTimeFilter('Time'),
+        new FrequencyFilter('Frequency', this.createGroupService),
+      ]),
+      new LeadersSiteFilter('Leaders Site', this.groupService)
+    ]);
   }
 
   _internalApplyFilters() {
     let settings = {
       dataset: this.searchResults.filter((r) => {
-        for (let i = 0; i < this.allFilters.length; i++) {
-          if(!this.allFilters[i].matches(r)) {
+        for (let i = 0; i < this.allFilters.getValues().length; i++) {
+          if(!this.allFilters.getValues()[i].matches(r)) {
             return false;
           }
         }
@@ -79,10 +68,7 @@ export default class GroupSearchResultsController {
   }
 
   clearFilters() {
-    this.allFilters.forEach(function(f) {
-      f.clear();
-    }, this);
-
+    this.allFilters.clear();
     this.applyFilters();
   }
 
@@ -92,11 +78,12 @@ export default class GroupSearchResultsController {
   }
 
   getCurrentFilters() {
-    return this.allFilters.filter((f) => f.isActive());
+    // return this.allFilters.getValues().filter((f) => f.isActive());
+    return this.allFilters.getCurrentFilters();
   }
 
   hasFilters() {
-    return this.allFilters.find((f) => f.isActive()) !== undefined;
+    return this.allFilters.getValues().find((f) => f.isActive()) !== undefined;
   }
 
   openFilters() {
@@ -123,89 +110,5 @@ export default class GroupSearchResultsController {
 
   isOpenFilter(filter) {
     return this.expandedFilter === filter;
-  }
-
-  loadAgeRanges() {
-    this.groupService.getAgeRanges().then(
-      (data) => {
-        this.ageRanges.push.apply(this.ageRanges, data.attributes.map((a) => {
-          return new SearchFilterValue(a.name, a.attributeId, false);
-        }));
-      },
-      (err) => {
-        // TODO what happens on error? (could be 404/no results, or other error)
-      }
-    ).finally(
-      () => {
-      });
-  }
-
-  loadGroupTypes() {
-    this.groupService.getGroupGenderMixType().then(
-      (data) => {
-        this.groupTypes.push.apply(this.groupTypes, data.attributes.map((a) => {
-          return new SearchFilterValue(a.name, a.attributeId, false);
-        }));
-      },
-      (/*err*/) => {
-        // TODO what happens on error? (could be 404/no results, or other error)
-      }).finally(
-        () => {
-      });
-  }
-
-  loadCategories() {
-    this.groupService.getGroupCategories().then(
-      (data) => {
-        this.categories.push.apply(this.categories, data.map((c) => {
-          return new SearchFilterValue(c.label, c.categoryId, false, c.labelDesc);
-        }));
-      },
-      (/*err*/) => {
-        // TODO what happens on error? (could be 404/no results, or other error)
-      }).finally(
-        () => {
-      });
-  }  
-
-  loadDays() {
-    this.groupService.getDaysOfTheWeek().then(
-      (data) => {
-        data = _.sortBy( data, 'dp_RecordID' );
-        data.push({dp_RecordID: 0, dp_RecordName: 'Flexible Meeting Time'});
-        this.days.push.apply(this.days, data.map((a) => {
-          return new SearchFilterValue(a.dp_RecordName, a.dp_RecordID, false);
-        }));
-      },
-      (err) => {
-        // TODO what happens on error? (could be 404/no results, or other error)
-      }
-    ).finally(
-      () => {
-      });
-  }
-
-  loadFrequencies() {
-    let frequencies = this.createGroupService.getMeetingFrequencies();
-    frequencies = _.sortBy( frequencies, 'meetingFrequencyId' );
-
-    this.frequencies.push.apply(this.frequencies, frequencies.map((a) => {
-      return new SearchFilterValue(a.meetingFrequencyDesc, a.meetingFrequencyId, false);
-    }));
-  }
-
-  loadLeadersSite() {
-    this.groupService.getSites().then(
-      (data) => {
-        data = _.sortBy( data, 'dp_RecordID' );
-        this.leadersSite.push.apply(this.leadersSite, data.map((a) => {
-          return new SearchFilterValue(a.dp_RecordName, a.dp_RecordID, false);
-        }));
-      },
-      (/*err*/) => {
-        // TODO what happens on error? (could be 404/no results, or other error)
-      }).finally(
-        () => {
-      });
   }
 }
