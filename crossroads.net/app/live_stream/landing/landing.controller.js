2fa51f6e64e9a91d1321f22e91155680d54412f9
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-15 14:32:31 -0400

    Inject StreamspotService into landing controller

--- crossroads.net/app/live_stream/landing/landing.controller.js
+++ crossroads.net/app/live_stream/landing/landing.controller.js
@@ -2,13 +2,18 @@ var WOW = require('wow.js/dist/wow.min.js');
 
 export default class LandingController {
   /*@ngInject*/
-  constructor($state, $rootScope) {
+  constructor($state, $rootScope, StreamspotService) {
     this.state = $state;
     this.rootScope = $rootScope;
+    this.streamspotService = StreamspotService
 
     new WOW({
       offset: 100,
       mobile: false
     }).init();
+  }
+
+  $onInit() {
+    
   }
 }
