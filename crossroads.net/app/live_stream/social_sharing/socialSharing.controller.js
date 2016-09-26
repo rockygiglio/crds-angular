ef187cfd97990a4290d4fb6370d4b319539a4e4b
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-26 09:10:53 -0400

    Implement social sharing icons

---
+++ crossroads.net/app/live_stream/social_sharing/socialSharing.controller.js
@@ -0,0 +1,14 @@
+export default class SocialSharingController {
+  constructor() {}
+
+  $onInit() {
+    addthis.init();
+    addthis.toolbox('social-share > a', 
+      {pubid: 'ra-5391d6a6145291c4'}, 
+      {
+        url: this.url || window.location.href,
+        title: this.title || document.querySelector('meta[property="og:title"]')['content'],
+        description: this.description || document.querySelector('meta[property="og:description"]')['content']
+      });
+  }
+}
