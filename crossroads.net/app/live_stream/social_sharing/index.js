543f2c691a39e628522b558167b00438be85bcb3
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-26 15:28:40 -0400

    Moved social_sharing to core

--- crossroads.net/app/live_stream/social_sharing/index.js
+++
@@ -1,6 +0,0 @@
-import CONSTANTS from 'crds-constants';
-import socialSharingComponent from './socialSharing.component';
-import html from './countdown.html';
-
-export default angular.module(CONSTANTS.MODULES.LIVE_STREAM)
-  .component('socialSharing', socialSharingComponent());
