f6e5d7b8af1b5ac4762217420b1958eb327f06d0
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-09-27 10:33:45 -0400

    US5101: Fix case on some files that didn't match causing linux to cry.

--- crossroads.net/app/formlyBuilder/formlyConfig/types/index.js
+++ crossroads.net/app/formlyBuilder/formlyConfig/types/index.js
@@ -1,15 +1,15 @@
 export default ngModule => {
-  require('./crdsmultiCheckBoxCombo')(ngModule);
+  require('./crdsMultiCheckBoxCombo')(ngModule);
   require('./zipCodeValidator')(ngModule);
   require('./crdsBoldCheckBox')(ngModule);
   require('./crdsProfilePicture')(ngModule);
   require('./crdsTimePicker')(ngModule);
   require('./crdsDatePicker')(ngModule);
   require('./crdsInput')(ngModule);
-  require('./crdsCheckBox')(ngModule);
+  require('./crdsCheckbox')(ngModule);
   require('./crdsMultiCheckbox')(ngModule);
   require('./crdsRadio')(ngModule);
   require('./crdsRadioDesc')(ngModule);
   require('./crdsSelect')(ngModule);
-  require('./crdsTextarea')(ngModule);
+  require('./crdsTextArea')(ngModule);
 }
