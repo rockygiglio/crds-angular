76d1fe46e3a38d334fddaa4c268bb4ec28842ab1
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-28 11:04:35 -0400

    Code cleanup

--- crossroads.net/app/live_stream/landing/landing.controller.js
+++ crossroads.net/app/live_stream/landing/landing.controller.js
@@ -2,18 +2,10 @@ var WOW = require('wow.js/dist/wow.min.js');
 
 export default class LandingController {
   /*@ngInject*/
-  constructor($state, $rootScope, StreamspotService) {
-    this.state = $state;
-    this.rootScope = $rootScope;
-    this.streamspotService = StreamspotService
-
+  constructor() {
     new WOW({
       offset: 100,
       mobile: false
     }).init();
-  }
-
-  $onInit() {
-    
   }
 }
