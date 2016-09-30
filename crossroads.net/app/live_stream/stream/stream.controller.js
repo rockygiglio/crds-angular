73d87fa5d0a511f1402273ef34992bc1d8b8b6ef
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-30 13:23:07 -0400

    Modified controller for tests; created tests

--- crossroads.net/app/live_stream/stream/stream.controller.js
+++ crossroads.net/app/live_stream/stream/stream.controller.js
@@ -23,35 +23,39 @@ export default class StreamingController {
     this.cmsService
         .getDigitalProgram()
         .then((data) => {
-          data.forEach((feature, i, data) => {
-            // null status indicates a published feature
-            if (feature.status === null || feature.status.toLowerCase() !== 'draft') {
-              feature.delay = i * 100
-              feature.url = 'javascript:;';
-
-              if (feature.link !== null) {
-                feature.url = feature.link;
-              }
-
-              feature.target = '_blank';
-
-              if (typeof feature.image !== 'undefined' && typeof feature.image.filename !== 'undefined') {
-                feature.image = feature.image.filename;
-              } else {
-                feature.image = 'https://crds-cms-uploads.imgix.net/content/images/register-bg.jpg'
-              }
-              if (feature.section === 1 ) {
-                this.dontMiss.push(feature)
-              } else if (feature.section === 2 ) {
-                this.beTheChurch.push(feature);
-              }
-            }
-          })
+          this.sortDigitalProgram(data);
         });
     
     new WOW({
       mobile: false
     }).init();
+  }
+
+  sortDigitalProgram(data) {
+    data.forEach((feature, i, data) => {
+      // null status indicates a published feature
+      if (feature.status === null || feature.status.toLowerCase() !== 'draft') {
+        feature.delay = i * 100
+        feature.url = 'javascript:;';
+
+        if (feature.link !== null) {
+          feature.url = feature.link;
+        }
+
+        feature.target = '_blank';
+
+        if (typeof feature.image !== 'undefined' && typeof feature.image.filename !== 'undefined') {
+          feature.image = feature.image.filename;
+        } else {
+          feature.image = 'https://crds-cms-uploads.imgix.net/content/images/register-bg.jpg'
+        }
+        if (feature.section === 1 ) {
+          this.dontMiss.push(feature)
+        } else if (feature.section === 2 ) {
+          this.beTheChurch.push(feature);
+        }
+      }
+    })
   }
 
   goBack() {
