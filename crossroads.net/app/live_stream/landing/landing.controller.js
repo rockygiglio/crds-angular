9b1e3af540e6110c893f3dd2fa274d0405d45314
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-30 14:28:50 -0400

    Modified and added tests

--- crossroads.net/app/live_stream/landing/landing.controller.js
+++ crossroads.net/app/live_stream/landing/landing.controller.js
@@ -14,27 +14,31 @@ export default class LandingController {
 
     this.cmsService
       .getRecentMessages(4)
-      .then((pastWeekends) => {
-        this.pastWeekends = pastWeekends.map((event, i, pastWeekends) => {
-          if (typeof event.series !== "undefined") {
-            let title = this.filter('replaceNonAlphaNumeric')(event.title);
-
-            event.delay = i * 100;
-            event.subtitle = event.title
-            if (event.number === 0) {
-              event.number++;
-            }
-            event.title = `${event.series.title} #${event.number}`;
-
-            event.url = `/message/${event.id}/${title}`
-            event.image = 'https://crds-cms-uploads.imgix.net/content/images/register-bg.jpg'
-
-            if (typeof event.messageVideo !== "undefined" && typeof event.messageVideo.still !== 'undefined') {
-              event.image = event.messageVideo.still.filename
-            } 
-          }
-          return event;
-        })
+      .then((response) => {
+        this.pastWeekends = this.parseWeekends(response)
       })
+  }
+
+  parseWeekends(response) {
+    return response.map((event, i, pastWeekends) => {
+      if (typeof event.series !== "undefined") {
+        let title = this.filter('replaceNonAlphaNumeric')(event.title);
+
+        event.delay = i * 100;
+        event.subtitle = event.title
+        if (event.number === 0) {
+          event.number++;
+        }
+        event.title = `${event.series.title} #${event.number}`;
+
+        event.url = `/message/${event.id}/${title}`
+        event.image = 'https://crds-cms-uploads.imgix.net/content/images/register-bg.jpg'
+
+        if (typeof event.messageVideo !== "undefined" && typeof event.messageVideo.still !== 'undefined') {
+          event.image = event.messageVideo.still.filename
+        } 
+      }
+      return event;
+    })
   }
 }
