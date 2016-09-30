75aa248f587591d22df4c7b70c12c6f742b6ba5e
Gabriel Smith <gabriel.smith01@gmail.com>
2016-09-30 16:35:36 -0400

    Apply ssid check to tests

--- crossroads.net/spec-es6/live_streaming/streamspot_player/streamspotPlayer.controller.spec.js
+++ crossroads.net/spec-es6/live_streaming/streamspot_player/streamspotPlayer.controller.spec.js
@@ -3,7 +3,7 @@ import constants from 'crds-constants';
 import StreamspotPlayerController from '../../../app/live_stream/streamspot_player/streamspotPlayer.controller';
 import StreamspotService from '../../../app/live_stream/services/streamspot.service';
 
-describe('Streaming Reminder Controller', () => {
+describe('Streampost player', () => {
   let fixture,
       response,
       rootScope,
@@ -144,7 +144,7 @@ describe('Streaming Reminder Controller', () => {
 
   it('should set the iframe src', () => {
     let id = '1adb55de';
-    if ( StreamspotService.ssid === 'crossr30e3' ) {
+    if ( fixture.streamspotService.ssid === 'crossr30e3' ) {
       id = '2887fba1';
     }
 
