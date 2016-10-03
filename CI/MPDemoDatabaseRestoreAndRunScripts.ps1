6aafb127fb40538a9ea4738329dac5c10b2edf5d
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-10-03 12:57:31 -0400

    Skip Database Step If no database changes.

--- CI/MPDemoDatabaseRestoreAndRunScripts.ps1
+++ CI/MPDemoDatabaseRestoreAndRunScripts.ps1
@@ -7,18 +7,29 @@
     [string]$DBUser = $(Get-ChildItem Env:MP_SOURCE_DB_USER).Value, # Default to environment variable
     [string]$DBPassword = $(Get-ChildItem Env:MP_SOURCE_DB_PASSWORD).Value, # Default to environment variable
     [string]$SQLcmd = "C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\sqlcmd.exe",
-    [boolean]$ForceBackup = $FALSE # Default to use existing backup file   
+    [boolean]$ForceBackup = $FALSE # Default to use existing backup file,
+	[string]$changeLogFile = "changelog.txt"
 )
 
-#Use mutex to ensure only 1 process executing against DBServer / DB at a time
-$uniqueName = "MPDemoDatabaseRestoreAndRunScripts$DBServer$DBName" 
-$singleInstanceMutex = New-Object System.Threading.Mutex($false, $uniqueName)
+#This line could be it's own build step. Or we can potentially just reference %system.teamcity.build.changedFiles.file% in the Get-Content call. 
+copy "%system.teamcity.build.changedFiles.file%" changelog.txt
 
-try
-{   
-    $singleInstanceMutex.WaitOne()
+$SQLChanges = @(Get-Content $changeLogFile | Where-Object {$_.StartsWith("CI/SQL")}).Count
 
-    .\CI\MPDemoDatabaseBackup.ps1 -DBServer $BackupDBServer -DBName $DBName -BackupPath $BackupPath -DBUser $DBUser -DBPassword $DBPassword -ForceBackup $ForceBackup
+if($SQLChanges -gt 0)
+{
+    Write-Host SQL Changes:
+    Write-Host (Get-Content $changeLogFile | Where-Object {$_.StartsWith("CI/SQL")})
+	
+	#Use mutex to ensure only 1 process executing against DBServer / DB at a time
+	$uniqueName = "MPDemoDatabaseRestoreAndRunScripts$DBServer$DBName" 
+	$singleInstanceMutex = New-Object System.Threading.Mutex($false, $uniqueName)
+
+	try
+	{   
+		$singleInstanceMutex.WaitOne()
+
+		.\CI\MPDemoDatabaseBackup.ps1 -DBServer $BackupDBServer -DBName $DBName -BackupPath $BackupPath -DBUser $DBUser -DBPassword $DBPassword -ForceBackup $ForceBackup
  
     if($LASTEXITCODE -eq 0) 
     {
@@ -32,9 +43,16 @@ try
     }
 
     exit $LASTEXITCODE
-}
-finally
+	}
+	finally
+	{
+		$singleInstanceMutex.ReleaseMutex()
+		$singleInstanceMutex.Dispose()
+	}
+		
+	}
+else 
 {
-    $singleInstanceMutex.ReleaseMutex()
-    $singleInstanceMutex.Dispose()
+    Write-Host No database changes found.
 }
+
