19ba54d0d241e9d9078799833873cc6ec1245be2
Yan <yyu@callibrity.com>
2016-10-07 12:17:20 -0400

    changed return statement in PageById factory from {url: '@_id'} to {id: '@_id'}

--- crossroads.net/core/cms/services/cms_services_module.js
+++ crossroads.net/core/cms/services/cms_services_module.js
@@ -27,5 +27,5 @@ cms_services_module.factory('PageById', function ($resource, $location) {
         return $resource(__CMS_ENDPOINT__ + '/api/Page/?id=:id&STAGE=:stage', { id: '@_id', stage: stageParam }, { cache: false });
     }
 
-    return $resource(__CMS_ENDPOINT__ + '/api/Page/?id=:id', { url: '@_id' }, { cache: true });
+    return $resource(__CMS_ENDPOINT__ + '/api/Page/?id=:id', { id: '@_id' }, { cache: true });
 })
