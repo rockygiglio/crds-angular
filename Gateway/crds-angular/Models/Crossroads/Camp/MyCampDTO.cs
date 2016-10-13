31a6d90734716b80ece086c664fb5f5023840546
lknair2015 <lknair2015@gmail.com>
2016-10-13 13:33:32 -0400

    assed jsonproperty to MyCampDTO

--- Gateway/crds-angular/Models/Crossroads/Camp/MyCampDTO.cs
+++ Gateway/crds-angular/Models/Crossroads/Camp/MyCampDTO.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace crds_angular.Models.Crossroads.Camp
 {
     public class MyCampDTO
     {
+        [JsonProperty(PropertyName = "Contact_ID")]
         public int CamperContactId { get; set; }
+
+        [JsonProperty(PropertyName = "Nickname")]
         public string CamperNickName { get; set; }
+
+        [JsonProperty(PropertyName = "Last_Name")]
         public string CamperLastName { get; set; }
+
+        [JsonProperty(PropertyName = "Event_Title")]
         public string CampName { get; set; }
+
+        [JsonProperty(PropertyName = "Event_Start_Date")]
         public DateTime CampStartDate { get; set; }
+
+        [JsonProperty(PropertyName = "Event_End_Date")]
         public DateTime CampEndDate { get; set; }
     }
 }
