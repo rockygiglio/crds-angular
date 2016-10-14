1b79aa9a552409c668cc8e8d59984da0b48fb321
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-10-14 10:42:36 -0400

    US3859: Move hard coded value into contants in anticipation of a code review comment.

--- crossroads.net/app/my_serve/serve_team_members/serveTeamMembers.controller.js
+++ crossroads.net/app/my_serve/serve_team_members/serveTeamMembers.controller.js
@@ -38,7 +38,7 @@ export default class ServeTeamMembersController {
 
   splitMembers(opportunities) {
     _.forEach(opportunities, (opportunity) => {
-      let partitionedArray = _.partition(opportunity.rsvpMembers, (member) => {return member.Response_Result_ID === 2});
+      let partitionedArray = _.partition(opportunity.rsvpMembers, (member) => {return member.Response_Result_ID === CONSTANTS.SERVING_RESPONSES.NOT_AVAILABLE});
       this.rsvpNoMembers = this.rsvpNoMembers.concat(partitionedArray[0]);
       partitionedArray = _.partition(partitionedArray[1], (member) => {return member.Group_Role_ID === CONSTANTS.GROUP.ROLES.LEADER});
       this.rsvpYesLeaders = this.rsvpYesLeaders.concat(partitionedArray[0]);
