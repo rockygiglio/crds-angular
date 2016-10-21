d08d6bc5cb7a09535f64e076f1191aa6cb35a62c
Ken Baum <ken.baum@ingagepartners.com>
2016-10-21 10:28:47 -0400

    US5185 cleanup stuff

--- crossroads.net/app/my_serve/serve_team_message/serveTeamMessage.controller.js
+++ crossroads.net/app/my_serve/serve_team_message/serveTeamMessage.controller.js
@@ -24,10 +24,9 @@ export default class ServeTeamMessageController {
     this.teamPeople = this.serveTeamService.getAllTeamMembersByLeader();
     this.tinymceOptions = {
       resize: false,
-      //width: 500,
       height: 300,
       plugins: 'paste link legacyoutput textcolor',
-      //valid_elements: 'ol, ul, li, p, br, b, i, strong, em, a',
+      valid_elements: 'ol,ul,li,p,br,strong/b,i,em,a[href|target=_blank],p,br',
       toolbar: 'undo redo | fontselect fontsizeselect forecolor backcolor | bold italic underline | alignleft aligncenter alignright | numlist bullist outdent indent | link',
       menubar: false,
       statusbar: false
