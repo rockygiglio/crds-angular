2deffaaa6a105574213f8a06fabd44975e092d03
Katie Dwyer <Katie Dwyer>
2016-10-25 14:29:45 -0400

    US5401: Check for existence before creating anything so the builds work

--- CI/SQL/20161024_142000_US5401-CreateDBAndTablesForAttendanceTracker.sql
+++ CI/SQL/20161024_142000_US5401-CreateDBAndTablesForAttendanceTracker.sql
@@ -1,36 +1,85 @@
 USE [master]
 GO
 
-CREATE DATABASE AttendanceTracker
+IF NOT EXISTS (SELECT name FROM master.sys.databases WHERE name = N'AttendanceTracker')
+BEGIN 
+
+CREATE DATABASE AttendanceTracker;
+
+END
 GO
 
 SET ANSI_NULLS ON
-GO
-
 SET QUOTED_IDENTIFIER ON
-GO
+
 
 USE [AttendanceTracker]
-GO
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'entry_type' AND type = 'U')
+BEGIN
+
 CREATE TABLE [dbo].[entry_type](
 	[entry_type_id] [int] IDENTITY(1,1) NOT NULL,
 	[entry_type_name] [nvarchar](255) NULL,
 	[display_order] [int] NULL
 ) ON [PRIMARY]
 
+SET IDENTITY_INSERT [dbo].[entry_type] ON 
+INSERT [dbo].[entry_type] ([entry_type_id], [entry_type_name], [display_order]) VALUES (1, N'Attendees', 0)
+INSERT [dbo].[entry_type] ([entry_type_id], [entry_type_name], [display_order]) VALUES (2, N'Volunteers', 1)
+INSERT [dbo].[entry_type] ([entry_type_id], [entry_type_name], [display_order]) VALUES (3, N'Paid', 2)
+SET IDENTITY_INSERT [dbo].[entry_type] OFF
+
+
+END 
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'ministry' AND type = 'U')
+BEGIN
 CREATE TABLE [dbo].[ministry](
 	[ministry_id] [int] IDENTITY(1,1) NOT NULL,
 	[ministry_name] [nvarchar](255) NULL,
 	[display_order] [int] NULL
 ) ON [PRIMARY]
 
+SET IDENTITY_INSERT [dbo].[ministry] ON 
+INSERT [dbo].[ministry] ([ministry_id], [ministry_name], [display_order]) VALUES (1, N'Adults', 0)
+INSERT [dbo].[ministry] ([ministry_id], [ministry_name], [display_order]) VALUES (2, N'Kids Club', 1)
+INSERT [dbo].[ministry] ([ministry_id], [ministry_name], [display_order]) VALUES (3, N'Middle School', 2)
+INSERT [dbo].[ministry] ([ministry_id], [ministry_name], [display_order]) VALUES (4, N'High School', 3)
+INSERT [dbo].[ministry] ([ministry_id], [ministry_name], [display_order]) VALUES (5, N'BIG', 4)
+INSERT [dbo].[ministry] ([ministry_id], [ministry_name], [display_order]) VALUES (6, N'Streaming', 5)
+SET IDENTITY_INSERT [dbo].[ministry] OFF
+
+END
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'service' AND type = 'U')
+BEGIN
 CREATE TABLE [dbo].[service](
 	[service_id] [int] IDENTITY(1,1) NOT NULL,
 	[service_name] [nvarchar](255) NULL,
 	[service_group] [nvarchar](255) NULL,
 	[display_order] [int] NULL
 ) ON [PRIMARY]
+SET IDENTITY_INSERT [dbo].[service] ON 
 
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (1, N'Saturday 1', NULL, 0)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (2, N'Saturday 2', NULL, 1)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (3, N'Saturday 3', NULL, 2)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (4, N'Sunday 1', NULL, 3)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (5, N'Sunday 2', NULL, 4)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (6, N'Sunday 3', NULL, 5)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (7, N'Sunday 4', NULL, 6)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (8, N'Special Event', NULL, 7)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (9, N'Mid-Week Service', NULL, 8)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (10, N'Student Ministry Camp', NULL, 9)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (11, N'Man Camp', NULL, 10)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (12, N'Woman Camp', NULL, 11)
+INSERT [dbo].[service] ([service_id], [service_name], [service_group], [display_order]) VALUES (14, N'On Demand', NULL, 12)
+SET IDENTITY_INSERT [dbo].[service] OFF
+END
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'service_instance' AND type = 'U')
+BEGIN
 CREATE TABLE [dbo].[service_instance](
 	[service_instance_id] [int] IDENTITY(1,1) NOT NULL,
 	[service_instance_map_id] [int] NULL,
@@ -46,7 +95,10 @@ CREATE TABLE [dbo].[service_instance](
 	[entry_value] [nvarchar](255) NULL,
 	[notes] [nvarchar](255) NULL
 ) ON [PRIMARY]
+END
 
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Sessions' AND type = 'U')
+BEGIN
 CREATE TABLE [dbo].[Sessions](
 	[sid] [nvarchar](32) NOT NULL,
 	[expires] [datetime2](7) NULL,
@@ -54,13 +106,29 @@ CREATE TABLE [dbo].[Sessions](
 	[createdAt] [datetime2](7) NOT NULL,
 	[updatedAt] [datetime2](7) NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
 
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'site' AND type = 'U')
+BEGIN
 CREATE TABLE [dbo].[site](
 	[site_id] [int] IDENTITY(1,1) NOT NULL,
 	[site_name] [nvarchar](255) NULL,
 	[display_order] [int] NULL
-) ON [PRIMARY]
+) ON [PRIMARY]SET IDENTITY_INSERT [dbo].[site] ON 
 
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (1, N'Oakley', 0)
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (2, N'Mason', 1)
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (3, N'Florence', 2)
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (4, N'West Side', 3)
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (5, N'Uptown', 4)
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (6, N'Oxford', 5)
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (7, N'East Side', 6)
+INSERT [dbo].[site] ([site_id], [site_name], [display_order]) VALUES (8, N'Anywhere', 7)
+SET IDENTITY_INSERT [dbo].[site] OFF
+END
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'user' AND type = 'U')
+BEGIN
 CREATE TABLE [dbo].[user](
 	[user_id] [int] IDENTITY(1,1) NOT NULL,
 	[ministry_platform_id] [int] NULL,
@@ -74,6 +142,8 @@ PRIMARY KEY CLUSTERED
 	[user_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+END
+
 
 GO
 
