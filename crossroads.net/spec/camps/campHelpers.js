b73f8ff0a4f9ceb5fcf4faf25d503f5972dd55bb
Matt Silbernagel <silbermm@gmail.com>
2016-10-26 09:07:00 -0400

    Emergency Contact Info - service test
    [US5234](https://rally1.rallydev.com/#/detail/userstory/66840484804?fdp=true)
    
    * add test for emergency contact form service

--- crossroads.net/spec/camps/campHelpers.js
+++ crossroads.net/spec/camps/campHelpers.js
@@ -25,6 +25,13 @@ function CampHelpers() {
       registrationStartDate: '2016-10-16T00:00:00',
       startDate: '2017-01-08T00:00:00'
     },
+    emergencyContactModel: {
+      firstName: 'Jon',
+      lastName: 'Horner',
+      mobileNumber: '513-123-2345',
+      email: null,
+      relationship: 'co-worker'
+    },
     messages: {
       summercampIntro: {
         content: 'summer camp intro text'
