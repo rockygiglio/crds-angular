47c4b8a59f5d563d99964b717adbe1db21209e79
lknair2015 <lknair2015@gmail.com>
2016-11-02 10:44:47 -0400

    updating the medical dto

--- Gateway/crds-angular/Models/Crossroads/Camp/MedicalInfoDTO.cs
+++ Gateway/crds-angular/Models/Crossroads/Camp/MedicalInfoDTO.cs
@@ -15,5 +15,11 @@ namespace crds_angular.Models.Crossroads.Camp
 
         [JsonProperty(PropertyName = "physicianPhone")]
         public string PhysicianPhone { get; set; }
+
+        [JsonProperty(PropertyName = "allergyType")]
+        public string AllergyType { get; set; }
+
+        [JsonProperty(PropertyName = "allergyDescription")]
+        public string AllergyDescription { get; set; }
     }
 }
