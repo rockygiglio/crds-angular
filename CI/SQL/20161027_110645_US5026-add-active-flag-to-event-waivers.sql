8da250d086aaba11b01235b3342c772ce594f790
Phil Lachmann <phil.lachmann@ingagepartners.com>
2016-11-02 09:20:40 -0400

    Fix the build

--- CI/SQL/20161027_110645_US5026-add-active-flag-to-event-waivers.sql
+++ CI/SQL/20161027_110645_US5026-add-active-flag-to-event-waivers.sql
@@ -7,8 +7,12 @@ IF NOT EXISTS(
     WHERE Name      = N'Active'
       AND Object_ID = Object_ID(N'cr_Event_Waivers'))
 BEGIN
-	UPDATE [dbo].[cr_Event_Waivers] set Active = 1;
-
+	
 	ALTER TABLE [dbo].[cr_Event_Waivers] 
 		ADD [Active] [bit] Default 1 not null;
+	 
 END
+GO
+
+UPDATE [dbo].[cr_Event_Waivers] set Active = 1;
+GO
