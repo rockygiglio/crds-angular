a1e1b236d5c7fc91c4d8d7e8222865054b60dfd4
Jon Horner <hornerjn@mail.uc.edu>
2016-11-02 15:44:30 -0400

    US5465 Hide Fields Using Radio Input
    Figured out how to hide form fields with a radio input on the form.

--- crossroads.net/app/camps/application_page/medical_info/medical_info_form.service.js
+++ crossroads.net/app/camps/application_page/medical_info/medical_info_form.service.js
@@ -6,7 +6,9 @@ class MedicalInfoForm {
       insuranceCompanyName: null,
       policyHolderName: null,
       physicianName: null,
-      physicianNumber: null
+      physicianNumber: null,
+      showAllergies: null,
+      medicineAllergies: null
     };
 
     this.medicalInfoResource = $resource(`${__API_ENDPOINT__}api/camps/medical/:contactId`);
@@ -56,6 +58,44 @@ class MedicalInfoForm {
           optionsTypes: ['phoneNumber'],
           templateOptions: {
             label: 'Physician Phone Number',
+            required: false
+          }
+        }]
+      },
+      {
+        className: 'row',
+        fieldGroup: [{
+          className: 'form-group col-xs-6',
+          key: 'showAllergies',
+          type: 'crdsRadio',
+          templateOptions: {
+            label: 'Are there any Allergy/Dietary Needs?',
+            required: true,
+            inline: true,
+            labelProp: 'label',
+            valueProp: 'id',
+            options: [
+              {
+                label: 'Yes',
+                id: true
+              },
+              {
+                label: 'No',
+                id: false
+              }
+            ]
+          }
+        }]
+      },
+      {
+        className: 'row',
+        hideExpression: () => !this.formModel.showAllergies,
+        fieldGroup: [{
+          className: 'form-group col-xs-12',
+          key: 'medicineAllergies',
+          type: 'crdsTextArea',
+          templateOptions: {
+            label: 'Medicine Allergies',
             required: false
           }
         }]
