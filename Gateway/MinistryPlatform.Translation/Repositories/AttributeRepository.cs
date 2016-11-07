67db51a987e49465abe90419348208b36f9b1bcd
Joe Kerstanoff <jkerstanoff@callibrity.com>
2016-11-07 14:37:47 -0500

    US5447: removed unused methods.

--- Gateway/MinistryPlatform.Translation/Repositories/AttributeRepository.cs
+++ Gateway/MinistryPlatform.Translation/Repositories/AttributeRepository.cs
@@ -37,12 +37,6 @@ namespace MinistryPlatform.Translation.Repositories
             return records;
         }
 
-        public List<MpAttribute> GetAttributesByFilter(string filter)
-        {
-            var token = base.ApiLogin();
-            return _ministryPlatformService.GetPageViewRecords(_attributesByTypePageViewId, token, filter).Select(MapMpAttribute).ToList();
-        }
-
         public int CreateAttribute(MpAttribute attribute)
         {
             var token = base.ApiLogin();
@@ -56,25 +50,6 @@ namespace MinistryPlatform.Translation.Repositories
                     };
 
             return _ministryPlatformService.CreateRecord(_attributesPageId, values, token, true);
-        }
-
-
-        private MpAttribute MapMpAttribute(Dictionary<string, object> record)
-        {
-            return new MpAttribute
-            {
-
-                AttributeId = record.ToInt("Attribute_ID"),
-                Name = record.ToString("Attribute_Name"),
-                Description = record.ToString("Attribute_Description"),
-                CategoryId = record.ToNullableInt("Attribute_Category_ID"),
-                Category = record.ToString("Attribute_Category"),
-                CategoryDescription = record.ToString("Attribute_Category_Description"),
-                AttributeTypeId = record.ToInt("Attribute_Type_ID"),
-                AttributeTypeName = record.ToString("Attribute_Type"),
-                PreventMultipleSelection = record.ToBool("Prevent_Multiple_Selection"),
-                SortOrder = record.ToInt("Sort_Order")
-            };
         }
     }
 }
