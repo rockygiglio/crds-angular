6b1bff646d4ef69ecd74151b7bd5e6fc82f5f5e2
Jon Horner <hornerjn@mail.uc.edu>
2016-11-09 17:04:11 -0500

    US5465 nextPage function
    added a function that tries to route to the next page when successfully
    submitting.

--- crossroads.net/app/camps/application_page/medical_info/medical_info.controller.js
+++ crossroads.net/app/camps/application_page/medical_info/medical_info.controller.js
@@ -1,8 +1,10 @@
+/* @ngInject */
 class MedicalInfoController {
-  constructor(MedicalInfoForm, $rootScope, $stateParams) {
+  constructor(MedicalInfoForm, $rootScope, $state) {
     this.medicalInfoForm = MedicalInfoForm;
     this.rootScope = $rootScope;
-    this.stateParams = $stateParams;
+    this.go = $state.go;
+    this.stateParams = $state.params;
     this.viewReady = false;
     this.submitting = false;
   }
@@ -15,9 +17,12 @@ class MedicalInfoController {
 
   submit() {
     this.submitting = true;
+
     if (this.medicalInfo.$valid) {
       this.medicalInfoForm.save(this.stateParams.contactId).then(() => {
         this.rootScope.$emit('notify', this.rootScope.MESSAGES.successfulSubmission);
+
+        this.nextPage(this.medicalInfoForm.formModel.contactId);
       }).catch(() => {
         this.rootScope.$emit('notify', this.rootScope.MESSAGES.generalError);
       }).finally(() => {
@@ -27,6 +32,10 @@ class MedicalInfoController {
       this.submitting = false;
       this.rootScope.$emit('notify', this.rootScope.MESSAGES.generalError);
     }
+  }
+
+  nextPage(camperId) {
+    this.go('campsignup.camper', { camperId });
   }
 }
 
