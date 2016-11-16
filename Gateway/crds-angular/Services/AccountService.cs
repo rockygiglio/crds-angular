bba95146465e846522269ba04a654e30380e7563
Sandi Ritter <sandi.ritter@ingagepartners.com>
2016-11-16 09:20:54 -0500

    DE2200 remove code that is no longer used

--- Gateway/crds-angular/Services/AccountService.cs
+++ Gateway/crds-angular/Services/AccountService.cs
@@ -124,17 +124,6 @@ namespace crds_angular.Services
             return recordId;
         }
 
-        private void CreateContactHouseholdRecord(string token, int householdRecordId, int contactRecordId)
-        {
-            var contactHouseholdDictionary = new Dictionary<string, object>();
-            contactHouseholdDictionary["Contact_ID"] = contactRecordId;
-            contactHouseholdDictionary["Household_ID"] = householdRecordId;
-            contactHouseholdDictionary["Household_Position_ID"] = _configurationWrapper.GetConfigIntValue("Household_Position_Default_ID");
-            contactHouseholdDictionary["Household_Type_ID"] = _configurationWrapper.GetConfigIntValue("Household_Type_Default_ID");
-
-            _ministryPlatformService.CreateRecord(_configurationWrapper.GetConfigIntValue("ContactHouseholds"), contactHouseholdDictionary, token);
-        }
-
         private int CreateUserRecord(User newUserData, string token, int contactRecordId)
         {
             var userDictionary = new Dictionary<string, object>();
