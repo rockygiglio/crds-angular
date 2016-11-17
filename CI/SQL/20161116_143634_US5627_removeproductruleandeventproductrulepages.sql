8b99b130100d7b581d47e54315016e0b93c690f5
lknair2015 <lknair2015@gmail.com>
2016-11-17 09:02:08 -0500

    adding begin end to delete all role ids

--- CI/SQL/20161116_143634_US5627_removeproductruleandeventproductrulepages.sql
+++ CI/SQL/20161116_143634_US5627_removeproductruleandeventproductrulepages.sql
@@ -4,8 +4,8 @@ GO
 DECLARE @PageSectionId AS INT = (SELECT Page_Section_ID from dp_Page_Sections WHERE Page_Section = 'Products & Payments');
 DECLARE @ProductPageId AS INT = 601;
 DECLARE @EventProductPageId AS INT = 602;
-DECLARE @ProductRoleId AS INT = (SELECT Role_ID FROM dp_Role_Pages WHERE Page_ID = @ProductPageId);
-DECLARE @EventProductRoleId AS INT = (SELECT Role_ID FROM dp_Role_Pages WHERE Page_ID = @EventProductPageId);
+DECLARE @ProductRoleId AS INT ;
+DECLARE @EventProductRoleId AS INT ;
 
 DELETE FROM [dbo].[dp_Page_Section_Pages]
       WHERE Page_Section_ID = @PageSectionId AND Page_ID = @ProductPageId ;
@@ -13,17 +13,32 @@ DELETE FROM [dbo].[dp_Page_Section_Pages]
 DELETE FROM [dbo].[dp_Page_Section_Pages]
       WHERE Page_Section_ID = @PageSectionId AND Page_ID = @EventProductPageId ;
 
-DELETE FROM [dbo].[dp_Role_Pages]
+
+IF EXISTS (SELECT * FROM dp_Role_Pages WHERE Page_ID = @ProductPageId)
+BEGIN
+   SET @ProductRoleId = (SELECT TOP 1 Role_ID FROM dp_Role_Pages WHERE Page_ID = @ProductPageId);
+   DELETE FROM [dbo].[dp_Role_Pages]
       WHERE Role_ID = @ProductRoleId AND Page_ID = @ProductPageId;
+END
 
-DELETE FROM [dbo].[dp_Role_Pages]
-      WHERE Role_ID = @EventProductRoleId AND Page_ID = @EventProductPageId;
+IF EXISTS (SELECT * FROM dp_Role_Pages WHERE Page_ID = @EventProductPageId)
+BEGIN
+    SET @EventProductRoleId = (SELECT TOP 1 Role_ID FROM dp_Role_Pages WHERE Page_ID = @EventProductPageId);
+    DELETE FROM [dbo].[dp_Role_Pages]
+         WHERE Role_ID = @EventProductRoleId AND Page_ID = @EventProductPageId;
+END
 
-DELETE FROM [dbo].[dp_Pages]
-      WHERE Page_ID = @ProductPageId;
+IF EXISTS (SELECT * FROM dp_Pages WHERE Page_ID =@ProductPageId)
+BEGIN
+    DELETE FROM [dbo].[dp_Pages]
+         WHERE Page_ID = @ProductPageId;
+END
 
-DELETE FROM [dbo].[dp_Pages]
-      WHERE Page_ID = @EventProductPageId;
+IF EXISTS (SELECT * FROM dp_Pages WHERE Page_ID =@EventProductPageId)
+BEGIN
+    DELETE FROM [dbo].[dp_Pages]
+       WHERE Page_ID = @EventProductPageId;
+END
 GO
 
 
