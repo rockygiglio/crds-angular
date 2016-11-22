2708824fa3874986fb257e33eb9697e44de4f3a8
Jon Horner <hornerjn@mail.uc.edu>
2016-11-22 10:34:26 -0500

    DE2273 Added Event Listeners
    Added a bunch of event listeners in an attempt to find a way to hijack
    the route change.

--- crossroads.net/app/camps/thank_you/camp_thank_you.controller.js
+++ crossroads.net/app/camps/thank_you/camp_thank_you.controller.js
@@ -1,17 +1,71 @@
 class CampThankYouController {
   /* @ngInject */
-  constructor(CampsService, $stateParams) {
+  constructor(CampsService, $stateParams, $rootScope, $scope) {
     this.campsService = CampsService;
     this.stateParams = $stateParams;
+    this.rootScope = $rootScope;
+    this.scope = $scope;
 
     this.viewReady = false;
     this.submitting = false;
 
     this.payment = CampsService.payment;
+
+    this.rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+      debugger;
+      event.preventDefault();
+    });
+
+    this.rootScope.$on('$stateChangeSuccess', (event) => {
+      debugger;
+      event.preventDefault();
+    });
+
+    this.rootScope.$on('$locationChangeStart', function(evt) {
+      debugger;
+      event.preventDefault();
+    });
+
+    this.rootScope.$on('$routeChangeStart', function (event) {
+      debugger;
+      event.preventDefault();
+    });
+
+    this.scope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+      debugger;
+    });
   }
 
   $onInit() {
     this.viewReady = true;
+
+    this.rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+      debugger;
+      event.preventDefault();
+    });
+
+    this.rootScope.$on('$stateChangeSuccess', (event) => {
+      debugger;
+      event.preventDefault();
+    });
+
+    this.rootScope.$on('$locationChangeStart', function(evt) {
+      debugger;
+      event.preventDefault();
+    });
+
+    this.rootScope.$on('$routeChangeStart', function (event) {
+      debugger;
+      event.preventDefault();
+    });
+
+    window.addEventListener('popstate', function(event) {
+      debugger;
+    });
+  }
+
+  $onDestroy() {
+    debugger;
   }
 }
 
