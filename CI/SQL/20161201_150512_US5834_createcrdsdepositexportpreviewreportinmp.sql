dc47156a4f7a24e1f57afe03d9b3dc42cf55f14d
lknair2015 <lknair2015@gmail.com>
2016-12-01 16:46:10 -0500

    using report id 305

--- CI/SQL/20161201_150512_US5834_createcrdsdepositexportpreviewreportinmp.sql
+++ CI/SQL/20161201_150512_US5834_createcrdsdepositexportpreviewreportinmp.sql
@@ -1,14 +1,14 @@
 USE MinistryPlatform
 GO
 
-DECLARE @report_id int;
+DECLARE @report_id int = 305;
 DECLARE @page_id int;
 
 SELECT @page_id = p.Page_ID FROM
 	[dbo].[dp_Pages] p WHERE p.Display_Name = N'Deposits'
 
 
-IF NOT EXISTS(SELECT * FROM [dbo].[dp_reports] WHERE Report_Path = '/MPReports/Crossroads/CRDS Selected Deposit Export Preview')
+IF NOT EXISTS(SELECT * FROM [dbo].[dp_reports] WHERE Report_ID = @report_id)
 BEGIN
     SET IDENTITY_INSERT dbo.dp_Reports ON
 	INSERT INTO [dbo].[dp_reports]
@@ -21,17 +21,15 @@ BEGIN
 				[On_Reports_Tab]
 			   )
 		 VALUES
-			   (305
+			   (@report_id
 			   ,N'Selected Deposit - Export Preview'
 			   ,N'Selected Deposit - Export Preview (Donations and Payments)'
 			   ,N'/MPReports/Crossroads/CRDS Selected Deposit Export Preview'
 			   ,1
 			   ,1          
 			   ,0)
+   SET IDENTITY_INSERT dbo.dp_Reports OFF
 END
-
-SELECT @report_id = r.Report_ID FROM
-	[dp_reports] r WHERE r.Report_Path = N'/MPReports/Crossroads/CRDS Selected Deposit Export Preview'
 
 IF NOT EXISTS(SELECT * FROM [dbo].[dp_Report_Pages] WHERE Report_ID = @report_id AND Page_ID = @page_id )
 BEGIN
