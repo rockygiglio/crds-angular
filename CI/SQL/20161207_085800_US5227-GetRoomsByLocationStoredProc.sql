49b32a990db8802b56474325200458d00f939ecf
Ken Baum <ken.baum@ingagepartners.com>
2016-12-07 09:49:25 -0500

    US5227 added stored proc to return rooms and reservation status

---
+++ CI/SQL/20161207_085800_US5227-GetRoomsByLocationStoredProc.sql
@@ -0,0 +1,51 @@
+USE [MinistryPlatform]
+GO
+/****** Object:  StoredProcedure [dbo].[api_crds_GetReservedAndAvailableRoomsByLocation]    Script Date: 12/07/2016 09:05:19 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[api_crds_GetReservedAndAvailableRoomsByLocation]') AND type in (N'P', N'PC'))
+BEGIN
+  EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[api_crds_GetReservedAndAvailableRoomsByLocation] AS'
+END
+GO
+
+-- =============================================
+-- Author:      Ken Baum
+-- Create date: 2016-12-07
+-- Description:  Returns all bookable rooms by 
+--               location id, plus whether they 
+--               are currently reserved for a 
+--               particular time or not
+-- =============================================
+ALTER PROCEDURE [dbo].[api_crds_GetReservedAndAvailableRoomsByLocation]
+	@startDate DateTime,
+	@endDate DateTime,
+	@locationId int = 3
+AS
+BEGIN
+
+	WITH Reserved_Rooms (Room_ID, _Approved)
+	AS
+	(
+	SELECT DISTINCT r.Room_ID, er._Approved
+	FROM dbo.Events e
+		inner join dbo.Event_Rooms er on e.Event_ID = er.Event_ID
+		inner join dbo.Rooms r on er.Room_ID = r.Room_ID
+	WHERE ( (e.Event_Start_Date >= @startDate AND e.Event_Start_Date < @endDate)
+		OR (e.Event_End_Date > @startDate AND e.Event_End_Date <= @endDate) )
+		AND er._Approved = 1
+		AND e.Location_ID = @locationId
+	)
+	SELECT r.Room_ID, r.Room_Name, r.Room_Number, b.Building_ID, b.Building_Name, b.Location_ID, 
+			r.Description, r.Theater_Capacity, r.Banquet_Capacity, ISNULL(rr._Approved, 0)
+	FROM dbo.Rooms r
+		INNER JOIN dbo.Buildings b on b.Building_ID = r.Building_ID
+		LEFT OUTER JOIN Reserved_Rooms rr on rr.Room_ID = r.Room_ID
+	WHERE r.Bookable = 1 and b.Location_ID = @locationId
+	ORDER BY r.Room_ID
+
+END
