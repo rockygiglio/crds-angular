cd667e8e603a3cf27ab7b5e71b213223defefdbc
silbermm <silbermm@gmail.com>
2016-12-14 13:10:47 -0500

    DE2453 Cleanup using's

--- Gateway/MinistryPlatform.Translation.Test/Services/CongregationRepositoryTest.cs
+++ Gateway/MinistryPlatform.Translation.Test/Services/CongregationRepositoryTest.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Crossroads.Utilities.FunctionalHelpers;
 using Crossroads.Utilities.Interfaces;
 using MinistryPlatform.Translation.Models;
