c11c55a76fe2c6c38e9bb45ca5e90b23b9db8b33
silbermm <silbermm@gmail.com>
2016-12-14 12:34:10 -0500

    DE2453 Update to allow saving of congregation form response

---
+++ Gateway/MinistryPlatform.Translation.Test/Services/CongregationRepositoryTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crossroads.Utilities.FunctionalHelpers;
+using Crossroads.Utilities.Interfaces;
+using MinistryPlatform.Translation.Models;
+using MinistryPlatform.Translation.Repositories;
+using MinistryPlatform.Translation.Repositories.Interfaces;
+using MinistryPlatform.Translation.Test.Helpers;
+using Moq;
+using NUnit.Framework;
+
+namespace MinistryPlatform.Translation.Test.Services
+{
+    [TestFixture]
+    public class CongregationRepositoryTest
+    {
+
+        private Mock<IMinistryPlatformService> _ministryPlatformService;
+        private Mock<IMinistryPlatformRestRepository> _ministryPlatformRest;        
+        private Mock<IAuthenticationRepository> _authRepository;
+        private Mock<IConfigurationWrapper> _configWrapper;
+        private ICongregationRepository _fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            Factories.MpCongregation();
+
+            _ministryPlatformService = new Mock<IMinistryPlatformService>();
+            _ministryPlatformRest = new Mock<IMinistryPlatformRestRepository>();
+            _authRepository = new Mock<IAuthenticationRepository>();
+            _configWrapper = new Mock<IConfigurationWrapper>();
+            _fixture = new CongregationRepository(_ministryPlatformService.Object, _ministryPlatformRest.Object, _authRepository.Object, _configWrapper.Object);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _ministryPlatformService.VerifyAll();
+            _ministryPlatformRest.VerifyAll();
+            _authRepository.VerifyAll();
+            _configWrapper.VerifyAll();
+        }
+
+        [Test]
+        public void ShouldGetCongregationByName()
+        {
+            const string congregationName = "Oakley";
+            const string token = "token";
+            var congregation = FactoryGirl.NET.FactoryGirl.Build<MpCongregation>(c => c.Name = "Oakley");
+            var congregationList = new List<MpCongregation>
+            {
+                congregation
+            };
+
+            _ministryPlatformRest.Setup(m => m.UsingAuthenticationToken(token)).Returns(_ministryPlatformRest.Object);
+            _ministryPlatformRest.Setup(m => m.Search<MpCongregation>(It.IsAny<String>(), null as string, null, false)).Returns(congregationList);
+
+            var result = _fixture.GetCongregationByName(congregationName, token);
+            Assert.IsInstanceOf<Ok<MpCongregation>>(result);
+            Assert.AreEqual(congregation.CongregationId, result.Value.CongregationId);
+        }
+
+        [Test]
+        public void ShouldNotFindCongregationByName()
+        {
+            const string congregationName = "Oakley";
+            const string token = "token";
+            var congregationList = new List<MpCongregation>();            
+
+            _ministryPlatformRest.Setup(m => m.UsingAuthenticationToken(token)).Returns(_ministryPlatformRest.Object);
+            _ministryPlatformRest.Setup(m => m.Search<MpCongregation>(It.IsAny<String>(), null as string, null, false)).Returns(congregationList);
+
+            var result = _fixture.GetCongregationByName(congregationName, token);
+            Assert.IsInstanceOf<Err<MpCongregation>>(result);
+            Assert.IsNotEmpty(result.ErrorMessage);            
+        }
+    }
+}
