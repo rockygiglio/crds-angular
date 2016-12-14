93e7def1b274222e495cafe49f7231387fc5c9a9
Jim Kriz <jim.kriz@ingagepartners.com>
2016-12-14 10:30:12 -0500

    DE2457: Fix kids club group assignment

--- CI/SQL-Archive/20161205_160100_US5954-AddMSandHSGroupParticipantsForCheckIn.sql
+++ CI/SQL-Archive/20161205_160100_US5954-AddMSandHSGroupParticipantsForCheckIn.sql
@@ -1,3 +1,9 @@
+delete from ministryplatform.dbo.group_participants
+from ministryplatform.[dbo].[Group_Participants] as gp
+join ministryplatform.[dbo].[groups] as g
+on g.group_ID = gp.group_ID
+where g.Group_ID in (173927,173928,173929,173930,173931,173932,173933,173934);
+
 --Bring back all active minor contacts and attempt to assign them a Check-In group by adding them as a group participant
 INSERT INTO [dbo].[Group_Participants]
 ([Group_ID],[Participant_ID],[Group_Role_ID],[Domain_ID],[Start_Date],[Employee_Role],[Notes],[Child_Care_Requested],[Need_Book])
@@ -13,38 +19,14 @@ Select T1.Group_ID
 from (
 select 
 CASE 
---Assign by rule 3
-when c.[Date_of_Birth] is null and c.[HS_Graduation_Year] = 2017 then 173927  --12th grade   High School Grade 12
-when c.[Date_of_Birth] is null and c.[HS_Graduation_Year] = 2018 then 173928  --11th grade   High School Grade 11
-when c.[Date_of_Birth] is null and c.[HS_Graduation_Year] = 2019 then 173929  --10th grade   High School Grade 10
-when c.[Date_of_Birth] is null and c.[HS_Graduation_Year] = 2020 then 173930  --9th grade    High School Grade 9
-when c.[Date_of_Birth] is null and c.[HS_Graduation_Year] = 2021 then 173931  --8th grade    Student Ministry Grade 8
-when c.[Date_of_Birth] is null and c.[HS_Graduation_Year] = 2022 then 173932  --7th grade    Student Ministry Grade 7
-when c.[Date_of_Birth] is null and c.[HS_Graduation_Year] = 2023 then 173933  --6th grade    Student Ministry Grade 6
---Assign by rule 8
-when c.[Date_of_Birth] < '1999-01-02' and c.[HS_Graduation_Year] = 2017 then 173927  --12th grade   High School Grade 12
-when c.[Date_of_Birth] < '1999-01-02' and c.[HS_Graduation_Year] = 2018 then 173928  --11th grade   High School Grade 11
-when c.[Date_of_Birth] < '1999-01-02' and c.[HS_Graduation_Year] = 2019 then 173929  --10th grade   High School Grade 10
-when c.[Date_of_Birth] < '1999-01-02' and c.[HS_Graduation_Year] = 2020 then 173930  --9th grade    High School Grade 9
-when c.[Date_of_Birth] < '1999-01-02' and c.[HS_Graduation_Year] = 2021 then 173931  --8th grade    Student Ministry Grade 8
-when c.[Date_of_Birth] < '1999-01-02' and c.[HS_Graduation_Year] = 2022 then 173932  --7th grade    Student Ministry Grade 7
-when c.[Date_of_Birth] < '1999-01-02' and c.[HS_Graduation_Year] = 2023 then 173933  --6th grade    Student Ministry Grade 6
---Assign by rule 10
-when c.[Date_of_Birth] between '1999-01-02' and '2011-09-30' and c.[HS_Graduation_Year] = 2017 then 173927  --12th grade   High School Grade 12
-when c.[Date_of_Birth] between '1999-01-02' and '2011-09-30' and c.[HS_Graduation_Year] = 2018 then 173928  --11th grade   High School Grade 11
-when c.[Date_of_Birth] between '1999-01-02' and '2011-09-30' and c.[HS_Graduation_Year] = 2019 then 173929  --10th grade   High School Grade 10
-when c.[Date_of_Birth] between '1999-01-02' and '2011-09-30' and c.[HS_Graduation_Year] = 2020 then 173930  --9th grade    High School Grade 9
-when c.[Date_of_Birth] between '1999-01-02' and '2011-09-30' and c.[HS_Graduation_Year] = 2021 then 173931  --8th grade    Student Ministry Grade 8
-when c.[Date_of_Birth] between '1999-01-02' and '2011-09-30' and c.[HS_Graduation_Year] = 2022 then 173932  --7th grade    Student Ministry Grade 7
-when c.[Date_of_Birth] between '1999-01-02' and '2011-09-30' and c.[HS_Graduation_Year] = 2023 then 173933  --6th grade    Student Ministry Grade 6
---Assign by rule 11
-when c.[Date_of_Birth] > '2011-09-30' and c.[HS_Graduation_Year] = 2017 then 173927  --12th grade   High School Grade 12
-when c.[Date_of_Birth] > '2011-09-30' and c.[HS_Graduation_Year] = 2018 then 173928  --11th grade   High School Grade 11
-when c.[Date_of_Birth] > '2011-09-30' and c.[HS_Graduation_Year] = 2019 then 173929  --10th grade   High School Grade 10
-when c.[Date_of_Birth] > '2011-09-30' and c.[HS_Graduation_Year] = 2020 then 173930  --9th grade    High School Grade 9
-when c.[Date_of_Birth] > '2011-09-30' and c.[HS_Graduation_Year] = 2021 then 173931  --8th grade    Student Ministry Grade 8
-when c.[Date_of_Birth] > '2011-09-30' and c.[HS_Graduation_Year] = 2022 then 173932  --7th grade    Student Ministry Grade 7
-when c.[Date_of_Birth] > '2011-09-30' and c.[HS_Graduation_Year] = 2023 then 173933  --6th grade    Student Ministry Grade 6
+when c.[HS_Graduation_Year] = 2017 then 173927  --12th grade   High School Grade 12
+when c.[HS_Graduation_Year] = 2018 then 173928  --11th grade   High School Grade 11
+when c.[HS_Graduation_Year] = 2019 then 173929  --10th grade   High School Grade 10
+when c.[HS_Graduation_Year] = 2020 then 173930  --9th grade    High School Grade 9
+when c.[HS_Graduation_Year] = 2021 then 173931  --8th grade    Student Ministry Grade 8
+when c.[HS_Graduation_Year] = 2022 then 173932  --7th grade    Student Ministry Grade 7
+when c.[HS_Graduation_Year] = 2023 then 173933  --6th grade    Student Ministry Grade 6
+when c.[HS_Graduation_Year] = 2024 then 173934  --5th grade   Kids Club Grade 5
 else null end as Group_ID
 --,c.[Contact_ID]
 --,c.[Date_of_Birth]
@@ -57,7 +39,7 @@ else null end as Group_ID
 ,p.Participant_ID
 ,16 as Group_Role_ID
 ,1 as Domain_ID
-,'2017-01-01' as Start_Date
+,getdate() as Start_Date
 ,0 as Employee_Role
 ,'Initial Check-In load 2017' as Notes
 ,0 as Child_Care_Requested
