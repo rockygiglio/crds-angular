001802225ae644560ae819d2eaa427b5e0404493
Taylor C. MacDonald <tcmacdonald@gmail.com>
2016-12-16 13:47:39 -0500

    DE2449 Iframe Resizer Add usage information

--- crossroads.net/core/embed/embed.controller.js
+++ crossroads.net/core/embed/embed.controller.js
@@ -1,5 +1,20 @@
 const iFrameResizer = require('iframe-resizer/js/iframeResizer.min.js'); // eslint-disable-line no-unused-vars
 
+/**
+ * This directive helps you implement a resizeable iframe
+ * for any routes within the `crds-embed` repo. The src attribute
+ * on the iframe will reflect the current environment (int, demo, etc.)
+ * automatically.
+ *
+ * To use, simply drop the following element in your markup (or content-block)
+ * and define the relative path for the route you want passed to the iframe.
+ *
+ * For example...
+ *
+ *    <crds-embed path="/?type=donation&theme=dark"></crds-embed>
+ *
+ */
+
 export default class EmbedController {
 
   constructor($element, $sce, $attrs) {
