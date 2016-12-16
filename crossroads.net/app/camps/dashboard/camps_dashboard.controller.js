979ce99d9ed9aa65f22747fc21326cc3c786a6d8
lknair2015 <lknair2015@gmail.com>
2016-12-16 14:47:45 -0500

    adding onPayment to dashboard controller

--- crossroads.net/app/camps/dashboard/camps_dashboard.controller.js
+++ crossroads.net/app/camps/dashboard/camps_dashboard.controller.js
@@ -1,12 +1,24 @@
 class CampDashboardController {
-  constructor(CampsService) {
+  constructor(CampsService, $state, $rootScope, $window) {
     this.campsService = CampsService;
     this.viewReady = false;
+    this.state = $state;
+    this.rootScope = $rootScope;
+    this.window = $window;
   }
 
   $onInit() {
     this.data = this.campsService.dashboard;
     this.viewReady = true;
+    this.onPayment();
+  }
+
+  onPayment() {
+    this.url = this.window.location.href.split('=');
+    this.paymentId = this.url[2];
+    if (!(this.paymentId === null)) {
+      this.rootScope.$emit('notify', this.rootScope.MESSAGES.successfulSubmission);
+    }
   }
 
   isDashboardEmpty() {
