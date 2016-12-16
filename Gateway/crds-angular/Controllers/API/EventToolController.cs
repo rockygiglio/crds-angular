72b0023323a6d11033e608864346b61a789a4baa
Ken Baum <ken.baum@ingagepartners.com>
2016-12-16 12:55:18 -0500

    US3065 Add authorization decorator to put

--- Gateway/crds-angular/Controllers/API/EventToolController.cs
+++ Gateway/crds-angular/Controllers/API/EventToolController.cs
@@ -96,6 +96,7 @@ namespace crds_angular.Controllers.API
             throw new HttpResponseException(dataError.HttpResponseMessage);
         }
 
+        [RequiresAuthorization]
         [VersionedRoute(template: "event-tool/{eventId}", minimumVersion: "1.0.0")]
         [Route("eventTool/{eventId}")]
         [HttpPut]
