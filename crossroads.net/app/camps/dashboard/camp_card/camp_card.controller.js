c7338e64bba0923fe9327fae9db3f7e0217bec7e
andycanterbury <andrew.canterbury@ingagepartners.com>
2016-12-20 13:09:23 -0500

    DE2510 Having a Zero balance isn't an error.

--- crossroads.net/app/camps/dashboard/camp_card/camp_card.controller.js
+++ crossroads.net/app/camps/dashboard/camp_card/camp_card.controller.js
@@ -42,7 +42,7 @@ class CampCardController {
   }
 
   formatAmountDue() {
-    if (!this.paymentRemaining) {
+    if (!this.paymentRemaining && this.paymentRemaining < 0) {
       return `Error getting payments. Please contact ${this.campPrimaryContact}`;
     }
 
