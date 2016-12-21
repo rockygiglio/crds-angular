7979cde634cf49390dda809943a9cb0a1937a677
silbermm <silbermm@gmail.com>
2016-12-21 12:16:23 -0500

    DE2516 Update Rules pages

---
+++ CI/SQL/20161212_121456_DE2516-update-rules-datastructure.sql
@@ -0,0 +1,53 @@
+use MinistryPlatform
+GO
+
+DECLARE @Rule_Registration int = 622;
+DECLARE @Product_Rulese int = 623;
+DECLARE @Gender_Rules int = 621;
+
+
+IF EXISTS (SELECT 1 FROM [dbo].[dp_Pages] WHERE [Page_ID] = @Rule_Registration)
+BEGIN
+	Update [dbo].[dp_Pages] SET
+	[Default_Field_List] = N'cr_Rule_Registrations.Maximum_Registrants, cr_Rule_Registrations.Rule_Start_Date, cr_Rule_Registrations.Rule_End_Date'
+	WHERE [Page_ID] = @Rule_Registration;
+END
+
+DECLARE @Rule_Page_Section int = 24;
+
+
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Page_Section_Pages] WHERE [Page_Section_ID] = @Rule_Page_Section AND [Page_ID] = @Rule_Registration) 
+BEGIN
+	INSERT INTO [dbo].[dp_Page_Section_Pages] (
+		 [Page_ID]
+		,[Page_Section_ID]
+	) VALUES (
+		 @Rule_Registration
+		,@Rule_Page_Section
+	)
+	
+END
+
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Page_Section_Pages] WHERE [Page_Section_ID] = @Rule_Page_Section AND [Page_ID] = @Product_Rulese) 
+BEGIN
+	INSERT INTO [dbo].[dp_Page_Section_Pages] (
+		 [Page_ID]
+		,[Page_Section_ID]
+	) VALUES (
+		 @Product_Rulese
+		,@Rule_Page_Section
+	)	
+END
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Page_Section_Pages] WHERE [Page_Section_ID] = @Rule_Page_Section AND [Page_ID] = @Gender_Rules) 
+BEGIN
+	INSERT INTO [dbo].[dp_Page_Section_Pages] (
+		 [Page_ID]
+		,[Page_Section_ID]
+	) VALUES (
+		 @Gender_Rules
+		,@Rule_Page_Section
+	)	
+END
