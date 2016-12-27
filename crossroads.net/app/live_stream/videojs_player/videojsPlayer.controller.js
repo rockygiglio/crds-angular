a9457742c01fe36c0f2ac638052a0aad13b77719
Samuel Mello <samueljmello@gmail.com>
2016-12-27 16:34:37 -0500

    US6153: Switch to VideoJS Player

--- crossroads.net/app/live_stream/videojs_player/videojsPlayer.controller.js
+++ crossroads.net/app/live_stream/videojs_player/videojsPlayer.controller.js
@@ -1,3 +1,4 @@
+
 window.videojs = require('video.js/dist/video');
 
 // require('./vendor/streamspotAnalytics');
@@ -5,16 +6,16 @@ require('../vendor/videojs5-hlsjs-source-handler.min');
 require('videojs-chromecast/dist/videojs-chromecast');
 
 export default class VideojsPlayerController {
-  /*@ngInject*/
+  /* @ngInject */
   constructor(StreamspotService, $rootScope, $analytics) {
     this.streamspotService = StreamspotService;
     this.rootScope         = $rootScope;
     this.angulartics       = $analytics;
 
-    this.id       = 'videojs-player';
+    this.id = 'videojs-player';
     this.visibile = false;
-    this.debug    = false;
-    this.player;
+    this.debug = false;
+    this.player = null;
   }
 
   $onDestroy() {
@@ -23,37 +24,36 @@ export default class VideojsPlayerController {
 
   $onInit() {
     this.streamspotService.getBroadcaster().then((response) => {
-      if ( response.success === true && response.data.broadcaster !== undefined ) {
+      if (response.success === true && response.data.broadcaster !== undefined) {
+        const broadcaster = response.data.broadcaster;
 
-        var broadcaster = response.data.broadcaster;
-
-        if ( broadcaster.players === undefined || broadcaster.players.length === 0 ) {
+        if (broadcaster.players === undefined || broadcaster.players.length === 0) {
           return false;
         }
 
         let defaultPlayer;
-        broadcaster.players.forEach(element => {
-          if ( element.default === true ) {
+        broadcaster.players.forEach((element) => {
+          if (element.default === true) {
             defaultPlayer = element;
-            return false;
           }
+          return false;
         });
 
-        if ( defaultPlayer === undefined ) {
+        if (defaultPlayer === undefined) {
           defaultPlayer = broadcaster.players[0];
         }
 
         this.player = window.videojs(this.id, {
-          "techOrder": ["html5"],
-          "fluid": true,
-          "poster" : defaultPlayer.bgLink,
-          "controls": true,
-          "html5": {
-            "hlsjsConfig": { 
-              "debug": false, 
-              "abrBandWidthFactor": 0.7, 
-              "abrBandWidthUpFactor": 0.6 
-            } 
+          techOrder: ['html5'],
+          fluid: true,
+          poster: defaultPlayer.bgLink,
+          controls: true,
+          html5: {
+            hlsjsConfig: {
+              debug: false,
+              abrBandWidthFactor: 0.7,
+              abrBandWidthUpFactor: 0.6
+            }
           }
         });
 
@@ -63,7 +63,7 @@ export default class VideojsPlayerController {
             window.SSTracker = window.SSTracker ? window.SSTracker : new window.Tracker(this.streamspot.ssid);
             window.SSTracker.start(broadcaster.live_src.cdn_hls, true, this.streamspot.ssid);
           }
-          if ( this.angulartics !== undefined ) {
+          if (this.angulartics !== undefined) {
             this.angulartics.eventTrack('Play', {
               category: 'Streaming',
               label: 'Live Streaming Play'
@@ -84,27 +84,28 @@ export default class VideojsPlayerController {
             });
           }
         });
-            
-        if ( broadcaster.isBroadcasting === true || this.debug ) {
+
+        if (broadcaster.isBroadcasting === true || this.debug) {
           this.playerInit(broadcaster);
         }
       }
-    })
+    });
   }
 
   playerInit(broadcaster) {
-    let src = this.debug ? "http://vjs.zencdn.net/v/oceans.mp4" : broadcaster.live_src.cdn_hls;
-    let type = this.debug ? "video/mp4" : "application/x-mpegURL";
+    const videoType = this.debug ? 'video/mp4' : 'application/x-mpegURL';
+    const videoSrc = this.debug ? 'http://vjs.zencdn.net/v/oceans.mp4' : broadcaster.live_src.cdn_hls;
 
     this.player.src([
       {
-        "type": type,
-        "src": src
+        type: videoType,
+        src: videoSrc
       }
     ]);
     this.player.qualityPickerPlugin();
-    this.player.updateStyleEl_();
     this.visible = true;
-    this.player.ready(() => this.player.play());
+    this.player.ready(() => {
+      this.player.play();
+    });
   }
 }
