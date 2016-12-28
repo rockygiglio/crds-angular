f981f847c0afc7bdba92c9b5bd3633e9f35fbfc1
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2016-12-28 13:35:13 -0500

    US6100 make script rerunnable - check for existance of table and column

--- CI/SQL/20161228_112000_US6100-appoved-host-flag.sql
+++ CI/SQL/20161228_112000_US6100-appoved-host-flag.sql
@@ -7,7 +7,10 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER TABLE [dbo].[Participants]
-ADD Approved_Host BIT NOT NULL DEFAULT(0)
-
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_NAME = N'Participants')
+   AND NOT EXISTS(select top 1 * from sys.columns  WHERE Name = N'Approved_Host' AND Object_ID = Object_ID(N'Participants'))
+BEGIN	
+    ALTER TABLE [dbo].[Participants]
+    ADD Approved_Host BIT NOT NULL DEFAULT(0)
+END
 GO
