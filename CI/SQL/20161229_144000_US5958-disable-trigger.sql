b4971f5acac8482aa00689bfc926034a13cf9d86
Katie Dwyer <Katie Dwyer>
2016-12-29 14:48:53 -0500

    US5958: Check for existinence before disabling to prevent errors`

--- CI/SQL/20161229_144000_US5958-disable-trigger.sql
+++ CI/SQL/20161229_144000_US5958-disable-trigger.sql
@@ -1,4 +1,7 @@
 use [MinistryPlatform]
 GO
 
-DISABLE TRIGGER [dbo].[tr_turn_off_event_registration] ON Event_Participants
+IF EXISTS  (select * from sys.triggers where name = 'tr_turn_off_event_registration')
+BEGIN
+  DISABLE TRIGGER [dbo].[tr_turn_off_event_registration] ON Event_Participants
+END 
