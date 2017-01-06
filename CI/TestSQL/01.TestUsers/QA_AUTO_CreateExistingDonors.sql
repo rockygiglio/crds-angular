7fcfda9bfa8d20b20008c7ad7500bcc4b577becb
Isaac Hookom <ihookom@callibrity.com>
2017-01-06 11:18:52 -0500

    add new automation-specific test data and scripts to create it

---
+++ CI/TestSQL/01.TestUsers/QA_AUTO_CreateExistingDonors.sql
@@ -0,0 +1,43 @@
+--Modify automation contacts created by createTestUsers.ps1, causing them to be existing donors
+USE [MinistryPlatform]
+GO
+
+--
+--mpcrds+auto+1@gmail.com (existing ACH donor)
+--
+--Retrieve contact ID
+DECLARE @contactID as int
+set @contactID = (select contact_id from contacts where Email_Address = 'mpcrds+auto+1@gmail.com' and Last_Name = 'Karpyshyn');
+
+--Create a Donor record for mpcrds+auto+1@gmail.com (used the Stripe processor ID from a newly-created ACH transaction)
+INSERT INTO [dbo].donors 
+(Contact_ID,Statement_Frequency_ID,Statement_Type_ID,Statement_Method_ID,Setup_Date                    ,Envelope_No,Cancel_Envelopes,Notes,First_Contact_Made,Domain_ID,__ExternalPersonID,_First_Donation_Date,_Last_Donation_Date,Processor_ID        ) VALUES
+(@contactID,1                     ,1                ,2                  ,{ts '2017-01-03 15:26:57.503'},null       ,0              ,null ,null              ,1        ,null              ,null                ,null               ,'cus_9rioLxKczE0gLb');
+
+--Retrieve Donor ID from new Donor record
+DECLARE @donor_id as int
+set @donor_id = (Select donor_ID from donors where contact_id = @contactID);
+
+--Update Contact record with Donor ID
+update [dbo].Contacts set Donor_Record = @donor_ID where contact_id = @contactID;
+
+
+--
+--mpcrds+auto+2@gmail.com (existing CC donor)
+--
+--Retrieve contact ID
+@contactID = (select contact_id from contacts where Email_Address = 'mpcrds+auto+2@gmail.com' and Last_Name = 'Kenobi');
+
+--Create a Donor record for mpcrds+auto+2@gmail.com (used the Stripe processor ID from a newly-created CC transaction)
+INSERT INTO [dbo].donors
+(Contact_ID,Statement_Frequency_ID,Statement_Type_ID,Statement_Method_ID,Setup_Date                    ,Envelope_No,Cancel_Envelopes,Notes,First_Contact_Made,Domain_ID,__ExternalPersonID,_First_Donation_Date,_Last_Donation_Date,Processor_ID        ) VALUES
+(@contactID,1                     ,1                ,2                  ,{ts '2017-01-03 15:26:57.503'},null       ,0              ,null ,null              ,1        ,null              ,null                ,null               ,'cus_9s2PUrgn12xY0n');
+
+--Retrieve Donor ID from new Donor record
+DECLARE @donor_id as int
+set @donor_id = (Select donor_ID from donors where contact_id = @contactID);
+
+--Update Contact record with Donor ID
+update [dbo].Contacts set Donor_Record = @donor_ID where contact_id = @contactID;
+
+GO
