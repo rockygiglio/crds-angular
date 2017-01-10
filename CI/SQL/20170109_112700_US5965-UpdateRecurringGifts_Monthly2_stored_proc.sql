6db6fd60a21ddc8a802e82a43bd0096aa9fe9685
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-01-10 08:35:35 -0500

    SQL keywords to upper case

--- CI/SQL/20170109_112700_US5965-UpdateRecurringGifts_Monthly2_stored_proc.sql
+++ CI/SQL/20170109_112700_US5965-UpdateRecurringGifts_Monthly2_stored_proc.sql
@@ -1,7 +1,6 @@
 USE [MinistryPlatform]
 GO
 
-/****** Object:  StoredProcedure [dbo].[report_CRDS_RecurringGifts_MonthlyII]    Script Date: 1/9/2017 11:26:37 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -16,33 +15,32 @@ GO
 
 ALTER PROCEDURE [dbo].[report_CRDS_RecurringGifts_MonthlyII]
 
-As Begin
+AS BEGIN
 
 
-select 'MONTHLY II' as recurrance,count(d.Donor_ID) num_of_gifts, sum(rg.amount) gifts_for_II, sum(rg.amount * 1) total_monthly_II_expected, sum(rg.amount * 12) total_yearly_II_expected
-		from dbo.Recurring_Gifts rg
-		inner join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID 
-		inner join dbo.Programs p on rg.Program_ID = p.Program_ID 
-		inner join dbo.Donors d on rg.Donor_ID = d.Donor_ID
+		SELECT 'MONTHLY II' AS recurrance,COUNT(d.Donor_ID) num_of_gifts, SUM(rg.amount) gifts_for_II, SUM(rg.amount * 1) total_monthly_II_expected, SUM(rg.amount * 12) total_yearly_II_expected
+		FROM dbo.Recurring_Gifts rg
+		INNER JOIN dbo.Recurring_Gift_Frequencies rgf ON rg.Frequency_ID = rgf.Frequency_ID 
+		INNER JOIN dbo.Programs p ON rg.Program_ID = p.Program_ID 
+		INNER JOIN dbo.Donors d ON rg.Donor_ID = d.Donor_ID
 		INNER JOIN dbo.Congregations c ON p.Congregation_Id = c.Congregation_Id
-		where 	
+		WHERE 	
 		  rgf.Frequency_ID = 2 --monthly
-		  and p.Program_ID = 146 --General Giving
-		  and rg.end_date is null
+		  AND p.Program_ID = 146 --General Giving
+		  AND rg.end_date IS NULL
 		  AND c.Accounting_Company_Id = 1 --crossroads
-		union all 
-		select 'WEEKLY II' as recurrance,count(d.Donor_ID) num_of_gifts, sum(rg.amount) gifts_for_II,sum(rg.amount *52/12) total_monthly_II_expected, sum(rg.amount * 52) total_yearly_II_expected
-		from dbo.Recurring_Gifts rg
-		inner join dbo.Recurring_Gift_Frequencies rgf on rg.Frequency_ID = rgf.Frequency_ID
-		inner join dbo.Programs p on rg.Program_ID = p.Program_ID
-		inner join dbo.Donors d on  rg.Donor_ID = d.Donor_ID
+		UNION ALL
+		SELECT 'WEEKLY II' AS recurrance,COUNT(d.Donor_ID) num_of_gifts, SUM(rg.amount) gifts_for_II,SUM(rg.amount *52/12) total_monthly_II_expected, SUM(rg.amount * 52) total_yearly_II_expected
+		FROM dbo.Recurring_Gifts rg
+		INNER JOIN dbo.Recurring_Gift_Frequencies rgf ON rg.Frequency_ID = rgf.Frequency_ID
+		INNER JOIN dbo.Programs p ON rg.Program_ID = p.Program_ID
+		INNER JOIN dbo.Donors d ON  rg.Donor_ID = d.Donor_ID
 		INNER JOIN dbo.Congregations c ON p.Congregation_Id = c.Congregation_Id
-		where 
+		WHERE 
 		 rgf.Frequency_ID = 1 --weekly
-		 and p.Program_ID = 146 --General Giving
-	  	 and rg.end_date is null
+		 AND p.Program_ID = 146 --General Giving
+	  	 AND rg.end_date IS NULL
 		 AND c.Accounting_Company_Id = 1 --crossroads
-End
+END
 GO
-
 
