487def2f010fd47d826d90f0b70d5a9818d383dd
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-01-18 12:42:04 -0500

    filter by missing addresses instead of zipcodes

--- CI/SQL/20180108_104400_DE2670-UpdateContributionStatementPageView.sql
+++ CI/SQL/20180108_104400_DE2670-UpdateContributionStatementPageView.sql
@@ -5,7 +5,7 @@ DECLARE @PageViewId INT = 1117
 
 IF EXISTS(SELECT * FROM dp_Page_Views where Page_View_ID = @PageViewId)
 BEGIN
-	UPDATE dp_Page_Views SET View_Clause = ' Contact_ID_Table_Household_ID_Table_Address_ID_Table.Postal_Code IS NOT NULL AND  Contact_ID_Table_Household_ID_Table_Address_ID_Table.Address_Line_1 IS NOT NULL AND  Contact_ID_Table_Household_ID_Table_Address_ID_Table.Address_Line_1 <> '''' AND Statement_Method_ID_Table.[Statement_Method_ID] = 1'
+	UPDATE dp_Page_Views SET View_Clause = '(Contact_ID_Table_Household_ID_Table_Address_ID_Table.Address_Line_1 IS NOT NULL AND Contact_ID_Table_Household_ID_Table_Address_ID_Table.Address_Line_1 <> '''') AND Statement_Method_ID_Table.[Statement_Method_ID] = 1'
 	     WHERE Page_View_ID = @PageViewId
 END
 
