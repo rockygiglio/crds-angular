c145f5305fb6843b1b24bdb2bf4334b4164ea045
Samuel Mello <samueljmello@gmail.com>
2017-01-23 09:28:31 -0500

    DE2682: Remove inappropriate headers for AWS requests

--- crossroads.net/app/search/search.service.js
+++ crossroads.net/app/search/search.service.js
@@ -8,6 +8,7 @@
       execute: {
         method : 'POST',
         transformRequest: function(data, headers){
+          console.log(headers);
           var str = [];
           for(var p in data) {
             if(data[p]){
@@ -16,7 +17,12 @@
           }
           return str.join('&');
         },
-        headers : {'Content-Type': 'application/x-www-form-urlencoded' }
+        headers : {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'ImpersonateUserId': undefined,
+          'Authorization': undefined,
+          'RefreshToken': undefined
+        }
       }
     });
   }
