283dbc036ee91ca7efeba81a7497c973ccdd7829
andycanterbury <andrew.canterbury@ingagepartners.com>
2017-01-25 16:45:14 -0500

    US6246 Tweaked stored proc and report

--- CI/SQL/20170125-120618_US6246-StoredProcForUserSecurityReport.sql
+++ CI/SQL/20170125-120618_US6246-StoredProcForUserSecurityReport.sql
@@ -11,7 +11,10 @@ CREATE PROCEDURE report_CRDS_PagesAccessByUser
 	@UserId int
 AS
 BEGIN
-	SELECT DISTINCT P.Page_ID, P.Display_Name, 
+	DECLARE @SecurityList TABLE (Page_ID int, Sub_Page_ID int, Name varchar(256), Security_Role varchar(1024))
+
+	INSERT INTO @SecurityList (Page_ID, Sub_Page_ID, Name, Security_Role)
+	(SELECT DISTINCT P.Page_ID, '', P.Display_Name, 
        STUFF((SELECT ','+ Role_Name FROM[dbo].[dp_Role_Pages] RP
                                     JOIN [dp_Pages] PGS on RP.Page_ID = PGS.Page_ID
                                     JOIN [dp_Roles] R on R.Role_ID = RP.Role_ID
@@ -21,6 +24,21 @@ BEGIN
        JOIN [dp_Pages] P on RP.Page_ID = P.Page_ID
        JOIN [dp_Roles] R on R.Role_ID = RP.Role_ID
        JOIN [dp_User_Roles] UR on UR.Role_ID = R.Role_ID
-       WHERE UR.User_ID = @UserId
+       WHERE UR.User_ID = @UserId)
+
+	INSERT INTO @SecurityList (Page_ID, Sub_Page_ID, Name, Security_Role)
+	(SELECT SP.Page_ID, SP.Sub_Page_ID, SP.Display_Name, 
+       STUFF((SELECT ','+ Role_Name FROM[dbo].[dp_Role_Sub_Pages] RP
+                                    JOIN [dp_Sub_Pages] SPGS on RP.Sub_Page_ID = SPGS.Sub_Page_ID
+                                    JOIN [dp_Roles] R on R.Role_ID = RP.Role_ID
+                                    JOIN [dp_User_Roles] UR on UR.Role_ID = R.Role_ID WHERE UR.User_ID = @UserId and SPGS.Sub_Page_ID = SP.Sub_Page_ID FOR XML Path('')),1,1,'') AS Security_Roles
+       
+       FROM [dbo].[dp_Role_Sub_Pages] RP
+       JOIN [dp_Sub_Pages] SP on RP.Sub_Page_ID = SP.Sub_Page_ID
+       JOIN [dp_Roles] R on R.Role_ID = RP.Role_ID
+       JOIN [dp_User_Roles] UR on UR.Role_ID = R.Role_ID
+       WHERE UR.User_ID = @UserId)
+
+	SELECT * FROM @SecurityList
 	END
 GO
