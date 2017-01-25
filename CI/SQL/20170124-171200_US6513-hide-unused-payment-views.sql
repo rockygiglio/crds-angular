9e6b856e4ec12c2d21db88c78e29e1e89c64f945
angie <angie.robey@gmail.com>
2017-01-25 14:36:41 -0500

    Added additional page views to hide

--- CI/SQL/20170124-171200_US6513-hide-unused-payment-views.sql
+++ CI/SQL/20170124-171200_US6513-hide-unused-payment-views.sql
@@ -3,6 +3,6 @@ GO
 DECLARE @genericuser INT = 9
 UPDATE dbo.dp_Page_Views
 SET User_ID = @genericuser
-WHERE Page_View_ID IN (746,747,748)
+WHERE Page_View_ID IN (744,746,747,748,749,750,751,752)
 
 GO
