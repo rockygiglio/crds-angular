e60f9b10bd592fd7fc8e21f820bf055a8d82c691
hookom <ihookom@callibrity.com>
2017-01-25 10:31:06 -0500

    Update TD_CommunityGroupsWith Waitlist.sql

--- CI/TestSQL_TearDown/02.Teardown_MPData/TD_CommunityGroupsWith Waitlist.sql
+++ CI/TestSQL_TearDown/02.Teardown_MPData/TD_CommunityGroupsWith Waitlist.sql
@@ -9,7 +9,7 @@ DELETE from [dbo].Event_Participants where Group_Participant_id in (select GROUP
 
 DELETE FROM Group_Participants WHERE Group_ID IN (SELECT Group_ID FROM Groups WHERE Group_Name = '(t) Fathers Oakley CG - Waitlist');
 
-UPDATE Groups SET Parent_Group = NULL  WHERE Group_Name = '(t) Fathers Oakley CG - Waitlist';
+UPDATE Groups SET Parent_Group = NULL  WHERE Group_Name like '(t) Fathers Oakley CG%';
 
 DELETE FROM [dbo].EVENT_GROUPS WHERE GROUP_ID = (select GROUP_ID from GROUPS where GROUP_NAME = '(t) Fathers Oakley CG');
 
