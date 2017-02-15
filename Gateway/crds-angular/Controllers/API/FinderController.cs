658dc9eb36f46254021677943815a7e330ae09ae
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-02-15 10:08:19 -0500

    US6418 - start to clean up - and go back - to merge back in Joe's branch....

--- Gateway/crds-angular/Controllers/API/FinderController.cs
+++ Gateway/crds-angular/Controllers/API/FinderController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Web.Http;
 using System.Web.Http.Description;
 using crds_angular.Exceptions.Models;
@@ -8,12 +9,15 @@ using crds_angular.Security;
 using crds_angular.Services.Interfaces;
 using Crossroads.ApiVersioning;
 using Crossroads.Web.Common.Security;
+using log4net;
+using MinistryPlatform.Translation.Models.Finder;
 
 namespace crds_angular.Controllers.API
 {
     public class FinderController : MPAuth
     {
         private readonly IFinderService _finderService;
+        private readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         public FinderController(IFinderService finderService, IUserImpersonationService userImpersonationService, IAuthenticationRepository authenticationRepository)
             : base(userImpersonationService, authenticationRepository)
@@ -38,5 +42,33 @@ namespace crds_angular.Controllers.API
                 throw new HttpResponseException(apiError.HttpResponseMessage);
             }
         }
+
+        /// <summary>
+        /// Create Pin with provided address details
+        /// </summary>
+        [RequiresAuthorization]
+        [ResponseType(typeof(PinDto))]
+        [VersionedRoute(template: "finder/pin", minimumVersion: "1.0.0")]
+        [Route("finder/pin")]
+        [HttpPost]
+        public IHttpActionResult PostPinDetails([FromBody] PinDto pin)
+        {
+            return Authorized(token =>
+            {
+                try
+                {
+                   // _finderService.SavePinDetails(pin);                    
+                    _logger.DebugFormat("Successfully created pin for participant {0} ", pin.Contact_ID);
+                    return (Ok());
+                }
+                // TODO: additional exception handling needed?
+                catch (Exception e)
+                {
+                    _logger.Error("Could not create pin", e);
+                    return BadRequest();
+                }
+            });
+        }
+
     }
 }
