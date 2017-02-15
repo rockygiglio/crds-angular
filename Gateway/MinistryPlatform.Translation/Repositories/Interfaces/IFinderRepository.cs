a054e51ca00cf5049ad11045233eccb5fcb48aa8
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-02-15 16:56:54 -0500

    US6418 interfaces WIP, merging dev again

--- Gateway/MinistryPlatform.Translation/Repositories/Interfaces/IFinderRepository.cs
+++ Gateway/MinistryPlatform.Translation/Repositories/Interfaces/IFinderRepository.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using MinistryPlatform.Translation.Models;
 using MinistryPlatform.Translation.Models.Finder;
 
 namespace MinistryPlatform.Translation.Repositories.Interfaces
@@ -7,5 +6,6 @@ namespace MinistryPlatform.Translation.Repositories.Interfaces
     public interface IFinderRepository
     {
         FinderPinDto GetPinDetails(int participantId);
+        void UpdatePinAddress(int contactId, Dictionary<string, object> addressDictionary, Dictionary<string, object> householdDictionary);
     }
 }
--- Gateway/crds-angular/Services/Interfaces/IFinderService.cs
+++ Gateway/crds-angular/Services/Interfaces/IFinderService.cs
@@ -1,11 +1,11 @@
-﻿using System.Device.Location;
-using crds_angular.Models.Finder;
-using crds_angular.Models.Crossroads;
+﻿using crds_angular.Models.Finder;
 
 namespace crds_angular.Services.Interfaces
 {
     public interface IFinderService
     {
         PinDto GetPinDetails(int participantId);
+        PinDto SavePin(string token, PinDto pin);
+
     }
 }
