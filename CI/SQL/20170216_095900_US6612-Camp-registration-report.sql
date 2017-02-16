ce5129c792df0ee80d6a034f4fae6a303c5764fd
hornerjn <hornerjn@HORNERJN-WINDOW>
2017-02-16 10:45:37 -0500

    US6612 Script camp registration report
    The procedure to create the report is currently empty except for the `@campId` parameter and has not
    been run.

---
+++ CI/SQL/20170216_095900_US6612-Camp-registration-report.sql
@@ -0,0 +1,29 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jon Horner
+-- Create date: 2/16/2017
+-- Description:	Gets data for camp registration report
+-- =============================================
+CREATE PROCEDURE report_CRDS_Camp_Registration
+	@campId int = 0
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    
+END
+GO
