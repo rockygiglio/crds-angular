61035fdc1cb9244ccf94b79a62533170e621f226
hornerjn <hornerjn@HORNERJN-WINDOW>
2017-02-17 10:17:12 -0500

    DE2783 Stopped checking other households in query
    Previously was getting many redundant records because contacts were
    getting compared for each additional household that they belong to after
    their primary household

--- CI/SQL/20170119_124900_US6243-create-duplicate-phone-sp.sql
+++ CI/SQL/20170119_124900_US6243-create-duplicate-phone-sp.sql
@@ -23,30 +23,53 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-    CREATE TABLE #household_phones (
-		Household_ID int
+    USE [MinistryPlatform]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jon Horner
+-- Create date: 2017-01-19
+-- Description:	Finds all of the duplicate Home and Mobile phone numbers
+-- =============================================
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[report_CRDS_Duplicate_Phone_Numbers]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[report_CRDS_Duplicate_Phone_Numbers] AS' 
+END
+GO
+
+ALTER PROCEDURE dbo.report_CRDS_Duplicate_Phone_Numbers
+	@Congregation_ID int = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    CREATE TABLE #phones (
+		ID int identity
+		, Household_ID int
 		, Congregation_ID int
 		, Household_Name nvarchar(75)
 		, Contact_ID int
-		, Display_Name nvarchar(75)
-		, Home_Phone nvarchar(25)
-		, Mobile_Phone nvarchar(25)
-		, PRIMARY KEY (Household_ID, Contact_ID)
+		, Display_Name nvarchar(75) NULL
+		, Home_Phone nvarchar(25) NULL
+		, Mobile_Phone nvarchar(25) NULL
+		, PRIMARY KEY (ID)
 	);
 
-	INSERT INTO #household_phones
+	INSERT INTO #phones
 		SELECT DISTINCT h.Household_ID
 			, h.Congregation_ID
 			, h.Household_Name
-			, ch.Contact_ID
+			, c.Contact_ID
 			, c.Display_Name
-			, replace(h.Home_Phone, '-', '')
-			, replace(c.Mobile_Phone, '-', '')
+			, replace(replace(replace(replace(h.Home_Phone, '-', ''), ')', ''), '(', ''), ' ', '')
+			, replace(replace(replace(replace(c.Mobile_Phone, '-', ''), ')', ''), '(', ''), ' ', '')
 		FROM dbo.Households h
-			, dbo.Contact_Households ch
-			, dbo.Contacts c
-		WHERE h.Household_ID = ch.Household_ID
-			AND ch.Contact_ID = c.Contact_ID;
+			JOIN Contacts c ON (c.Household_ID = h.Household_ID)
 
 	SELECT main.Household_ID
 		, main.Household_Name
@@ -60,8 +83,8 @@ BEGIN
 		, dup.Display_Name AS dup_Display_Name
 		, dup.Home_Phone AS dup_Home_Phone
 		, dup.Mobile_Phone AS dup_Mobile_Phone
-	FROM #household_phones main
-		inner join #household_phones dup ON
+	FROM #phones main
+		inner join #phones dup ON
 		(
 			main.Household_ID <> dup.Household_ID
 			AND main.Home_Phone = dup.Home_Phone
@@ -76,6 +99,6 @@ BEGIN
 	WHERE ISNULL(@Congregation_ID, 0) = 0
 		OR @Congregation_ID = main.Congregation_ID;
 
-	DROP TABLE #household_phones;
+	DROP TABLE #phones;
 END
 GO
