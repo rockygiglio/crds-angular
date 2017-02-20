d215cd54afb20e2d7567abe9fdc60e5dc5719699
hornerjn <hornerjn@HORNERJN-WINDOW>
2017-02-20 11:30:34 -0500

    US6612 Change create to alter
    Changed stored proc script to check if the sp already exists before
    creating.

--- CI/SQL/20170216_095900_US6612-Camp-registration-report.sql
+++ CI/SQL/20170216_095900_US6612-Camp-registration-report.sql
@@ -1,24 +1,23 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
+USE [MinistryPlatform]
+GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		Jon Horner
 -- Create date: 2/16/2017
 -- Description:	Gets data for camp registration report
 -- =============================================
-CREATE PROCEDURE report_CRDS_Camp_Registration
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[report_CRDS_Camp_Registration]') AND type in (N'P', N'PC'))
+BEGIN
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[report_CRDS_Camp_Registration] AS' 
+END
+GO
+
+ALTER PROCEDURE report_CRDS_Camp_Registration
 	@campId int = 0
 AS
 BEGIN
