cf0a0f8f5e6ebde2e2fbd6b30afbd947bebd052a
andycanterbury <andrew.canterbury@ingagepartners.com>
2017-02-22 16:34:04 -0500

    US6605 Use camper's congregation, not payer

--- CI/SQL/20170221_111140_US6605-PopulateCongregationIDonPaymentDetail.sql
+++ CI/SQL/20170221_111140_US6605-PopulateCongregationIDonPaymentDetail.sql
@@ -5,8 +5,8 @@ GO
 UPDATE pd
 SET pd.Congregation_ID = h.Congregation_ID
 FROM dbo.Payment_Detail pd
-JOIN dbo.Payments p on p.Payment_ID = pd.Payment_ID
-JOIN dbo.Contacts c on c.Contact_ID = p.Contact_ID
+JOIN dbo.Invoice_Detail i on i.Invoice_Detail_ID = pd.Invoice_Detail_ID
+JOIN dbo.Contacts c on c.Contact_ID = i.Recipient_Contact_ID
 JOIN dbo.Households h on c.Household_ID = h.Household_ID
 WHERE pd.Congregation_ID IS NULL
 
