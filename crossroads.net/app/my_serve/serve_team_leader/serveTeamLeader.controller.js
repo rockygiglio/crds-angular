900de27b6d688639ec5fc9ef0a1bead0fe85f9a5
Joe Kerstanoff <jkerstanoff@callibrity.com>
2017-02-22 15:12:35 -0500

    DE1232: HD12439 - Serve Signup - No Max Makes Opportunity Full

--- crossroads.net/app/my_serve/serve_team_leader/serveTeamLeader.controller.js
+++ crossroads.net/app/my_serve/serve_team_leader/serveTeamLeader.controller.js
@@ -23,6 +23,7 @@ export default class ServeTeamLeaderController {
       this.teamMembers = data;
     });
     _.each(this.team.serveOpportunities, (opp) => {
+      debugger;
       opp.capacity = this.serveTeamService.getCapacity(opp, this.team.eventId);
     });
 
