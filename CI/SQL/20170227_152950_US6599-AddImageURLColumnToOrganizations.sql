11ff1e6dfc7a1be4b8f1990abcbefa6d16ae765f
andycanterbury <andrew.canterbury@ingagepartners.com>
2017-02-27 16:36:22 -0500

    US6599 Populate ImageURLs

--- CI/SQL/20170227_152950_US6599-AddImageURLColumnToOrganizations.sql
+++ CI/SQL/20170227_152950_US6599-AddImageURLColumnToOrganizations.sql
@@ -11,3 +11,19 @@ BEGIN
 	SET Default_Field_List = 'Name,Open_Signup,Start_Date,End_Date,Primary_Contact_Table.Display_Name as [Primary_Contact],Image_URL'
 	WHERE Page_ID = @OrgPage
 END
+
+DECLARE @CRorg int = 2
+DECLARE @ArchOrg int = 1
+DECLARE @OtherOrg int = 3
+
+UPDATE cr_Organizations
+SET Image_URL = 'https://crds-cms-uploads.imgix.net/content/images/gc-crossroads.jpg'
+WHERE Organization_ID = @CRorg
+
+UPDATE cr_Organizations
+SET Image_URL = 'https://crds-cms-uploads.imgix.net/content/images/gc-archdiocese.jpg'
+WHERE Organization_ID = @ArchOrg
+
+UPDATE cr_Organizations
+SET Image_URL = 'https://crds-cms-uploads.imgix.net/content/images/gc-other.jpg'
+WHERE Organization_ID = @OtherOrg
