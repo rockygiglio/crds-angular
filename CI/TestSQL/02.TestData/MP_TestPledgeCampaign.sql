11e2cb5957aa5b5eb15739ab889779e8f379cbfc
Mike Lipps <michael.lipps@thrivecincinnati.com>
2017-03-01 14:50:28 -0500

    Correct error in pledge campaign. Increased goal and pledges.

--- CI/TestSQL/02.TestData/MP_TestPledgeCampaign.sql
+++ CI/TestSQL/02.TestData/MP_TestPledgeCampaign.sql
@@ -67,7 +67,7 @@ SET IDENTITY_INSERT [dbo].[Pledge_Campaigns] OFF;
 DBCC CHECKIDENT (Pledge_Campaigns, reseed, @curr_pledgeCampaign_id);
 
 --Update Program to point at pledge campaign
-UPDATE [dbo].Programs SET Pledge_Campaign_ID = 10000011 where program_name = '(t) Test Pledge Campaign2';
+UPDATE [dbo].Programs SET Pledge_Campaign_ID = 10000011 where program_name = '(t) Test Pledge Program2';
 
 --Pledge for (t) Test Pledge
 
@@ -107,7 +107,7 @@ SET @donorId = (SELECT Donor_ID FROM Donors WHERE Contact_ID = (SELECT Contact_I
 
 INSERT INTO Pledges
 (Donor_ID, Pledge_Campaign_ID, Pledge_Status_ID, Total_Pledge, Installments_Planned, Installments_Per_Year, First_Installment_Date      , Notes, Domain_ID, Beneficiary, Trip_Leader, Currency, __ExternalPersonID1, __ExternalPersonID2, __ExternalCommitmentID, __ExternalApplicationID, Trip_General_Fund) VALUES
-(@donorId, @pledgeCampaignId , 1               , 5000.00     , 10                  , 5000.00              ,  DATEADD(DAY, -3, GETDATE()), null , 1        , null       , null       , null    , null               , null               , null                  , null                   , null );               
+(@donorId, @pledgeCampaignId , 1               , 50000.00     , 0                  , 0                    ,  DATEADD(DAY, -3, GETDATE()), null , 1        , null       , null       , null    , null               , null               , null                  , null                   , null );               
 ---------------------------------------------------------------------------------------------------------------------------
 --Program for (t) Test Pledge Program+Auto2
 
@@ -124,18 +124,18 @@ SET @programId = (SELECT Program_ID FROM Programs WHERE Program_Name = '(t) Test
 
 INSERT INTO Pledge_Campaigns 
 (Pledge_Campaign_ID,Campaign_Name                      ,Nickname                        ,Pledge_Campaign_Type_ID,[Description]  ,Campaign_Goal,[Start_Date]                     ,End_Date ,Domain_ID,Event_ID,Program_ID,Registration_Details,Registration_Start ,Registration_End ,Maximum_Registrants,Youngest_Age_Allowed,Registration_Deposit,Fundraising_Goal,Registration_Form,Online_Pledge_Details,Allow_Online_Pledge,Online_Thank_You_Message,Pledge_Beyond_End_Date,Show_On_My_Pledges,__ExternalTripID,__ExternalFundID) VALUES
-(@pledgeCampaignId ,'(t) Test Pledge Campaign+Auto2'   ,'(t) Test Pledge Campaign+Auto2',1                      ,null           ,1000.00      ,CAST(@startDate as smalldatetime),null     ,1        ,null    ,@programId,null                ,null               ,null             ,null               , null               ,null                ,null            ,null             ,null                 ,1                  ,null                    ,1                     ,1                 ,40              ,99              );
+(@pledgeCampaignId ,'(t) Test Pledge Campaign+Auto2'   ,'(t) Test Pledge Campaign+Auto2',1                      ,null           ,10000000.00  ,CAST(@startDate as smalldatetime),null     ,1        ,null    ,@programId,null                ,null               ,null             ,null               , null               ,null                ,null            ,null             ,null                 ,1                  ,null                    ,1                     ,1                 ,40              ,99              );
 
 SET IDENTITY_INSERT [dbo].[Pledge_Campaigns] OFF;
 
 DBCC CHECKIDENT (Pledge_Campaigns, reseed, @curr_pledgeCampaign_id);
 
 --Update Program to point at pledge campaign
-UPDATE [dbo].Programs SET Pledge_Campaign_ID = 10000013 where program_name = '(t) Test Pledge Campaign+Auto2';
+UPDATE [dbo].Programs SET Pledge_Campaign_ID = 10000013 where program_name = '(t) Test Pledge Program+Auto2';
 
 --Pledge for (t) Test Pledge Program+Auto2
 SET @donorId = (SELECT Donor_ID FROM Donors WHERE Contact_ID = (SELECT Contact_ID FROM Contacts WHERE Email_Address = 'mpcrds+auto+2@gmail.com' AND Last_Name = 'Kenobi'));
 
 INSERT INTO Pledges
 (Donor_ID, Pledge_Campaign_ID, Pledge_Status_ID, Total_Pledge, Installments_Planned, Installments_Per_Year, First_Installment_Date      , Notes, Domain_ID, Beneficiary, Trip_Leader, Currency, __ExternalPersonID1, __ExternalPersonID2, __ExternalCommitmentID, __ExternalApplicationID, Trip_General_Fund) VALUES
-(@donorId, @pledgeCampaignId , 1               , 5000.00     , 10                  , 5000.00              ,  DATEADD(DAY, -3, GETDATE()), null , 1        , null       , null       , null    , null               , null               , null                  , null                   , null );               
+(@donorId, @pledgeCampaignId , 1               , 50000.00    , 0                   , 0                    ,  DATEADD(DAY, -3, GETDATE()), null , 1        , null       , null       , null    , null               , null               , null                  , null                   , null );               
