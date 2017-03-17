a46cb2ac09ac7bcdb6bf3207767575eef84a9a0f
Yan <yuy6@miamioh.edu>
2017-03-17 13:59:17 -0400

    add back-end code to validate phone number update

---
+++ Gateway/MinistryPlatform.Translation/Utils/PhoneNumberValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MinistryPlatform.Translation.Helpers
+{
+    public static class PhoneNumberValidator
+    {
+        private static readonly string pattern = "^\\d{3}-\\d{3}-\\d{4}$";
+
+        public static bool Validate(params string[] phoneNumbers)
+        {
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                Console.WriteLine("Phone number to be validated " + phoneNumber);
+
+                if (phoneNumber != null && phoneNumber != "")
+                {
+                    if (!Regex.IsMatch(phoneNumber, pattern))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
