3fe0e110420af96e51014b49e3550824b526e6b2
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-03-21 08:43:51 -0400

    US7251 - create AwsCloudsearchService

---
+++ Gateway/crds-angular/Services/AwsCloudsearchService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using crds_angular.Services.Interfaces;
+using Crossroads.Web.Common.Configuration;
+
+namespace crds_angular.Services
+{
+    public class AwsCloudsearchService: MinistryPlatformBaseService, IAwsCloudsearchService
+    {
+        private readonly IConfigurationWrapper _configurationWrapper;
+        public AwsCloudsearchService(
+                           IConfigurationWrapper configurationWrapper)
+        {
+            _configurationWrapper = configurationWrapper;
+        }
+    }
+}
---
+++ Gateway/crds-angular/Services/Interfaces/IAwsCloudsearchService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crds_angular.Services.Interfaces
+{
+    public interface IAwsCloudsearchService
+    {
+    }
+}
