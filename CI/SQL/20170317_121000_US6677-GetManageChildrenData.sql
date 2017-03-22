eae12adb6411a2ab02b239026a19394a6e47c476
dtkocher <dtkocher@gmail.com>
2017-03-22 15:19:20 -0400

    Add security.

--- CI/SQL/20170317_121000_US6677-GetManageChildrenData.sql
+++ CI/SQL/20170317_121000_US6677-GetManageChildrenData.sql
@@ -21,6 +21,9 @@ END
 GO
 USE [MinistryPlatform]
 GO
+
+GO
+
 /****** Object:  StoredProcedure [dbo].[api_crds_Get_Manage_Children_data]    Script Date: 3/17/2017 10:09:24 AM ******/
 SET ANSI_NULLS ON
 GO
@@ -112,3 +115,36 @@ BEGIN
 
 END
 GO
+
+GO
+
+DECLARE @PROCEDURE_NAME nvarchar(100) = N'api_crds_Get_Manage_Children_data';
+
+IF NOT EXISTS (SELECT 1 FROM dbo.dp_API_Procedures WHERE Procedure_Name = @PROCEDURE_NAME)
+BEGIN
+    INSERT INTO dbo.dp_API_Procedures (
+         Procedure_Name
+        ,Description
+    ) VALUES (
+         @PROCEDURE_NAME
+        ,N'Based on a contact_id and a group_id, returns a column if the contact is rsvpd in childcare'
+    )
+END
+
+DECLARE @PROCEDURE_ID int;
+DECLARE @API_ROLE_ID int = 112;
+
+SELECT @PROCEDURE_ID = API_Procedure_ID FROM dbo.dp_API_Procedures WHERE Procedure_Name = @PROCEDURE_NAME;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.dp_Role_API_Procedures WHERE Role_ID = @API_ROLE_ID AND API_Procedure_ID = @PROCEDURE_ID)
+BEGIN
+    INSERT INTO dbo.dp_Role_API_Procedures (
+         Role_ID
+        ,API_Procedure_ID
+        ,Domain_ID
+    ) VALUES (
+         @API_ROLE_ID
+        ,@PROCEDURE_ID
+        ,1
+    )
+END
