0a4ea6d7e3d585283c0acdc84f5cd94cec9dfbe6
jpc11623 <john.cleaver@ingagepartners.com>
2017-03-24 08:51:57 -0400

    US503 - Store Turned Away Kid Data
    Script to update participant status types

---
+++ CI/SQL/20170324_084700_US503-participant-status-types.sql
@@ -0,0 +1,37 @@
+USE [MinistryPlatform]
+GO
+
+SET IDENTITY_INSERT [dbo].[Participation_Statuses] ON
+
+IF NOT EXISTS( SELECT 1 FROM [dbo].[Participation_Statuses] WHERE [Participation_Status] = '06 Capacity')
+BEGIN
+
+	INSERT INTO [dbo].[Participation_Statuses]
+			   ([Participation_Status_ID]
+			   ,[Participation_Status]
+			   ,[Description]
+			   ,[Domain_ID])
+		 VALUES
+			   (6,
+			    '06 Capacity'
+			   ,N'Type used for Sign In'
+			   ,1)
+END
+
+IF NOT EXISTS( SELECT 1 FROM [dbo].[Participation_Statuses] WHERE [Participation_Status] = '07 Error')
+BEGIN
+
+	INSERT INTO [dbo].[Participation_Statuses]
+			   ([Participation_Status_ID]
+			   ,[Participation_Status]
+			   ,[Description]
+			   ,[Domain_ID])
+		 VALUES
+			   (7,
+			    '07 Error'
+			   ,N'Type used for Sign In'
+			   ,1)
+END
+
+SET IDENTITY_INSERT [dbo].[Participation_Statuses] OFF
+ 
