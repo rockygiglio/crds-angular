c3fb8764d2cc608310c58072f087674795ab6751
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-03-27 14:22:27 -0400

    unit test

--- Gateway/crds-angular.test/Services/AwsCloudsearchServiceTest.cs
+++ Gateway/crds-angular.test/Services/AwsCloudsearchServiceTest.cs
@@ -10,38 +10,28 @@ namespace crds_angular.test.Services
     [Category("IntegrationTests")]
     public class AwsCloudsearchServiceTest
     {
-        //private AwsCloudsearchService _fixture;
+        private AwsCloudsearchService _fixture;
+        private Mock<IFinderRepository> _mpFinderRepository;
+        private Mock<IConfigurationWrapper> _mpConfigurationWrapper;
         
-        //[SetUp]
-        //public void SetUp()
-        //{
-        //    _mpFinderRepository = new Mock<IFinderRepository>();
-        //    _mpConfigurationWrapper = new Mock<IConfigurationWrapper>();
+        [SetUp]
+        public void SetUp()
+        {
+            _mpFinderRepository = new Mock<IFinderRepository>();
+            _mpConfigurationWrapper = new Mock<IConfigurationWrapper>();
 
-        //    _fixture = new AwsCloudsearchService(_mpFinderRepository.Object, _mpConfigurationWrapper.Object);
+            _fixture = new AwsCloudsearchService(_mpFinderRepository.Object, _mpConfigurationWrapper.Object);
 
-        //    //force AutoMapper to register
-        //    AutoMapperConfig.RegisterMappings();
-        //}
+            //force AutoMapper to register
+            AutoMapperConfig.RegisterMappings();
+        }
 
-        //[Test]
-        //[Category("IntegrationTests")]
-        //public void ShouldReturnBoundingBox()
-        //{
-        //    var rc = _fixture.BuildBoundingBox("39$1234", "-84$51", "27$66", "-81.55");
-        //    Assert.IsNotNull(rc);
-        //    Assert.IsTrue( rc.UpperLeftCoordinates.Lat == 39.1234);
-        //}
-
-        //[Test]
-        //[Category("IntegrationTests")]
-        //public void TestGetProximityBadOrigin()
-        //{
-            
-        //    var result = _fixture.GetProximity("1234 test st, testy mctestface, te, 99999-9999, xyz", new List<AddressDTO> { address });
-        //    Assert.IsNotNull(result);
-        //    Assert.AreEqual(1, result.Count);
-        //    Assert.IsNull(result[0]);
-        //}
+        [Test]
+        public void ShouldReturnBoundingBox()
+        {
+            var rc = _fixture.BuildBoundingBox("39$1234", "-84$51", "27$66", "-81.55");
+            Assert.IsNotNull(rc);
+            Assert.IsTrue( rc.UpperLeftCoordinates.Lat == 39.1234);
+        }
     }
 }
