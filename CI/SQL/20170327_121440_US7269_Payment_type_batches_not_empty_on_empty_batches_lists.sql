02be4c89fad49522268dfe4fb4a2c8d78edaccb5
Yan <yyu@callibrity.com>
2017-03-27 13:38:00 -0400

    change escape character

--- CI/SQL/20170327_121440_US7269_Payment_type_batches_not_empty_on_empty_batches_lists.sql
+++ CI/SQL/20170327_121440_US7269_Payment_type_batches_not_empty_on_empty_batches_lists.sql
@@ -2,6 +2,6 @@ USE [MinistryPlatform]
 GO
 
 UPDATE [dbo].[dp_Page_Views]
-SET View_Clause = N'Batches.Setup_Date >= (SELECT top 1 CONVERT(DateTime,Value) FROM dp_Configuration_Settings CS WHERE ISDATE(CS.Value) = 1 AND CS.Key_Name = \'CutOverDate\' AND CS.Application_Code = \'COMMON\' AND CS.domain_ID = Batches.Domain_ID) AND NOT EXISTS (SELECT 1 FROM Donations WHERE Donations.Batch_ID = Batches.Batch_ID) AND NOT EXISTS (SELECT 1 FROM Payments WHERE Payments.Batch_ID = Batches.Batch_ID)'
+SET View_Clause = N'Batches.Setup_Date >= (SELECT top 1 CONVERT(DateTime,Value) FROM dp_Configuration_Settings CS WHERE ISDATE(CS.Value) = 1 AND CS.Key_Name = ''CutOverDate'' AND CS.Application_Code = ''COMMON'' AND CS.domain_ID = Batches.Domain_ID) AND NOT EXISTS (SELECT 1 FROM Donations WHERE Donations.Batch_ID = Batches.Batch_ID) AND NOT EXISTS (SELECT 1 FROM Payments WHERE Payments.Batch_ID = Batches.Batch_ID)'
 WHERE page_view_id = 363
 GO
