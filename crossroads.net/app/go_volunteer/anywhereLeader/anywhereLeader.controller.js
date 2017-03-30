0f83488f6891b3648865625fa3c61bacb039e8ad
Jon Horner <hornerjn@mail.uc.edu>
2017-03-30 16:34:14 -0400

    US7143 Wired up browser download

--- crossroads.net/app/go_volunteer/anywhereLeader/anywhereLeader.controller.js
+++ crossroads.net/app/go_volunteer/anywhereLeader/anywhereLeader.controller.js
@@ -1,9 +1,13 @@
 export default class AnywhereLeaderController {
   /* @ngInject */
-  constructor(GoVolunteerService) {
+  constructor($state, $log, GoVolunteerService, GoVolunteerDataService, FileSaver) {
+    this.state = $state;
+    this.log = $log;
     this.viewReady = false;
     this.project = GoVolunteerService.project;
     this.participants = GoVolunteerService.dashboard || [];
+    this.goVolunteerDataService = GoVolunteerDataService;
+    this.fileSaver = FileSaver;
   }
 
   $onInit() {
@@ -14,5 +18,11 @@ export default class AnywhereLeaderController {
     return this.participants
             .map(participant => participant.adults + participant.children)
             .reduce((acc, val) => acc + val, 1); // start at 1 to account for leader
+  }
+
+  getExport() {
+    return this.goVolunteerDataService.getDashboardExport(this.state.toParams.projectId).then((result) => {
+      this.fileSaver.saveAs(result.response.blob, result.response.filename);
+    }).catch(this.log.error);
   }
 }
--- crossroads.net/app/go_volunteer/goVolunteer.module.js
+++ crossroads.net/app/go_volunteer/goVolunteer.module.js
@@ -7,7 +7,7 @@ import goVolunteerDataService from './goVolunteerData.service';
 import skillsService from './skills.service';
 import groupConnectors from './groupConnectors.service';
 
-export default angular.module(constants.MODULES.GO_VOLUNTEER, ['crossroads.core', 'crossroads.common'])
+export default angular.module(constants.MODULES.GO_VOLUNTEER, ['crossroads.core', 'crossroads.common', 'ngFileSaver'])
   .config(routes)
   .config(formly)
   .service('GoVolunteerService', goVolunteerService)
