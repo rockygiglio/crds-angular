a64478a4d2b73dea9c88328b905b8ba45aa23ff2
Charlie Retzler <charlie.retzler@gmail.com>
2017-04-18 16:40:55 -0400

    US7757: Reworked to use correct env vars

--- crossroads.net/app/corkboard/services/corkboard.contact.service.js
+++ crossroads.net/app/corkboard/services/corkboard.contact.service.js
@@ -1,22 +1,21 @@
 (function () {
-'use strict';
+  'use strict';
 
-module.exports = ContactAboutPost;
-var emailEndpoint = __API_ENDPOINT__ + 'api/sendemail/';
+  module.exports = ContactAboutPost;
+  var emailEndpoint = __GATEWAY_CLIENT_ENDPOINT__ + 'corkboard/api/sendemail/';
 
-ContactAboutPost.$inject = ['$resource', '$http'];
+  ContactAboutPost.$inject = ['$resource', '$http'];
 
-function ContactAboutPost($resource, $http) {
+  function ContactAboutPost($resource, $http) {
 
-  return {
+    return {
       post: function (post) {
-          return $resource(emailEndpoint,
-              post,
-              {
-                  post: { method: 'POST' }
-                });
-        }
+        return $resource(emailEndpoint,
+          post,
+          {
+            post: { method: 'POST' }
+          });
+      }
     };
-};
-
+  };
 })();
--- crossroads.net/app/corkboard/services/corkboard.service.js
+++ crossroads.net/app/corkboard/services/corkboard.service.js
@@ -1,36 +1,36 @@
 (function () {
-'use strict';
+    'use strict';
 
-module.exports = CorkboardListings;
+    module.exports = CorkboardListings;
 
-CorkboardListings.$inject = ['$resource', '$http', '$cacheFactory'];
-var postEndpoint = __CORKBOARD_API_ENDPOINT__ + 'api/posts/';
+    CorkboardListings.$inject = ['$resource', '$http', '$cacheFactory'];
+    var postEndpoint = __SERVICES_CLIENT_ENDPOINT__ + '/corkboard/api/posts/';
 
-function CorkboardListings($resource, $http, $cacheFactory, $log) {
-  return {
-      InvalidateCache: function () {
-          $cacheFactory.get('$http').remove(postEndpoint);
-        },
+    function CorkboardListings($resource, $http, $cacheFactory, $log) {
+        return {
+            InvalidateCache: function () {
+                $cacheFactory.get('$http').remove(postEndpoint);
+            },
 
-      post: function (post) {
-          return $resource(postEndpoint,
-          post,
-          {
-              get: { method: 'GET', cache: true },
-              query: { method: 'GET', cache: true, isArray: true },
-              save: { method: 'POST' }
-            });
-        },
+            post: function (post) {
+                return $resource(postEndpoint,
+                    post,
+                    {
+                        get: { method: 'GET', cache: true },
+                        query: { method: 'GET', cache: true, isArray: true },
+                        save: { method: 'POST' }
+                    });
+            },
 
-      flag: function () {
-          return $resource(postEndpoint + 'flag/:id',
-          { id: '@id' },
-          {
-              post: { method: 'POST', params: { id: '@id' } }
-            });
-        }
-    };
+            flag: function () {
+                return $resource(postEndpoint + 'flag/:id',
+                    { id: '@id' },
+                    {
+                        post: { method: 'POST', params: { id: '@id' } }
+                    });
+            }
+        };
 
-}
+    }
 
 })();
