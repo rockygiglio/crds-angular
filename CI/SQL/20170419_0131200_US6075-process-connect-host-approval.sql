0c0cb11969ab7dd9a686d61d56f074e88d1fbc2d
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-04-19 15:59:55 -0400

    US6075 process for email on host approval

---
+++ CI/SQL/20170419_0131200_US6075-process-connect-host-approval.sql
@@ -0,0 +1,76 @@
+USE [MinistryPlatform]
+GO
+
+IF NOT EXISTS(SELECT 1 FROM [dbo].[dp_Processes] WHERE Process_ID = 50)
+BEGIN
+	SET IDENTITY_INSERT [dbo].[dp_Processes] ON
+	INSERT INTO [dbo].[dp_Processes]
+			   ([Process_ID]
+			   ,[Process_Name]
+			   ,[Process_Manager]
+			   ,[Active]
+			   ,[Description]
+			   ,[Record_Type]
+			   ,[Domain_ID]
+			   ,[On_Submit]
+			   ,[On_Complete]
+			   ,[Trigger_Fields]
+			   ,[Dependent_Condition])
+		 VALUES
+			   (50
+			   ,'HostApprovedNotification'
+			   ,5       -- Church Administrator
+			   ,1
+			   ,'After Anywhere Gathering Host is approved, send email'
+			   ,355     -- Participants
+			   ,1
+			   ,NULL
+			   ,NULL
+			   ,'Host_Status_ID'
+			   ,'Host_Status_ID=1')
+	SET IDENTITY_INSERT [dbo].[dp_Processes] OFF
+END
+
+IF NOT EXISTS(SELECT 1 FROM [dbo].[dp_Process_Steps] WHERE Process_Step_ID = 85)
+BEGIN
+	SET IDENTITY_INSERT [dbo].[dp_Process_Steps] ON
+	INSERT INTO [dbo].[dp_Process_Steps]
+			   ([Process_Step_ID]
+			   ,[Step_Name]
+			   ,[Instructions]
+			   ,[Process_Step_Type_ID]
+			   ,[Escalation_Only]
+			   ,[Order]
+			   ,[Process_ID]
+			   ,[Specific_User]
+			   ,[Supervisor_User]
+			   ,[Lookup_User_Field]
+			   ,[Domain_ID]
+			   ,[Escalate_to_Step]
+			   ,[Task_Deadline]
+			   ,[Email_Template]
+			   ,[To_Specific_Contact]
+			   ,[Email_To_Contact_Field]
+			   ,[SQL_Statement])
+		 VALUES
+			   (85
+			   ,'Send Approved Connect Host Notification'
+			   ,NULL
+			   ,4
+			   ,0
+			   ,1
+			   ,50
+			   ,NULL
+			   ,0
+			   ,NULL
+			   ,1
+			   ,NULL
+			   ,NULL
+			   ,2014
+			   ,NULL
+			   ,'Participant_ID_Table_Contact_ID_Table.Contact_ID'
+			   ,NULL)
+	SET IDENTITY_INSERT [dbo].[dp_Process_Steps] ON
+END
+
+
---
+++ CI/SQL/20170419_070111_US6075-connect-host-approval-email.sql
@@ -0,0 +1,39 @@
+USE [MinistryPlatform]
+GO
+
+DECLARE @TemplateID int = 2014;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.dp_Communications WHERE Communication_ID = @TemplateID)
+BEGIN
+	SET IDENTITY_INSERT [dbo].[dp_Communications] ON
+
+	INSERT INTO [dbo].[dp_Communications]
+			   ([Communication_ID]
+			   ,[Author_User_ID]
+			   ,[Subject]
+			   ,[Body]
+			   ,[Domain_ID]
+			   ,[Start_Date]
+			   ,[Communication_Status_ID]
+			   ,[From_Contact]
+			   ,[Reply_to_Contact]
+			   ,[Template]
+			   ,[Active])
+		 VALUES
+			   (@TemplateID
+			   ,5 --Church Administrator
+			   ,'Crossroads Connect Gathering Host Approved'
+			   ,'We are excited that you will be on the map as a Gathering Host on Crossroads Connect.<br><br>
+			   Thanks again for joining the team and welcoming others!<br>
+			   The Crossroads Anywhere Team'
+			   ,1 --Domain ID
+			   ,GETDATE()
+			   ,1 --Draft Status
+			   ,768371  -- anywhere@crossroads.net
+			   ,768371  -- anywhere@crossroads.net
+			   ,1 --Template
+			   ,1) --Active
+
+	SET IDENTITY_INSERT [dbo].[dp_Communications] OFF
+
+END
