1e880a5ad0f222d3d4edbc6188b2337b853507b4
Jonathan Horner <Ganonside@users.noreply.github.com>
2017-04-21 10:12:00 -0400

    Update 20170417_115800_US7367-share-your-story-form.sql
    Added static form field ids

--- CI/SQL/20170417_115800_US7367-share-your-story-form.sql
+++ CI/SQL/20170417_115800_US7367-share-your-story-form.sql
@@ -19,44 +19,65 @@ DECLARE @lgTextBoxID INT = 2;
 DECLARE @domainID INT = 1;
 DECLARE @required INT = 1;
 
-INSERT INTO Form_Fields
-(
-	Field_Order
-	, Field_Label
-	, Field_Type_ID
-	, [Required]
-	, Form_ID
-	, Domain_ID
-	, Placement_Required
-)
-VALUES
-(
-	40
-	, 'Spiritual Growth as a Disciple'
-	, @lgTextBoxID
-	, @required
-	, @glaFormID
-	, @domainID
-	, 0
-);
+DECLARE @storyFieldId INT = 1522;
+DECLARE @taughtFieldId INT = 1523;
 
-INSERT INTO Form_Fields
-(
-	Field_Order
-	, Field_Label
-	, Field_Type_ID
-	, [Required]
-	, Form_ID
-	, Domain_ID
-	, Placement_Required
-)
-VALUES
-(
-	50
-	, 'What has God Taught You'
-	, @lgTextBoxID
-	, @required
-	, @glaFormID
-	, @domainID
-	, 0
-);
+IF NOT EXISTS (SELECT 1 FROM Form_Fields WHERE Form_Field_ID = @storyFieldId)
+BEGIN
+	SET IDENTITY_INSERT Form_Fields ON;
+
+	INSERT INTO Form_Fields
+	(
+		Form_Field_ID
+		, Field_Order
+		, Field_Label
+		, Field_Type_ID
+		, [Required]
+		, Form_ID
+		, Domain_ID
+		, Placement_Required
+	)
+	VALUES
+	(
+		@storyFieldId
+		, 40
+		, 'Spiritual Growth as a Disciple'
+		, @lgTextBoxID
+		, @required
+		, @glaFormID
+		, @domainID
+		, 0
+	);
+
+	SET IDENTITY_INSERT Form_Fields OFF;
+END
+
+IF NOT EXISTS (SELECT 1 FROM Form_Fields WHERE Form_Field_ID = @taughtFieldId)
+BEGIN
+	SET IDENTITY_INSERT Form_Fields ON;
+
+	INSERT INTO Form_Fields
+	(
+		Form_Field_ID
+		, Field_Order
+		, Field_Label
+		, Field_Type_ID
+		, [Required]
+		, Form_ID
+		, Domain_ID
+		, Placement_Required
+	)
+	VALUES
+	(
+		@taughtFieldId
+		, 50
+		, 'What has God Taught You'
+		, @lgTextBoxID
+		, @required
+		, @glaFormID
+		, @domainID
+		, 0
+	);
+
+	SET IDENTITY_INSERT Form_Fields OFF;
+END
