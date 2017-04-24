cf91b6c4100e02727d5270bf0d83541a13b902ee
Phil Lachmann <phil.lachmann@ingagepartners.com>
2017-04-24 15:00:28 -0400

    US7564 - models for connect communication

---
+++ Gateway/MinistryPlatform.Translation/Models/Finder/MpConnectCommunication.cs
@@ -0,0 +1,27 @@
+﻿using Crossroads.Web.Common.MinistryPlatform;
+using Newtonsoft.Json;
+
+namespace MinistryPlatform.Translation.Models.Finder
+{
+    [MpRestApiTable(Name = "cr_Connect_Communications")]
+    public class MpConnectCommunication
+    {
+        [JsonProperty(PropertyName = "Connect_Communication_ID")]
+        public int ConnectCommunicationId { get; set; }
+
+        [JsonProperty(PropertyName = "FromUser_Contact_ID")]
+        public int FromUserContactId { get; set; }
+
+        [JsonProperty(PropertyName = "ToUser_Contact_ID")]
+        public int ToUserContactId { get; set; }
+
+        [JsonProperty(PropertyName = "Communication_ID")]
+        public int CommunicationId { get; set; }
+
+        [JsonProperty(PropertyName = "Communication_Type")]
+        public string CommunicationType { get; set; }
+
+        [JsonProperty(PropertyName = "Communication_Status")]
+        public string CommunicationStatus { get; set; }
+    }
+}
---
+++ Gateway/crds-angular/Models/Finder/ConnectCommunicationDto.cs
@@ -0,0 +1,26 @@
+﻿using crds_angular.Models.Crossroads;
+using Newtonsoft.Json;
+
+namespace crds_angular.Models.Finder
+{
+    public class ConnectCommunicationDto
+    {
+        [JsonProperty("connectCommunicationsId")]
+        public int ConnectCommunicationsId { get; set; }
+
+        [JsonProperty("fromUserContactId")]
+        public int FromUserContactId { get; set; }
+
+        [JsonProperty("toUserContactId")]
+        public int ToUserContactId { get; set; }
+
+        [JsonProperty("communicationsId")]
+        public int CommunicationsId { get; set; }
+
+        [JsonProperty("communicationType")]
+        public string CommunicationType { get; set; }
+
+        [JsonProperty("communicationStatus")]
+        public string CommunicationStatus { get; set; }
+    }
+}
