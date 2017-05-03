0b6f6099ad93f931733aae47827958b6983b7878
Charlie Retzler <charlie.retzler@gmail.com>
2017-05-03 16:28:29 -0400

    US7757: Adding code to call corkboard API

--- Gateway/crds-angular/Services/CorkboardService.cs
+++ Gateway/crds-angular/Services/CorkboardService.cs
@@ -1,5 +1,7 @@
-﻿using crds_angular.Services.Interfaces;
-using Crossroads.Web.Common.Configuration;
+﻿using System;
+using System.Net;
+using crds_angular.Services.Interfaces;
+using Crossroads.Web.Common.MinistryPlatform;
 using RestSharp;
 
 namespace crds_angular.Services
@@ -7,18 +9,27 @@ namespace crds_angular.Services
     public class CorkboardService: ICorkboardService
     {
         private readonly IRestClient _corkboardRestClient;
-        private readonly IConfigurationWrapper _configuration;
+        private readonly IApiUserRepository _apiUserRepository;
 
-        public CorkboardService(IRestClient corkboardRestClient, IConfigurationWrapper configuration)
+        public CorkboardService(IRestClient corkboardRestClient, IApiUserRepository apiUserRepository)
         {
             _corkboardRestClient = corkboardRestClient;
-            _configuration = configuration;
+            _apiUserRepository = apiUserRepository;
         }
 
             
         public void SyncPosts()
         {
-            // TODO: Implement code
+            // TODO: What do we do with Client API token?
+            var token = _apiUserRepository.GetToken();                    
+            var request = new RestRequest("corkboard/api/v1.0.0/syncposts", Method.POST);
+            request.AddParameter("Authorization",$"Bearer {token}", ParameterType.HttpHeader);
+
+            var response = _corkboardRestClient.Execute(request);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new ApplicationException($"Received {response.StatusCode} status code from corkboard api.");
+            }
         }
     }
 }
