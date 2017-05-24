b81bbb9524b4e07ab1694b1cdb587312865f5866
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-05-24 13:36:49 -0400

    US8241 lost use MP.... ??

--- CI/SQL/20170524_100322_US8241-insert-into-connect-audit-log.sql
+++ CI/SQL/20170524_100322_US8241-insert-into-connect-audit-log.sql
@@ -1,3 +1,6 @@
+USE [MinistryPlatform]
+GO
+
 INSERT INTO cr_connect_communications_type (Communication_Type, Start_Date, Domain_ID)
 VALUES  ('Request To Join Small Group', GETDATE(), 1)
       , ('Invite to Small Group', GETDATE(), 1)
@@ -7,5 +10,3 @@ VALUES  ('Request To Join Small Group', GETDATE(), 1)
 UPDATE cr_connect_communications_type
 SET Communication_Type = 'Request To Join Gathering'
 WHERE Connect_Communications_Type_ID = 3
-
-GO
