1e03aa819836defe31b0d7b2a17ce2d3161c005e
Tim Giblin <timmyg13@gmail.com>
2017-05-25 11:03:20 -0400

    Revert "remove tests"
    This reverts commit f7304ad1b89162967f0344e805853a039c33d5ab.

--- crossroads.net/spec-es6/invoices/invoices.service.spec.js
+++ crossroads.net/spec-es6/invoices/invoices.service.spec.js
@@ -1,23 +1,23 @@
-// import invoicesModule from '../../app/invoices/invoices.module';
-//
-// describe('Invoice Service', () => {
-//   // eslint-disable-next-line no-underscore-dangle
-//   const endpoint = `${window.__env__.CRDS_GATEWAY_CLIENT_ENDPOINT}api`;
-//   let invoicesService;
-//   let httpBackend;
-//
-//   beforeEach(angular.mock.module(invoicesModule));
-//
-//   beforeEach(inject((_InvoicesService_, _$httpBackend_) => {
-//     invoicesService = _InvoicesService_;
-//     httpBackend = _$httpBackend_;
-//   }));
-//
-//   it('should make the API call to get invoice', () => {
-//     const invoiceId = 111;
-//     httpBackend.expectGET(`${endpoint}/v1.0.0/invoice/${invoiceId}`)
-//       .respond(200, {});
-//     invoicesService.getInvoice(invoiceId);
-//   });
-//
-// });
+import invoicesModule from '../../app/invoices/invoices.module';
+
+describe('Invoice Service', () => {
+  // eslint-disable-next-line no-underscore-dangle
+  const endpoint = `${window.__env__.CRDS_GATEWAY_CLIENT_ENDPOINT}api`;
+  let invoicesService;
+  let httpBackend;
+
+  beforeEach(angular.mock.module(invoicesModule));
+
+  beforeEach(inject((_InvoicesService_, _$httpBackend_) => {
+    invoicesService = _InvoicesService_;
+    httpBackend = _$httpBackend_;
+  }));
+
+  it('should make the API call to get invoice', () => {
+    const invoiceId = 111;
+    httpBackend.expectGET(`${endpoint}/v1.0.0/invoice/${invoiceId}`)
+      .respond(200, {});
+    invoicesService.getInvoice(invoiceId);
+  });
+
+});
