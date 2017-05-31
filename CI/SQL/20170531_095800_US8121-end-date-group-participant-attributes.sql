11b87e98f2b8f70694b15d5163a729fbb6440f61
Chris Branch <chris.branch@ingagepartners.com>
2017-05-31 11:14:48 -0400

    US8121 - Clean up End_Date for Group Participant Attributes * New trigger that will sync End_Date in Group_Participant_Attributes * New index on Group_Participant_Attributes to ensure the trigger is efficient * One-time query to clean up existing data

---
+++ CI/SQL/20170531_095800_US8121-end-date-group-participant-attributes.sql
@@ -0,0 +1,26 @@
+﻿USE [MinistryPlatform]
+GO
+
+CREATE INDEX IX_Group_Participant_Attributes__GroupParticipantID ON Group_Participant_Attributes(Group_Participant_ID);
+GO
+
+CREATE TRIGGER crds_tr_End_Date_Group_Participant_Attributes
+	ON Group_Participants
+	AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	-- When Group_Participants.End_Date is modified, apply the same change to Group_Participant_Attributes.End_Date
+	IF UPDATE(End_Date)
+	BEGIN
+		UPDATE gpa
+		SET gpa.End_Date = gp.End_Date
+		FROM
+			INSERTED i
+			INNER JOIN Group_Participants gp ON gp.Group_Participant_ID = i.Group_Participant_ID
+			INNER JOIN Group_Participant_Attributes gpa ON gpa.Group_Participant_ID = gp.Group_Participant_ID
+		;
+	END
+END
+GO
