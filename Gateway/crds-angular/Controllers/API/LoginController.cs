5c7e1bccd6993c3377f8f6c9576aaf334ba466f1
Jim Kriz <jim.kriz@ingagepartners.com>
2017-06-05 10:26:31 -0400

    US7742: Fix failing unit test

--- Gateway/crds-angular/Controllers/API/LoginController.cs
+++ Gateway/crds-angular/Controllers/API/LoginController.cs
@@ -9,6 +9,7 @@ using crds_angular.Services;
 using crds_angular.Services.Interfaces;
 using MinistryPlatform.Translation.Models.DTO;
 using Crossroads.ApiVersioning;
+using Crossroads.ClientApiKeys;
 using Crossroads.Web.Common.Security;
 using MinistryPlatform.Translation.Repositories.Interfaces;
 
@@ -112,6 +113,7 @@ namespace crds_angular.Controllers.API
         [VersionedRoute(template: "login", minimumVersion: "1.0.0")]
         [Route("login")]
         [ResponseType(typeof (LoginReturn))]
+        [IgnoreClientApiKey]
         public IHttpActionResult Post([FromBody] Credentials cred)
         {
             try
