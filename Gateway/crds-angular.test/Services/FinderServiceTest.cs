912fd5bea04f62fd89f12a49dcdb469f59302924
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-07-10 12:46:28 -0400

    US8257 add filter param

--- Gateway/crds-angular.test/Services/FinderServiceTest.cs
+++ Gateway/crds-angular.test/Services/FinderServiceTest.cs
@@ -385,7 +385,7 @@ namespace crds_angular.test.Services
                 BottomRightCoordinates = new GeoCoordinates(21.52, -77.78)
             };
 
-            List<PinDto> pins = _fixture.GetPinsInBoundingBox(originCoords, address, boundingBox, "CONNECT", 0);
+            List<PinDto> pins = _fixture.GetPinsInBoundingBox(originCoords, address, boundingBox, "CONNECT", 0, "filterstring");
 
             Assert.IsInstanceOf<List<PinDto>>(pins);
         }
