80da354e9c6c10f4e42035372361fe3f8a4aa2fc
ING062\sara.seissiger <sara.seissiger@ingagepartners.com>
2017-07-13 16:08:07 -0400

    US8575 unit

--- Gateway/crds-angular.test/Services/GroupServiceTest.cs
+++ Gateway/crds-angular.test/Services/GroupServiceTest.cs
@@ -76,7 +76,6 @@ namespace crds_angular.test.Services
         private const int GroupTypeAttributeTypeId = 20;
         private const int GroupAgeRangeAttributeTypeId = 30;
         private const int DefaultAuthorUser = 5;
-        private const int GroupRoleLeader = 16;
         private const int RemoveSelfFromGroupTemplateId = 2024;
 
 
@@ -117,7 +116,6 @@ namespace crds_angular.test.Services
             config.Setup(mocked => mocked.GetConfigIntValue("GroupTypeAttributeTypeId")).Returns(GroupTypeAttributeTypeId);
             config.Setup(mocked => mocked.GetConfigIntValue("GroupAgeRangeAttributeTypeId")).Returns(GroupAgeRangeAttributeTypeId);
             config.Setup(mocked => mocked.GetConfigIntValue("DefaultAuthorUser")).Returns(DefaultAuthorUser);
-            config.Setup(mocked => mocked.GetConfigIntValue("GroupRoleLeader")).Returns(GroupRoleLeader);
             config.Setup(mocked => mocked.GetConfigIntValue("RemoveSelfFromGroupTemplateId")).Returns(RemoveSelfFromGroupTemplateId);
 
             fixture = new GroupService(groupRepository.Object,
