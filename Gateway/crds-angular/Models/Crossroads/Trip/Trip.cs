fa44680ad9a3bbb08a470f8a1f93e58cdd726733
ganonside <jon.horner@ingagepartners.com>
2017-07-19 17:51:16 -0400

    Trying to retrieve waivers for frontend

--- Gateway/crds-angular/Models/Crossroads/Trip/Trip.cs
+++ Gateway/crds-angular/Models/Crossroads/Trip/Trip.cs
@@ -41,6 +41,15 @@ namespace crds_angular.Models.Crossroads.Trip
         [JsonProperty(PropertyName = "eventParticipantFirstName")]
         public string EventParticipantFirstName { get; set; }
 
+        [JsonProperty(PropertyName = "waiverId")]
+        public int WaiverId { get; set; }
+
+        [JsonProperty(PropertyName = "waiverSigned")]
+        public bool WaiverSigned { get; set; }
+
+        [JsonProperty(PropertyName = "contactId")]
+        public int ContactId { get; set; }
+
         public Trip()
         {
             TripGifts = new List<TripGift>();
