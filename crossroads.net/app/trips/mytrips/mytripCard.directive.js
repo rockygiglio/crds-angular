d5bc940dd5c0a64d5dc9f8f7d84c02e7fe59e0a6
Jon Horner <hornerjn@mail.uc.edu>
2017-07-19 17:01:13 -0400

    US8165 checking if waiver is signed on frontend

--- crossroads.net/app/trips/mytrips/mytripCard.directive.js
+++ crossroads.net/app/trips/mytrips/mytripCard.directive.js
@@ -3,9 +3,9 @@
 
   module.exports = MyTripCard;
 
-  MyTripCard.$inject = ['$log', 'TripsUrlService'];
+  MyTripCard.$inject = ['$log', '$cookies', 'TripsUrlService'];
 
-  function MyTripCard($log, TripsUrlService) {
+  function MyTripCard($log, $cookies, TripsUrlService) {
     return {
       restrict: 'EA',
       transclude: true,
@@ -21,6 +21,8 @@
       scope.cardName = cardName;
       scope.goalMet = goalMet;
       scope.shareUrl = TripsUrlService.ShareUrl(scope.trip.eventParticipantId);
+      scope.showWaiver = showWaiver;
+      scope.hasSignedWaiver = hasSignedWaiver;
 
       function goalMet(totalRaised, goal) {
         return (totalRaised >= goal);
@@ -28,6 +30,15 @@
 
       function cardName(first, last) {
         return first + ' ' + last;
+      }
+
+      function showWaiver(contactId) {
+        const loggedInContact = $cookies.get('userId');
+        return contactId === parseInt(loggedInContact);
+      }
+
+      function hasSignedWaiver(signed) {
+        return signed;
       }
     }
   }
