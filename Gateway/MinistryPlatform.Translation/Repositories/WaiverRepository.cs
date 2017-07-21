c11237f48dd0ee67b6f72b47bd8adf53bd05071e
silbermm <silbermm@gmail.com>
2017-07-21 09:42:52 -0400

    US8165

--- Gateway/MinistryPlatform.Translation/Repositories/WaiverRepository.cs
+++ Gateway/MinistryPlatform.Translation/Repositories/WaiverRepository.cs
@@ -2,6 +2,7 @@
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Crossroads.Web.Common.Configuration;
+using Crossroads.Web.Common.MinistryPlatform;
 using Crossroads.Web.Common.Security;
 using MinistryPlatform.Translation.Models;
 using MinistryPlatform.Translation.Repositories.Interfaces;
@@ -10,14 +11,29 @@ namespace MinistryPlatform.Translation.Repositories
 {
     public class WaiverRepository : BaseRepository,IWaiverRepository
     {
-        public WaiverRepository(IAuthenticationRepository authenticationService, IConfigurationWrapper configurationWrapper) : base(authenticationService, configurationWrapper)
+
+        private readonly IMinistryPlatformRestRepository _ministryPlatformRestRepository;
+
+        public WaiverRepository(IMinistryPlatformRestRepository ministryPlatformRestRepository, IAuthenticationRepository authenticationService, IConfigurationWrapper configurationWrapper) : base(authenticationService, configurationWrapper)
         {
+            _ministryPlatformRestRepository = ministryPlatformRestRepository;
         }
 
         public IObservable<MpWaivers> GetWaiver(int waiverId)
         {
             return Observable.Create<MpWaivers>(observer =>
             {
+                try
+                {
+                    var apiToken = ApiLogin();
+                    var res = _ministryPlatformRestRepository.UsingAuthenticationToken(apiToken).Get<MpWaivers>(waiverId);
+                    observer.OnNext(res);
+                    observer.OnCompleted();
+                }
+                catch (Exception e)
+                {
+                    observer.OnError(e);
+                }               
                 return Disposable.Empty;
             });
         }
