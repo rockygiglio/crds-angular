1fd35bd67c39a8a0802131fbbb57230a220eea97
silbermm <silbermm@gmail.com>
2017-07-25 16:18:23 -0400

    configure unitity with Waiver service and repo

--- Gateway/crds-angular/Services/Interfaces/IWaiverService.cs
+++ Gateway/crds-angular/Services/Interfaces/IWaiverService.cs
@@ -1,7 +1,12 @@
-﻿namespace crds_angular.Services.Interfaces
+﻿using System;
+using crds_angular.Models.Crossroads.Waivers;
+
+namespace crds_angular.Services.Interfaces
 {
     public interface IWaiverService
     {
-        
+
+        IObservable<WaiverDTO> GetWaiver(int waiverId);
+
     }
 }
--- Gateway/crds-angular/Services/WaiverService.cs
+++ Gateway/crds-angular/Services/WaiverService.cs
@@ -1,7 +1,32 @@
-﻿namespace crds_angular.Services
+﻿using System;
+using System.Reactive.Linq;
+using crds_angular.Models.Crossroads.Waivers;
+using crds_angular.Services.Interfaces;
+using MinistryPlatform.Translation.Models;
+using MinistryPlatform.Translation.Repositories.Interfaces;
+
+namespace crds_angular.Services
 {
-    public class WaiverService
+    public class WaiverService : IWaiverService
     {
-        
+        private readonly IWaiverRepository _waiverRepository;
+
+        public WaiverService(IWaiverRepository waiverRepository)
+        {
+            _waiverRepository = waiverRepository;
+        }
+
+        public IObservable<WaiverDTO> GetWaiver(int waiverId)
+        {            
+            return _waiverRepository.GetWaiver(waiverId).Select<MpWaivers, WaiverDTO>(w => new WaiverDTO
+            {
+                Accepted = w.Accepted,
+                Required = w.Required,
+                SigneeContactId = w.SigneeContactId,
+                WaiverId = w.WaiverId,
+                WaiverName = w.WaiverName,
+                WaiverText = w.WaiverText
+            });
+        }
     }
 }
