35f005e771e0f0cc16b7a6a706dad21cdc2081d6
Jon Horner <hornerjn@mail.uc.edu>
2017-07-26 16:08:06 -0400

    US8165 Added Controller for Accept

--- crossroads.net/app/waivers/waivers.routes.js
+++ crossroads.net/app/waivers/waivers.routes.js
@@ -16,13 +16,40 @@ export default function WaiversRoutes($stateProvider) { /* , $urlMatcherFactoryP
         }
       },
       resolve: {
+        $log: '$log',
+        $rootScope: '$rootScope',
         $state: '$state',
         loggedin: crds_utilities.checkLoggedin,
         waiversService: 'WaiversService'
       }
     })
-    // .state('accept-waiver', {
-    //
-    // })
-    ;
+    .state('accept-waiver', {
+      parent: 'noHeaderOrFooter',
+      url: '/waivers/accept/:waiverId/:eventParticipantId',
+      template: '<accept-waiver></accept-waiver>',
+      controller: ($log, $rootScope, $state, waiversService) => {
+        const { waiverId, eventParticipantId } = $state.params;
+
+        waiversService.acceptWaiver(waiverId, eventParticipantId).then(() => {
+          $state.go('mytrips');
+        }).catch((err) => {
+          $log.error(err);
+          $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+        });
+      },
+      data: {
+        isProtected: true,
+        meta: {
+          title: 'Accept Waiver',
+          description: ''
+        }
+      },
+      resolve: {
+        $log: '$log',
+        $rootScope: '$rootScope',
+        $state: '$state',
+        loggedin: crds_utilities.checkLoggedin,
+        waiversService: 'WaiversService'
+      }
+    });
 }
