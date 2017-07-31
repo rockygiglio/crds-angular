c03caf0cbfd539d08635171290ef445486e4daee
Ken Baum <ken.baum@ingagepartners.com>
2017-07-31 09:32:18 -0400

    US9163 add track calls and tests

---
+++ Gateway/crds-angular/Services/Analytics/AnalyticsAstronomer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+using MinistryPlatform.Translation.PlatformService;
+using Segment;
+using Segment.Model;
+
+namespace crds_angular.Services.Analytics
+{
+    public class AnalyticsAstronomer
+    {
+        private string ASTRONOMER_APPLICATION_ID = "sZQew9P77arhJe3Qx";
+
+        public  void Track(string userId, string eventName, EventProperties props)
+        {
+            Properties segProps = mapProps(props);
+            Segment.Analytics.Initialize(ASTRONOMER_APPLICATION_ID, new Config().SetAsync(false));
+            Segment.Analytics.Client.Track(userId, eventName, segProps);
+
+        }
+
+        private Properties mapProps(EventProperties eventProps)
+        {
+            var props = new Properties();
+            foreach (KeyValuePair<string, object> p in eventProps)
+            {
+                props.Add(p.Key, p.Value);
+            }
+            return props;
+        }
+    }
+}
---
+++ Gateway/crds-angular/Services/Analytics/AnalyticsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using crds_angular.Services;
+using crds_angular.Services.Interfaces;
+
+namespace crds_angular.Services.Analytics
+{
+    public class AnalyticsService: IAnalyticsService
+    {
+        public void Track(string userId, string eventName)
+        {
+            EventProperties props = new EventProperties();
+            props.Add("Source", "CrossroadsNet");
+            var astronomer = new AnalyticsAstronomer();
+            astronomer.Track(userId, eventName, props);
+        }
+    }
+}
