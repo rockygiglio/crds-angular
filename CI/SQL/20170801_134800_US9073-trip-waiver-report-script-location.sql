b5c014730599e5745504d42ef4888da70cd5ec2a
ganonside <jon.horner@ingagepartners.com>
2017-08-02 09:41:37 -0400

    US9073 Added 'Use MinistryPlatform'

--- CI/SQL/20170801_134800_US9073-trip-waiver-report-script-location.sql
+++ CI/SQL/20170801_134800_US9073-trip-waiver-report-script-location.sql
@@ -1,3 +1,6 @@
+USE [MinistryPlatform]
+GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -12,11 +15,11 @@ DECLARE @ReportId INT = 325			-- From Identity
 DECLARE @TripPledgeCampaignPageId INT = 514
 DECLARE @TripReportsRoleId INT = 108
 
-IF NOT EXISTS (SELECT 1 FROM dbo.dp_Reports WHERE Report_ID = @ReportId)
+IF NOT EXISTS (SELECT 1 FROM dp_Reports WHERE Report_ID = @ReportId)
 BEGIN
-	SET IDENTITY_INSERT dbo.dp_Reports ON;
+	SET IDENTITY_INSERT dp_Reports ON;
 
-	INSERT INTO dbo.dp_Reports
+	INSERT INTO dp_Reports
 	(
 		Report_ID
 		, Report_Name
@@ -31,12 +34,12 @@ BEGIN
 		, '/MPReports/Crossroads/CRDS Trip Waiver'
 	)
 
-	SET IDENTITY_INSERT dbo.dp_Reports OFF;
+	SET IDENTITY_INSERT dp_Reports OFF;
 END
 
-IF NOT EXISTS (SELECT 1 FROM dbo.dp_Report_Pages WHERE Report_ID = @ReportId AND Page_ID = @TripPledgeCampaignPageId)
+IF NOT EXISTS (SELECT 1 FROM dp_Report_Pages WHERE Report_ID = @ReportId AND Page_ID = @TripPledgeCampaignPageId)
 BEGIN
-	INSERT INTO dbo.dp_Report_Pages
+	INSERT INTO dp_Report_Pages
 	(
 		Report_ID
 		, Page_ID
@@ -48,9 +51,9 @@ BEGIN
 	)
 END
 
-IF NOT EXISTS (SELECT 1 FROM dbo.dp_Role_Reports WHERE Role_ID = @TripReportsRoleId AND Report_ID = @ReportId)
+IF NOT EXISTS (SELECT 1 FROM dp_Role_Reports WHERE Role_ID = @TripReportsRoleId AND Report_ID = @ReportId)
 BEGIN
-	INSERT INTO dbo.dp_Role_Reports
+	INSERT INTO dp_Role_Reports
 	(
 		Role_ID
 		, Report_ID
