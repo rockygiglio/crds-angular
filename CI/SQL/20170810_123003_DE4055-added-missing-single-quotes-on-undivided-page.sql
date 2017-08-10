1022a7fc4e36c5ed25a495d63916903d8d2a3805
afranzencallibrity <afranzen@callibrity.com>
2017-08-10 14:00:08 -0400

    DE4055 Added Top 1 to sub-SELECT statement for if it ever returns > 1

--- CI/SQL/20170810_123003_DE4055-added-missing-single-quotes-on-undivided-page.sql
+++ CI/SQL/20170810_123003_DE4055-added-missing-single-quotes-on-undivided-page.sql
@@ -9,7 +9,7 @@ UPDATE [dbo].[dp_Pages]
 		, Group_ID_Table.[Description]
 		, Group_Role_ID_Table.Role_Title
 		, Group_ID_Table_Congregation_ID_Table.[Congregation_Name] AS [Site]
-		, (SELECT Attribute_Name FROM Group_Participant_Attributes GPA, Attributes A WHERE GPA.Attribute_ID = A.Attribute_ID AND GPA.Group_Participant_ID = Group_Participants.Group_Participant_ID AND GETDATE() BETWEEN GPA.Start_Date AND ISNULL(GPA.End_Date,GETDATE()) AND Attribute_Type_ID = 85) AS [Facilitator_Training]
+		, (SELECT TOP 1 Attribute_Name FROM Group_Participant_Attributes GPA, Attributes A WHERE GPA.Attribute_ID = A.Attribute_ID AND GPA.Group_Participant_ID = Group_Participants.Group_Participant_ID AND GETDATE() BETWEEN GPA.Start_Date AND ISNULL(GPA.End_Date,GETDATE()) AND Attribute_Type_ID = 85) AS [Facilitator_Training]
 		, (STUFF((SELECT '' ''+ c.First_Name + '' '' + c.Last_Name + '' ('' + c.email_address + '') '' FROM Group_Participants gp INNER JOIN Contacts c ON c.Participant_record = gp.Participant_Id WHERE group_id = Group_ID_Table.[Group_ID] AND Group_Role_ID = 22 FOR XML PATH('''')), 1, 1, '''')) AS [Facilitators]
 		, Group_Participants.[Notes]'
       ,[Selected_Record_Expression] = 'Participant_ID_Table_Contact_ID_Table.Display_Name'
