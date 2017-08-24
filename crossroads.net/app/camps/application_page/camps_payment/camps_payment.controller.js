dd4e0360b17bb145e28f49a1f62462264a46f024
Jim Kriz <jim.kriz@ingagepartners.com>
2017-08-24 13:48:43 -0400

    US9525: Replace code in camps with embed component

--- crossroads.net/app/camps/application_page/camps_payment/camps_payment.controller.js
+++ crossroads.net/app/camps/application_page/camps_payment/camps_payment.controller.js
@@ -21,30 +21,17 @@ export default class CampPaymentController {
       this.redirectTo = this.state.toParams.redirectTo;
     }
 
-    // eslint-disable-next-line global-require
-    this.iFrameResizer = require('iframe-resizer/js/iframeResizer.min.js');
-
-    this.iFrames = this.iFrameResizer({
-      heightCalculationMethod: 'taggedElement',
-      checkOrigin: false,
-      interval: -16
-    }, this.iframeSelector);
-
     switch (__CRDS_ENV__) {
       case 'local':
-        this.baseUrl = 'http://local.crossroads.net:8080/give';
         this.returnUrl = 'http://local.crossroads.net:3000/camps';
         break;
       case 'int':
-        this.baseUrl = 'https://embedint.crossroads.net/give';
         this.returnUrl = 'https://int.crossroads.net/camps';
         break;
       case 'demo':
-        this.baseUrl = 'https://embeddemo.crossroads.net/give';
         this.returnUrl = 'https://demo.crossroads.net/camps';
         break;
       default:
-        this.baseUrl = 'https://embed.crossroads.net/give';
         this.returnUrl = 'https://crossroads.net/camps';
         break;
     }
@@ -63,10 +50,6 @@ export default class CampPaymentController {
     } else {
       this.depositPrice = (this.campsService.productInfo.financialAssistance) ? 50 : this.campsService.productInfo.depositPrice;
     }
-  }
-
-  $onDestroy() {
-    this.closeIframes();
   }
 
   buildUrl() {
@@ -90,15 +73,7 @@ export default class CampPaymentController {
       url = encodeURIComponent(`${this.returnUrl}/${campId}/confirmation/${contactId}`);
     }
 
-    return this.sce.trustAsResourceUrl(`${this.baseUrl}?type=payment&min_payment=${this.depositPrice}&invoice_id=${invoiceId}&total_cost=${this.totalPrice}&title=${this.campsService.campTitle}&url=${url}`);
-  }
-
-  closeIframes() {
-    this.iFrames.forEach((frame) => {
-      if (frame.iFrameResizer !== undefined) {
-        frame.iFrameResizer.close();
-      }
-    });
+    return this.sce.trustAsResourceUrl(`?type=payment&min_payment=${this.depositPrice}&invoice_id=${invoiceId}&total_cost=${this.totalPrice}&title=${this.campsService.campTitle}&url=${url}`);
   }
 
   getOptionPrice() {
